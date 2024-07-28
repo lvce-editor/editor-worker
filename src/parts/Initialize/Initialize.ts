51b7f61b101be932503e5bb2f31c6a8d15855447
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-28 16:47:40 +0200

    feature: add syncIncremental option (#17)

--- src/parts/Initialize/Initialize.ts
+++ src/parts/Initialize/Initialize.ts
@@ -1,13 +1,17 @@
 import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
 import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
-import * as SyntaxHighlightingWorker from '../SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts'
+import * as SyncIncremental from '../SyncIncremental/SyncIncremental.ts'
 import * as SyntaxHighlightingState from '../SyntaxHighlightingState/SyntaxHighlightingState.ts'
+import * as SyntaxHighlightingWorker from '../SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts'
 
-export const intialize = async (syntaxHighlightingEnabled: boolean) => {
+export const intialize = async (syntaxHighlightingEnabled: boolean, syncIncremental: boolean) => {
   await RendererProcess.listen()
   if (syntaxHighlightingEnabled) {
     SyntaxHighlightingState.setEnabled(true)
     await SyntaxHighlightingWorker.listen()
+  }
+  if (syncIncremental) {
+    SyncIncremental.setEnabled(true)
   }
   await ExtensionHostWorker.listen()
 }
---
+++ src/parts/SyncIncremental/SyncIncremental.ts
@@ -0,0 +1,9 @@
+let enabled = false
+
+export const setEnabled = (value: boolean) => {
+  enabled = value
+}
+
+export const getEnabled = () => {
+  return enabled
+}
