076f120ef2c83546e65420fa1aac4efd479933cb
Le Vivilet <levivlet@pm.me>
2024-07-28 12:16:06 +0200

    feature: add syntax highlighting worker icp

--- src/parts/Initialize/Initialize.ts
+++ src/parts/Initialize/Initialize.ts
@@ -1,7 +1,11 @@
 import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
 import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
+import * as SyntaxHighlightingWorker from '../SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts'
 
-export const intialize = async () => {
+export const intialize = async (syntaxHighlightingEnabled: boolean) => {
   await RendererProcess.listen()
+  if (syntaxHighlightingEnabled) {
+    await SyntaxHighlightingWorker.listen()
+  }
   await ExtensionHostWorker.listen()
 }
