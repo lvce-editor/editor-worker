7f072813c41d2c2ebc5daaf53223db27eef880b7
Le Vivilet <levivlet@pm.me>
2024-07-31 21:55:40 +0200

    feature: add color picker rendering to editor worker

---
+++ src/parts/ColorPickerRender/ColorPickerRender.ts
@@ -0,0 +1,37 @@
+import * as GetColorPickerVirtualDom from '../GetColorPickerVirtualDom/GetColorPickerVirtualDom.ts'
+import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
+import type { ColorPickerState } from '../ColorPickerTypes/ColorPickerTypes.ts'
+
+export const hasFunctionalRender = true
+
+export const hasFunctionalRootRender = true
+
+const renderColor = {
+  isEqual(oldState: ColorPickerState, newState: ColorPickerState) {
+    return oldState.color === newState.color
+  },
+  apply(oldState: ColorPickerState, newState: ColorPickerState) {
+    return [/* method */ RenderMethod.SetColor, /* color */ newState.color]
+  },
+}
+
+const renderOffsetX = {
+  isEqual(oldState: ColorPickerState, newState: ColorPickerState) {
+    return oldState.offsetX === newState.offsetX
+  },
+  apply(oldState: ColorPickerState, newState: ColorPickerState) {
+    return [/* method */ RenderMethod.SetOffsetX, /* offsetX */ newState.offsetX]
+  },
+}
+
+const renderColorPicker = {
+  isEqual(oldState: ColorPickerState, newState: ColorPickerState) {
+    return oldState.min === newState.min && oldState.max === newState.max
+  },
+  apply(oldState: ColorPickerState, newState: ColorPickerState) {
+    const dom = GetColorPickerVirtualDom.getColorPickerVirtualDom()
+    return ['Viewlet.setDom2', dom]
+  },
+}
+
+export const render = [renderColorPicker, renderColor, renderOffsetX]
