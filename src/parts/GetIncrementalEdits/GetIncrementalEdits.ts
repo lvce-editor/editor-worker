18f72ab2719ee7567be11c425c19f08d6a2d4ade
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-01 17:58:20 +0200

    feature: add back incremental syntax highlighting (#31)

--- src/parts/GetIncrementalEdits/GetIncrementalEdits.ts
+++ src/parts/GetIncrementalEdits/GetIncrementalEdits.ts
@@ -1,9 +1,7 @@
 import * as EditOrigin from '../EditOrigin/EditOrigin.ts'
-import * as GetInitialLineState from '../GetInitialLineState/GetInitialLineState.ts'
-import * as SafeTokenizeLine from '../SafeTokenizeLine/SafeTokenizeLine.ts'
-import * as TokenizerMap from '../TokenizerMap/TokenizerMap.ts'
+import * as SyntaxHighlightingWorker from '../SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts'
 
-export const getIncrementalEdits = (oldState: any, newState: any) => {
+export const getIncrementalEdits = async (oldState: any, newState: any) => {
   if (!newState.undoStack) {
     return undefined
   }
@@ -15,46 +13,16 @@ export const getIncrementalEdits = (oldState: any, newState: any) => {
       const lines = newState.lines
       const oldLine = oldState.lines[rowIndex]
       const newLine = lines[rowIndex]
-      const oldTokenizer = TokenizerMap.get(oldState.tokenizerId)
-      const newTokenizer = TokenizerMap.get(newState.tokenizerId)
-      const initialLineState = newState.lineCache[rowIndex] || GetInitialLineState.getInitialLineState(newTokenizer.initialLineState)
-      const { tokens: oldTokens } = SafeTokenizeLine.safeTokenizeLine(
+      const incrementalEdits = await SyntaxHighlightingWorker.invoke(
+        'TokenizeIncremental.tokenizeIncremental',
+        newState.uid,
         newState.languageId,
-        oldTokenizer.tokenizeLine,
         oldLine,
-        initialLineState,
-        newTokenizer.hasArrayReturn,
-      )
-      // @ts-ignore
-      const { tokens: newTokens, lineState } = SafeTokenizeLine.safeTokenizeLine(
-        newState.languageId,
-        newTokenizer.tokenizeLine,
         newLine,
-        initialLineState,
-        newTokenizer.hasArrayReturn,
+        rowIndex,
+        newState.minLineY,
       )
-      if (newTokens.length !== oldTokens.length) {
-        return undefined
-      }
-      const incrementalEdits = []
-      let offset = 0
-      const relativeRowIndex = rowIndex - newState.minLineY
-      for (let i = 0; i < oldTokens.length; i += 2) {
-        const oldTokenType = oldTokens[i]
-        const oldTokenLength = oldTokens[i + 1]
-        const newTokenType = newTokens[i]
-        const newTokenLength = newTokens[i + 1]
-        if (oldTokenType === newTokenType && oldTokenLength !== newTokenLength && oldTokenLength > 0) {
-          const columnTokenIndex = i / 2
-          incrementalEdits.push({
-            rowIndex: relativeRowIndex,
-            columnIndex: columnTokenIndex,
-            text: newLine.slice(offset, offset + newTokenLength),
-          })
-        }
-        offset += newTokenLength
-      }
-      if (incrementalEdits.length === 1) {
+      if (incrementalEdits && incrementalEdits.length === 1) {
         return incrementalEdits
       }
     }
