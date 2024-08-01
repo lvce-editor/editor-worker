75dbe7c4a0b31d4dbbb1ce1a02dd5524fac87569
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-01 22:17:02 +0200

    feature: add copyLineUp integration test (#38)

---
+++ test-integration/editor-copy-line-up.test.js
@@ -0,0 +1,25 @@
+import * as assert from 'assert'
+
+export const test = async (rpc) => {
+  await rpc.invoke('Editor.create', {
+    id: 0,
+    content: 'abc',
+    fontFamily: '',
+    fontSize: 14,
+    lineHeight: 20,
+    fontWeight: 400,
+    letterSpacing: 0.5,
+    tabSize: 2,
+    width: 100,
+    isMonospaceFont: true,
+    charWidth: 9,
+  })
+  await rpc.invoke('Editor.cursorSet', 0, 0, 0)
+  await rpc.invoke('Editor.copyLineUp', 0)
+  const text = await rpc.invoke('Editor.getText', 0)
+  assert.strictEqual(
+    text,
+    `abc
+abc`,
+  )
+}
