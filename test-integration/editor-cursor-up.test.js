d11915a4c78b5b966dd3bd87b7fb6ae48763f1cb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-01 22:33:11 +0200

    feature: add cursor up test (#43)

---
+++ test-integration/editor-cursor-up.test.js
@@ -0,0 +1,21 @@
+import * as assert from 'assert'
+
+export const test = async (rpc) => {
+  await rpc.invoke('Editor.create', {
+    id: 0,
+    content: 'abc\nabc',
+    fontFamily: '',
+    fontSize: 14,
+    lineHeight: 20,
+    fontWeight: 400,
+    letterSpacing: 0.5,
+    tabSize: 2,
+    width: 100,
+    isMonospaceFont: true,
+    charWidth: 9,
+  })
+  await rpc.invoke('Editor.cursorSet', 0, 1, 0)
+  await rpc.invoke('Editor.cursorUp', 0)
+  const selections = await rpc.invoke('Editor.getSelections', 0)
+  assert.deepEqual(selections, new Uint32Array([0, 0, 0, 0]))
+}
