84c571c02fe1e2b0a6e0760c94442c1c3e5082bc
Le Vivilet <levivlet@pm.me>
2024-08-01 22:15:33 +0200

    feature: add copyLineUp integration test

--- test-integration/test.js
+++ test-integration/test.js
@@ -1,19 +1,29 @@
+import { readdir } from 'fs/promises'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { pathToFileURL } from 'url'
 import { setup } from '../test-integration-util/setup.js'
-import * as CreateEditor from './create-editor.test.js'
-import * as EditorCopyLineDown from './editor-copy-line-down.test.js'
-import * as FindWidget from './find-widget.test.js'
 
-const tests = [CreateEditor, FindWidget, EditorCopyLineDown]
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const runTests = async (tests) => {
-  for (const module of tests) {
+const runTests = async (dirents) => {
+  for (const dirent of dirents) {
+    const absolutePath = join(__dirname, dirent)
+    const absoluteUri = pathToFileURL(absolutePath).toString()
+    const module = await import(absoluteUri)
     const rpc = await setup()
     await module.test(rpc)
   }
 }
 
+const isTest = (dirent) => {
+  return dirent.endsWith('.test.js')
+}
+
 const main = async () => {
-  await runTests(tests)
+  const dirents = await readdir(__dirname)
+  const filteredDirents = dirents.filter(isTest)
+  await runTests(filteredDirents)
   process.exit(0)
 }
 
