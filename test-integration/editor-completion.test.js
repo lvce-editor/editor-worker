b8f0ce20243e8c59075db8b7da568edfc0721305
Le Vivilet <levivlet@pm.me>
2024-08-04 17:53:58 +0200

    fix test

--- test-integration/editor-completion.test.js
+++ test-integration/editor-completion.test.js
@@ -15,15 +15,14 @@ export const test = async (rpc) => {
     charWidth: 9,
   })
   await rpc.invoke('Editor.cursorSet', 0, 0, 1)
-  await rpc.invoke('Editor.openCompletion', 0)
-  const { commands } = await rpc.invoke('EditorCompletion.focusNext', 0)
-  assert.strictEqual(commands[4][2], 'setDom')
-  assert.deepEqual(commands[4][3], [
-    { type: 4, childCount: 1 },
+  const { commands } = await rpc.invoke('Editor.openCompletion', 0)
+  assert.strictEqual(commands[5][2], 'setDom')
+  assert.deepEqual(commands[5][3], [
+    { type: 4, childCount: 2 },
     {
       type: 4,
       role: 'option',
-      className: 'EditorCompletionItem',
+      className: 'EditorCompletionItem EditorCompletionItemFocused',
       top: 0,
       childCount: 2,
     },
@@ -39,6 +38,26 @@ export const test = async (rpc) => {
       childCount: 1,
     },
     { type: 12, text: 'a', childCount: 0 },
-    { type: 12, text: 'bc', childCount: 0 },
+    { type: 12, text: 'bc 1', childCount: 0 },
+    {
+      type: 4,
+      role: 'option',
+      className: 'EditorCompletionItem',
+      top: 20,
+      childCount: 2,
+    },
+    {
+      type: 4,
+      className: 'ColoredMaskIcon SymbolDefault',
+      childCount: 0,
+    },
+    { type: 4, className: 'Label', childCount: 2 },
+    {
+      type: 8,
+      className: 'EditorCompletionItemHighlight',
+      childCount: 1,
+    },
+    { type: 12, text: 'a', childCount: 0 },
+    { type: 12, text: 'bc 2', childCount: 0 },
   ])
 }
