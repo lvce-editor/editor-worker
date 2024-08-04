03633c4e61e42543f71efaf67ce27b957de83325
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-04 22:22:14 +0200

    feature: move applyWidgetChange function into separate file (#65)

--- src/parts/ApplyWidgetChanges/ApplyWidgetChanges.ts
+++ src/parts/ApplyWidgetChanges/ApplyWidgetChanges.ts
@@ -1,5 +1,4 @@
-import * as EditOrigin from '../EditOrigin/EditOrigin.ts'
-import * as Widgets from '../Widgets/Widgets.ts'
+import * as ApplyWidgetChange from '../ApplyWidgetChange/ApplyWidgetChange.ts'
 
 export const applyWidgetChanges = (editor: any, changes: any) => {
   const widgets = editor.widgets || []
@@ -7,15 +6,8 @@ export const applyWidgetChanges = (editor: any, changes: any) => {
     return widgets
   }
   const newWidgets = widgets.map((widget: any) => {
-    const module = Widgets.getModule(widget.id)
-    if (changes.length === 1 && changes[0].origin === EditOrigin.EditorType && module.handleEditorType) {
-      const newState = module.handleEditorType(editor, widget.newState)
-      return {
-        ...widget,
-        newState,
-      }
-    }
-    return widget
+    const newWidget = ApplyWidgetChange.applyWidgetChange(editor, widget, changes)
+    return newWidget
   })
   return newWidgets
 }
