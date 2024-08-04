db81b599ddc3f94c4ca1061fb8e36ba582c5532a
Le Vivilet <levivlet@pm.me>
2024-08-04 21:20:30 +0200

    feature: move completion widget into separate file

--- src/parts/EditorCommand/EditorCommandApplyEdit.ts
+++ src/parts/EditorCommand/EditorCommandApplyEdit.ts
@@ -1,9 +1,7 @@
-// @ts-ignore
 import * as Editor from '../Editor/Editor.ts'
 import * as Assert from '../Assert/Assert.ts'
 
-// @ts-ignore
-export const applyEdit = async (editor, changes) => {
+export const applyEdit = async (editor: any, changes: any[]) => {
   Assert.object(editor)
   Assert.array(changes)
   return Editor.scheduleDocumentAndCursorsSelections(editor, changes)
---
+++ src/parts/EditorCompletionWidget/EditorCompletionWidget.ts
@@ -0,0 +1,31 @@
+import * as EditorCompletionRender from '../EditorCompletionRender/EditorCompletionRender.ts'
+
+export const render = (oldState: any, newState: any) => {
+  const commands = EditorCompletionRender.renderCompletion(oldState, newState)
+  const wrappedCommands = []
+  const uid = newState.uid
+  for (const command of commands) {
+    wrappedCommands.push(['Viewlet.send', uid, ...command])
+  }
+  return wrappedCommands
+}
+
+export const add = (widget: any) => {
+  const commands = render(widget.oldState, widget.newState)
+  const id = 'EditorCompletion'
+  // TODO how to generate a unique integer id
+  // that doesn't collide with ids created in renderer worker?
+  const uid = widget.newState.uid
+  const allCommands: any[] = []
+  allCommands.push(['Viewlet.create', id, uid])
+  allCommands.push(...commands)
+  return allCommands
+}
+
+export const remove = (widget: any) => {
+  return [['Viewlet.send', widget.newState.uid, 'dispose']]
+}
+
+export const handleEditorType = (state: any) => {
+  return state
+}
