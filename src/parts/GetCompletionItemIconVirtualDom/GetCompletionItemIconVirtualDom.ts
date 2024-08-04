b13eef4ac41173ac5e09f464ff192961ef69b602
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-04 23:00:20 +0200

    feature: add completion icon dom test (#76)

--- src/parts/GetCompletionItemIconVirtualDom/GetCompletionItemIconVirtualDom.ts
+++ src/parts/GetCompletionItemIconVirtualDom/GetCompletionItemIconVirtualDom.ts
@@ -2,7 +2,7 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetFileIconVirtualDom from '../GetFileIconVirtualDom/GetFileIconVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
-export const getIconDom = (fileIcon: any, symbolName: string) => {
+export const getIconDom = (fileIcon: string, symbolName: string) => {
   if (fileIcon) {
     return GetFileIconVirtualDom.getFileIconVirtualDom(fileIcon)
   }
