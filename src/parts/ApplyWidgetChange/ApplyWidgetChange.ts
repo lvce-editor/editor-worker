fc186653a599b4aaabb4caed3c076781bd152abc
Le Vivilet <levivlet@pm.me>
2024-08-04 22:20:36 +0200

    feature: move applyWidgetChange function into separate file

---
+++ src/parts/ApplyWidgetChange/ApplyWidgetChange.ts
@@ -0,0 +1,14 @@
+import * as EditOrigin from '../EditOrigin/EditOrigin.ts'
+import * as Widgets from '../Widgets/Widgets.ts'
+
+export const applyWidgetChange = (editor: any, widget: any, changes: any[]) => {
+  const module = Widgets.getModule(widget.id)
+  if (changes.length === 1 && changes[0].origin === EditOrigin.EditorType && module.handleEditorType) {
+    const newState = module.handleEditorType(editor, widget.newState)
+    return {
+      ...widget,
+      newState,
+    }
+  }
+  return widget
+}
