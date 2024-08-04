e853ba82063763176a344ab33f07b35e7bcff8bb
Le Vivilet <levivlet@pm.me>
2024-08-04 15:03:07 +0200

    try to improve rendering

--- src/parts/EditorCommand/EditorCommandCloseCompletion.ts
+++ src/parts/EditorCommand/EditorCommandCloseCompletion.ts
@@ -1,9 +1,16 @@
-import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+const isCompletionWidget = (widget: any) => {
+  return widget.id === 'completion'
+}
 
-export const closeCompletion = async (editor: any) => {
-  // TODO
-  const completionUid = editor.completionUid
-  await RendererWorker.invoke('Viewlet.dispose', completionUid)
-  editor.completionUid = 0
-  return editor
+export const closeCompletion = (editor: any) => {
+  const { widgets } = editor
+  const completionWidgetIndex = widgets.findIndex(isCompletionWidget)
+  if (completionWidgetIndex === -1) {
+    return editor
+  }
+  const newWidgets = [...widgets.slice(0, completionWidgetIndex), ...widgets.slice(completionWidgetIndex + 1)]
+  return {
+    ...editor,
+    widgets: newWidgets,
+  }
 }
