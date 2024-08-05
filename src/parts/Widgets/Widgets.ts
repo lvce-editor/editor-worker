41bfd1f105bbf154f1349901e6be99f598b36f11
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-05 19:54:49 +0200

    feature: add applyWidgetChange test (#85)

--- src/parts/Widgets/Widgets.ts
+++ src/parts/Widgets/Widgets.ts
@@ -1,7 +1,9 @@
 import * as EditorCompletionWidget from '../EditorCompletionWidget/EditorCompletionWidget.ts'
 import * as WidgetId from '../WidgetId/WidgetId.ts'
+import * as WidgetModules from '../WidgetModules/WidgetModules.ts'
 
 export const getModule = (id: string) => {
+  return WidgetModules.get(id)
   switch (id) {
     case WidgetId.Completion:
       return EditorCompletionWidget
