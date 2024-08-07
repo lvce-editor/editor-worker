82091b8f70b9bfacfe824ef23d283d8ddca1b598
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-07 21:53:10 +0200

    feature: add types for editor cursor commands (#138)

--- src/parts/EditorCommand/EditorCommandCursorHorizontalRight.ts
+++ src/parts/EditorCommand/EditorCommandCursorHorizontalRight.ts
@@ -1,11 +1,8 @@
-// @ts-ignore
 import * as Editor from '../Editor/Editor.ts'
-// @ts-ignore
 import * as GetSelectionPairs from '../GetSelectionPairs/GetSelectionPairs.ts'
 import * as EditorGetPositionRight from './EditorCommandGetPositionRight.ts'
 
-// @ts-ignore
-const getNewSelections = (selections, lines, getDelta) => {
+const getNewSelections = (selections: any, lines: string[], getDelta: any) => {
   const newSelections = new Uint32Array(selections.length)
   for (let i = 0; i < selections.length; i += 4) {
     const [selectionStartRow, selectionStartColumn, selectionEndRow, selectionEndColumn] = GetSelectionPairs.getSelectionPairs(selections, i)
@@ -19,8 +16,7 @@ const getNewSelections = (selections, lines, getDelta) => {
   return newSelections
 }
 
-// @ts-ignore
-export const editorCursorHorizontalRight = (editor, getDelta) => {
+export const editorCursorHorizontalRight = (editor: any, getDelta: any) => {
   const { lines, selections } = editor
   const newSelections = getNewSelections(selections, lines, getDelta)
   return Editor.scheduleSelections(editor, newSelections)
