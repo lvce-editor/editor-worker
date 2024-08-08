d9769538a8318c10bdc27c9d61396ea5ea54340f
Le Vivilet <levivlet@pm.me>
2024-08-08 22:23:27 +0200

    remove unused code

--- src/parts/EditorCommand/EditorCommandSelectAllOccurrences.ts
+++ src/parts/EditorCommand/EditorCommandSelectAllOccurrences.ts
@@ -57,39 +57,6 @@ const getAllOccurrencesMultiLine = (lines: string[], wordParts: string[]) => {
   return new Uint32Array(newSelections)
 }
 
-<<<<<<< HEAD
-const getWordStartIndex = (line: string, index: number) => {
-  for (let i = index - 1; i >= 0; i--) {
-    if (!IsAlphaNumeric.isAlphaNumeric(line[i])) {
-      return i + 1
-    }
-  }
-  return 0
-}
-
-const getWordEndIndex = (line: string, index: number) => {
-  for (let i = index; i < line.length; i++) {
-    if (!IsAlphaNumeric.isAlphaNumeric(line[i])) {
-      return i
-    }
-  }
-  return line.length - 1
-}
-
-const getWordMatchAtPosition = (lines: string[], rowIndex: number, columnIndex: number) => {
-  const line = lines[rowIndex]
-  const start = getWordStartIndex(line, columnIndex)
-  const end = getWordEndIndex(line, columnIndex)
-  const word = line.slice(start, end)
-  return {
-    start,
-    end,
-    word,
-  }
-}
-
-=======
->>>>>>> 6df37b882b42 (reuse code)
 const getNewSelections = (lines: string[], selections: any) => {
   if (selections.length < 4) {
     throw new Error('selections must have at least one entry')
