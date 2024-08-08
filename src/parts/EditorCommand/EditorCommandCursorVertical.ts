b2effb1e338dfead5ca26b724dd9ce7c693c582a
Le Vivilet <levivlet@pm.me>
2024-08-08 22:57:05 +0200

    feature: remove unused cursorVertical code

--- src/parts/EditorCommand/EditorCommandCursorVertical.ts
+++ src/parts/EditorCommand/EditorCommandCursorVertical.ts
@@ -1,41 +1,5 @@
 import * as Editor from '../Editor/Editor.ts'
-import * as TextDocument from '../TextDocument/TextDocument.ts'
-import * as TextSegmenter from '../TextSegmenter/TextSegmenter.ts'
 import * as EditorSelection from '../EditorSelection/EditorSelection.ts'
-
-// @ts-ignore
-const editorCursorsVerticalWithIntlSegmenter = (editor: any, getPosition: any, getEdgePosition: any, delta: any) => {
-  const edgePosition = getEdgePosition(editor)
-  // @ts-ignore
-  const moveSelection = (selection) => {
-    const position = getPosition(selection)
-    if (position.rowIndex === edgePosition.rowIndex) {
-      return {
-        start: edgePosition,
-        end: edgePosition,
-      }
-    }
-
-    const lineCurrent = TextDocument.getLine(editor, position.rowIndex)
-    const lineOther = TextDocument.getLine(editor, position.rowIndex + delta)
-
-    const segmenter = TextSegmenter.create()
-    const lineCurrentVisualIndex = segmenter.visualIndex(lineCurrent, position.columnIndex)
-
-    const lineOtherModelIndex = segmenter.modelIndex(lineOther, lineCurrentVisualIndex)
-
-    const newPosition = {
-      rowIndex: position.rowIndex + delta,
-      columnIndex: lineOtherModelIndex,
-    }
-    return {
-      start: newPosition,
-      end: newPosition,
-    }
-  }
-  const selectionEdits = editor.selections.map(moveSelection)
-  return Editor.scheduleSelections(editor, selectionEdits)
-}
 
 const moveSelectionWithoutIntlSegmenter = (
   selections: any,
@@ -43,7 +7,7 @@ const moveSelectionWithoutIntlSegmenter = (
   selectionStartRow: number,
   selectionStartColumn: number,
   selectionEndRow: number,
-  selectionEndColumn: number,
+  selectionEndColumn: number
 ) => {
   if (selectionStartRow === 0) {
     EditorSelection.moveRangeToPosition(selections, i, 0, 0)
@@ -57,16 +21,7 @@ const getNewSelections = (selections: any) => {
 }
 
 export const cursorVertical = (editor: any, getPosition: any, getEdgePosition: any, delta: any) => {
-  // if (TextSegmenter.supported()) {
-  //   return editorCursorsVerticalWithIntlSegmenter(
-  //     editor,
-  //     getPosition,
-  //     getEdgePosition,
-  //     delta
-  //   )
-  // }
-  const selections = editor.selections
-  // @ts-ignore
-  const newSelections = getNewSelections(selections, delta)
+  const { selections } = editor
+  const newSelections = getNewSelections(selections)
   return Editor.scheduleSelections(editor, newSelections)
 }
