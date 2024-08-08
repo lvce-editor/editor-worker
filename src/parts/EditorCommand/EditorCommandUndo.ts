db7fd761ad0a92e1f77557d8065799690bdb1ae7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-08 22:29:36 +0200

    feature: move inverseChange function into separate file (#151)

--- src/parts/EditorCommand/EditorCommandUndo.ts
+++ src/parts/EditorCommand/EditorCommandUndo.ts
@@ -1,27 +1,13 @@
 import * as Editor from '../Editor/Editor.ts'
+import * as InverseChange from '../InverseChange/InverseChange.ts'
 
-// @ts-ignore
-const inverseChange = (edit) => {
-  const endColumnIndex = edit.end.columnIndex - edit.deleted[0].length + edit.inserted[0].length
-  return {
-    start: edit.start,
-    end: {
-      rowIndex: edit.end.rowIndex,
-      columnIndex: endColumnIndex,
-    },
-    inserted: edit.deleted,
-    deleted: edit.inserted,
-  }
-}
-
-// @ts-ignore
-export const undo = (state) => {
+export const undo = (state: any) => {
   const { undoStack } = state
   if (undoStack.length === 0) {
     return state
   }
   // TODO avoid side effect?
   const last = undoStack.pop()
-  const inverseChanges = last.map(inverseChange)
+  const inverseChanges = last.map(InverseChange.inverseChange)
   return Editor.scheduleDocumentAndCursorsSelectionIsUndo(state, inverseChanges)
 }
