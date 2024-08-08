c880e3bd40fd4b874982afa53e38315b4c49a54c
Le Vivilet <levivlet@pm.me>
2024-08-08 22:58:49 +0200

    test

--- test/EditorCopyLineUp.test.ts
+++ test/EditorCopyLineUp.test.ts
@@ -3,20 +3,11 @@ import * as EditorCopyLineUp from '../src/parts/EditorCommand/EditorCommandCopyL
 import * as TokenizePlainText from '../src/parts/TokenizePlainText/TokenizePlainText.ts'
 
 test('editorCopyLineUp', () => {
-  const cursor = {
-    rowIndex: 2,
-    columnIndex: 0,
-  }
   const editor = {
     lines: ['line 1', 'line 2', 'line 3'],
-    cursor,
-    selections: [
-      {
-        start: cursor,
-        end: cursor,
-      },
-    ],
+    selections: new Uint32Array([2, 0, 2, 0]),
     tokenizer: TokenizePlainText,
+    undoStack: [],
   }
   const newEditor = EditorCopyLineUp.copyLineUp(editor)
   expect(newEditor.lines).toEqual(['line 1', 'line 2', 'line 3', 'line 3'])
