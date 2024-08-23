620c1ddafd944afeb3dd28d4974c1901d5627e56
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-23 22:10:44 +0200

    feature: automatically detect and transfer trasnferrables via jsonrpc (#166)

--- src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts
+++ src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts
@@ -2,9 +2,8 @@ import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const sendMessagePortToExtensionHostWorker = async (port: MessagePort) => {
   await RendererWorker.invokeAndTransfer(
-    [port],
     'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker',
     port,
-    'HandleMessagePort.handleMessagePort',
+    'HandleMessagePort.handleMessagePort'
   )
 }
--- src/parts/SendMessagePortToRendererProcess/SendMessagePortToRendererProcess.ts
+++ src/parts/SendMessagePortToRendererProcess/SendMessagePortToRendererProcess.ts
@@ -2,9 +2,8 @@ import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const sendMessagePortToRendererProcess = async (port: MessagePort) => {
   await RendererWorker.invokeAndTransfer(
-    [port],
     'SendMessagePortToRendererProcess.sendMessagePortToRendererProcess',
     port,
-    'HandleMessagePort.handleMessagePort',
+    'HandleMessagePort.handleMessagePort'
   )
 }
