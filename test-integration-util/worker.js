3cf720966d9d4b1aaea523674d62607f5afdc7f9
Le Vivilet <levivlet@pm.me>
2024-09-14 21:56:37 +0200

    debug

--- test-integration-util/worker.js
+++ test-integration-util/worker.js
@@ -1,36 +1,40 @@
 import { IpcChildWithNodeWorker } from '@lvce-editor/ipc'
 
 const createWorkerIpc = async (workerPath) => {
-  globalThis.WorkerGlobalScope = {}
+  try {
+    globalThis.WorkerGlobalScope = {}
 
-  globalThis.OffscreenCanvas = class {
-    getContext() {
-      return {
-        measureText() {
-          return {
-            width: 2,
-          }
-        },
+    globalThis.OffscreenCanvas = class {
+      getContext() {
+        return {
+          measureText() {
+            return {
+              width: 2,
+            }
+          },
+        }
       }
     }
-  }
 
-  const readyPromise = new Promise((resolve) => {
-    globalThis.postMessage = (message) => {
-      if (message === 'ready') {
-        resolve(undefined)
+    const readyPromise = new Promise((resolve) => {
+      globalThis.postMessage = (message) => {
+        if (message === 'ready') {
+          resolve(undefined)
+        }
       }
+    })
+
+    let _listener = (data) => {}
+
+    globalThis.addEventListener = (type, listener, options) => {
+      _listener = listener
     }
-  })
-
-  let _listener = (data) => {}
-
-  globalThis.addEventListener = (type, listener, options) => {
-    _listener = listener
+    await import(workerPath)
+    await readyPromise
+    return _listener
+  } catch (error) {
+    throw new VError(error, `Failed to start worker`)
   }
-  await import(workerPath)
-  await readyPromise
-  return _listener
 }
 
 export const createMessagePortIpc = async (listener, port) => {
@@ -47,6 +51,10 @@ const handleMessage = async (event) => {
   if (data.method === 'loadEditorWorker') {
     const workerPath = data.params[0]
     const port = data.params[1]
+    if (!port) {
+      throw new Error('port is required')
+    }
+    console.log('load editor worker', workerPath, port)
     const listener = await createWorkerIpc(workerPath)
     await createMessagePortIpc(listener, port)
   }
