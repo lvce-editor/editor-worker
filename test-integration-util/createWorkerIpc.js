be99f0e817c3f8b3fd1bbdacfd291c27401638f9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-14 22:48:12 +0200

    Discard changes to test-integration-util/createWorkerIpc.js

--- test-integration-util/createWorkerIpc.js
+++ test-integration-util/createWorkerIpc.js
@@ -1,35 +1,22 @@
+import { IpcParentWithNodeWorker } from '@lvce-editor/ipc'
 import * as JsonRpc from '@lvce-editor/json-rpc'
 import { fileURLToPath } from 'node:url'
-import { MessageChannel, Worker } from 'node:worker_threads'
-
-const createIpc = async (workerUrl) => {
-  const worker = new Worker(workerUrl, {})
-  const target = new EventTarget()
-  return {
-    addEventListener(type, listener) {
-      target.addEventListener(type, listener)
-    },
-    dispose() {
-      worker.terminate()
-    },
-  }
-}
-
-class Ipc {
-  constructor(workerUrl) {}
-}
+import { MessageChannel } from 'node:worker_threads'
 
 export const createWorkerIpc = async (workerPath) => {
   const workerUrl = fileURLToPath(new URL('./worker.js', import.meta.url))
+  const rawIpc = await IpcParentWithNodeWorker.create({
+    path: workerUrl,
+  })
+  const ipc = IpcParentWithNodeWorker.wrap(rawIpc)
 
-  const ipc = await createIpc(workerUrl)
-
-  ipc.addEventListener('message', (message) => {
+  ipc.on('message', (event) => {
+    const message = event.data
     JsonRpc.resolve(message.id, message)
   })
 
   const { port1, port2 } = new MessageChannel()
-  await JsonRpc.invokeAndTransfer(ipc, 'loadEditorWorker', workerPath, port1)
+  await JsonRpc.invokeAndTransfer(ipc, [port1], 'loadEditorWorker', workerPath, port1)
   return {
     port: port2,
     ipc,
