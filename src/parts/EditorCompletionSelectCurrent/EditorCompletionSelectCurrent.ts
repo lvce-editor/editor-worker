e0e22ed41358279871428e8c08c046fbd95a7914
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-06 07:36:02 +0200

    fix: error when completion widget is alredy disposed (#170)

--- src/parts/EditorCompletionSelectCurrent/EditorCompletionSelectCurrent.ts
+++ src/parts/EditorCompletionSelectCurrent/EditorCompletionSelectCurrent.ts
@@ -3,6 +3,9 @@ import * as GetCompletionState from '../GetCompletionState/GetCompletionState.ts
 
 export const selectCurrent = (editor: any) => {
   const child = GetCompletionState.getCompletionState(editor)
+  if (!child) {
+    return editor
+  }
   const { focusedIndex } = child
   return ViewletEditorCompletionSelectIndex.selectIndex(editor, focusedIndex)
 }
