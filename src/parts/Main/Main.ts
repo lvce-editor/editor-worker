d9b15c5e367a52a5dca4c8dab2fd6ef100722542
Le Vivilet <levivlet@pm.me>
2024-10-08 20:52:10 +0200

    feature: add widget registry to simplify registering more widgets

--- src/parts/Main/Main.ts
+++ src/parts/Main/Main.ts
@@ -1,5 +1,13 @@
+import * as ColorPickerWidget from '../ColorPickerWidget/ColorPickerWidget.ts'
+import * as CompletionDetailWidget from '../CompletionDetailWidget/CompletionDetailWidget.ts'
+import * as CompletionWidget from '../CompletionWidget/CompletionWidget.ts'
 import * as Listen from '../Listen/Listen.ts'
+import * as WidgetId from '../WidgetId/WidgetId.ts'
+import * as WidgetRegistry from '../WidgetRegistry/WidgetRegistry.ts'
 
 export const main = async () => {
   await Listen.listen()
+  WidgetRegistry.set(WidgetId.ColorPicker, ColorPickerWidget)
+  WidgetRegistry.set(WidgetId.Completion, CompletionWidget)
+  WidgetRegistry.set(WidgetId.CompletionDetail, CompletionDetailWidget)
 }
