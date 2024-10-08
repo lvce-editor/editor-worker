e37d54c56dd122764058ad7d5d5010534e780500
Le Vivilet <levivlet@pm.me>
2024-10-08 21:02:23 +0200

    render widget

---
+++ src/parts/EditorColorPickerWidget/EditorColorPickerWidget.ts
@@ -0,0 +1,32 @@
+import * as EditorCompletionDetailRender from '../EditorCompletionDetailRender/EditorCompletionDetailRender.ts'
+
+export const render = (oldState: any, newState: any) => {
+  const commands: any[] = EditorCompletionDetailRender.renderFull(oldState, newState)
+  const wrappedCommands = []
+  const uid = newState.uid
+  for (const command of commands) {
+    if (command[0] === 'Viewlet.setDom2') {
+      wrappedCommands.push(command)
+    } else {
+      wrappedCommands.push(['Viewlet.send', uid, ...command])
+    }
+  }
+  return wrappedCommands
+}
+
+export const add = (widget: any) => {
+  const commands = render(widget.oldState, widget.newState)
+  const id = 'EditorColorPicker'
+  // TODO how to generate a unique integer id
+  // that doesn't collide with ids created in renderer worker?
+  const uid = widget.newState.uid
+  const allCommands: any[] = []
+  allCommands.push(['Viewlet.createFunctionalRoot', id, uid])
+  allCommands.push(...commands)
+  allCommands.push(['Viewlet.send', uid, 'appendWidget'])
+  return allCommands
+}
+
+export const remove = (widget: any) => {
+  return [['Viewlet.send', widget.newState.uid, 'dispose']]
+}
