b35becd35dfeef65e22528ccd2f72cb173736d2d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-08 19:18:08 +0200

    feature: add completion detail integration test (#185)

---
+++ test-integration/editor-completion-details.test.js
@@ -0,0 +1,38 @@
+import * as assert from 'assert'
+
+export const test = async (rpc) => {
+  await rpc.invoke('Editor.create', {
+    id: 0,
+    content: 'a',
+    fontFamily: '',
+    fontSize: 14,
+    lineHeight: 20,
+    fontWeight: 400,
+    letterSpacing: 0.5,
+    tabSize: 2,
+    width: 100,
+    isMonospaceFont: true,
+    charWidth: 9,
+  })
+  await rpc.invoke('Editor.cursorSet', 0, 0, 1)
+  await rpc.invoke('Editor.openCompletion', 0)
+  const { commands } = await rpc.invoke('EditorCompletion.openDetails', 0)
+  assert.strictEqual(commands[5][0], 'Viewlet.setDom2')
+  const dom = commands[5][2]
+  assert.deepEqual(dom, [
+    {
+      type: 4,
+      className: 'Viewlet EditorCompletionDetails',
+      childCount: 2,
+    },
+    { type: 4, className: 'CompletionDetailContent', childCount: 1 },
+    { type: 12, text: 'abc', childCount: 0 },
+    {
+      type: 4,
+      className: 'CompletionDetailCloseButton',
+      onClick: 'handleClose',
+      childCount: 1,
+    },
+    { type: 4, className: 'MaskIcon IconClose', childCount: 0 },
+  ])
+}
