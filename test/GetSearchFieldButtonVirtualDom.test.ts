2e61a5caacaa64c7326744ea63d57ee6167449f1
Le Vivilet <levivlet@pm.me>
2024-10-09 22:05:06 +0200

    tsc

--- test/GetSearchFieldButtonVirtualDom.test.ts
+++ test/GetSearchFieldButtonVirtualDom.test.ts
@@ -1,10 +1,11 @@
 import { expect, test } from '@jest/globals'
 import * as AriaRoles from '../src/parts/AriaRoles/AriaRoles.ts'
 import * as GetSearchFieldButtonVirtualDom from '../src/parts/GetSearchFieldButtonVirtualDom/GetSearchFieldButtonVirtualDom.ts'
+import type { ISearchFieldButton } from '../src/parts/ISearchFieldButton/ISearchFieldButton.ts'
 import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
 
 test('getIconVirtualDom', () => {
-  const button = {
+  const button: ISearchFieldButton = {
     icon: 'MaskIconTest',
     checked: false,
     title: 'Test',
