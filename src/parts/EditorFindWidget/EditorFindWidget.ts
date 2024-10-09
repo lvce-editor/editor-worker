682ffc07a8acd1314031f0b6382c99741225ba22
Le Vivilet <levivlet@pm.me>
2024-10-09 21:44:31 +0200

    fix

--- src/parts/EditorFindWidget/EditorFindWidget.ts
+++ src/parts/EditorFindWidget/EditorFindWidget.ts
@@ -2,9 +2,10 @@ import * as AddWidget from '../AddWidget/AddWidget.ts'
 import * as EditorColorPickerRender from '../EditorColorPickerRender/EditorColorPickerRender.ts'
 import { IFindWidget } from '../IFindWidget/IFindWidget.ts'
 import * as RemoveWidget from '../RemoveWidget/RemoveWidget.ts'
+import * as FindWidgetRender from '../FindWidgetRender/FindWidgetRender.ts'
 
 export const render = (widget: IFindWidget) => {
-  const commands: any[] = EditorColorPickerRender.renderFull(widget.oldState, widget.newState)
+  const commands: readonly any[] = FindWidgetRender.apply(widget.oldState, widget.newState)
   const wrappedCommands = []
   const uid = widget.newState.uid
   for (const command of commands) {
