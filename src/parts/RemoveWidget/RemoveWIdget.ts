c19cc918c4b41e6e4ea594f1c0db2ad4ea5a1205
Le Vivilet <levivlet@pm.me>
2024-10-09 21:57:58 +0200

    tsc

--- src/parts/RemoveWidget/RemoveWIdget.ts
+++
@@ -1,6 +0,0 @@
-import type { Widget } from '../Widget/Widget.ts'
-
-export const removeWidget = <T>(widget: Widget<T>): readonly any[] => {
-  // @ts-ignore
-  return [['Viewlet.send', widget.newState.uid, 'dispose']]
-}
