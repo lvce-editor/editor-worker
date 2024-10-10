b5be7703663ba419bd93fbeb3c29ebd0c5d58de0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-10 19:28:48 +0200

    feature: register find widget (#218)

--- src/parts/Main/Main.ts
+++ src/parts/Main/Main.ts
@@ -1,13 +1,7 @@
-import * as EditorColorPickerWidget from '../EditorColorPickerWidget/EditorColorPickerWidget.ts'
-import * as EditorCompletionDetailWidget from '../EditorCompletionDetailWidget/EditorCompletionDetailWidget.ts'
-import * as EditorCompletionWidget from '../EditorCompletionWidget/EditorCompletionWidget.ts'
 import * as Listen from '../Listen/Listen.ts'
-import * as WidgetId from '../WidgetId/WidgetId.ts'
-import * as WidgetRegistry from '../WidgetRegistry/WidgetRegistry.ts'
+import * as RegisterWidgets from '../RegisterWidgets/RegisterWidgets.ts'
 
 export const main = async () => {
   await Listen.listen()
-  WidgetRegistry.set(WidgetId.ColorPicker, EditorColorPickerWidget)
-  WidgetRegistry.set(WidgetId.Completion, EditorCompletionWidget)
-  WidgetRegistry.set(WidgetId.CompletionDetail, EditorCompletionDetailWidget)
+  RegisterWidgets.registerWidgets()
 }
