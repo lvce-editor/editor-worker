6a489d98b95b35d4cac2df999efdd5be6ba8a29a
Le Vivilet <levivlet@pm.me>
2024-10-10 19:43:19 +0200

    feature: simplify color picker open function

--- src/parts/EditorCommand/EditorCommandColorPicker.ts
+++ src/parts/EditorCommand/EditorCommandColorPicker.ts
@@ -1,32 +1,14 @@
-import type { ColorPickerWidget } from '../ColorPickerWidget/ColorPickerWidget.ts'
+import * as AddWidgetToEditor from '../AddWidgetToEditor/AddWidgetToEditor.ts'
+import * as ColorPicker from '../ColorPicker/ColorPicker.ts'
+import type { ColorPickerState } from '../ColorPickerState/ColorPickerState.ts'
 import * as ColorPickerWidgetFactory from '../ColorPickerWidgetFactory/ColorPickerWidgetFactory.ts'
 import * as FocusKey from '../FocusKey/FocusKey.ts'
-import * as HasWidget from '../HasWidget/HasWidget.ts'
-import * as SetAdditionalFocus from '../SetAdditionalFocus/SetAdditionalFocus.ts'
 import * as WidgetId from '../WidgetId/WidgetId.ts'
-import * as ColorPicker from '../ColorPicker/ColorPicker.ts'
+
+const newStateGenerator = (state: ColorPickerState): ColorPickerState => {
+  return ColorPicker.loadContent(state)
+}
 
 export const openColorPicker = async (editor: any) => {
-  const { widgets } = editor
-  if (HasWidget.hasWidget(widgets, WidgetId.ColorPicker)) {
-    return editor
-  }
-  const colorPickerWidget: ColorPickerWidget = ColorPickerWidgetFactory.create()
-  const newState = ColorPicker.loadContent(colorPickerWidget.newState)
-  const newWidget = {
-    ...colorPickerWidget,
-    newState,
-  }
-  const newWidgets = [...widgets, newWidget]
-
-  // TODO avoid side effect, apply focus shift during render
-  await SetAdditionalFocus.setAdditionalFocus(FocusKey.ColorPicker)
-  const newEditor = {
-    ...editor,
-    widgets: newWidgets,
-  }
-  // TODO
-  return {
-    ...newEditor,
-  }
+  return AddWidgetToEditor.addWidgetToEditor(WidgetId.ColorPicker, FocusKey.ColorPicker, editor, ColorPickerWidgetFactory.create, newStateGenerator)
 }
