c62679efe47b5431d7b87c1d5441a6f5d48fa404
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-10 19:49:40 +0200

    feature: simplify opening completion details (#222)

--- src/parts/EditorCompletionOpenDetails/EditorCompletionOpenDetails.ts
+++ src/parts/EditorCompletionOpenDetails/EditorCompletionOpenDetails.ts
@@ -1,36 +1,32 @@
-import { CompletionDetailState } from '../CompletionDetailState/CompletionDetailState.ts'
+import * as AddWidgetToEditor from '../AddWidgetToEditor/AddWidgetToEditor.ts'
+import type { CompletionDetailState } from '../CompletionDetailState/CompletionDetailState.ts'
 import * as CompletionDetailWidgetFactory from '../CompletionDetailWidgetFactory/CompletionDetailWidgetFactory.ts'
-import * as GetCompletionDetailState from '../GetCompletionDetailState/GetCompletionDetailState.ts'
-import * as GetCompletionState from '../GetCompletionState/GetCompletionState.ts'
+import * as FocusKey from '../FocusKey/FocusKey.ts'
 import * as GetCompletionDetailBounds from '../GetCompletionDetailBounds/GetCompletionDetailBounds.ts'
+import * as GetCompletionState from '../GetCompletionState/GetCompletionState.ts'
+import * as WidgetId from '../WidgetId/WidgetId.ts'
 
 export const openDetails = (editor: any) => {
   const child = GetCompletionState.getCompletionState(editor)
   if (!child) {
     return editor
   }
-  const detailState = GetCompletionDetailState.getCompletionDetailState(editor)
-  if (detailState) {
-    return editor
-  }
-  const widget = CompletionDetailWidgetFactory.create()
-
-  const borderSize = 1
-  const newestState: CompletionDetailState = {
-    ...widget.newState,
-    content: 'abc',
-    ...GetCompletionDetailBounds.getCompletionDetailBounds(child, borderSize),
+  // TODO avoid closure
+  const newStateGenerator = (state: CompletionDetailState): CompletionDetailState => {
+    const borderSize = 1
+    const newestState: CompletionDetailState = {
+      ...state,
+      content: 'abc',
+      ...GetCompletionDetailBounds.getCompletionDetailBounds(child, borderSize),
+    }
+    return newestState
   }
 
-  const latestWidgets = [
-    ...editor.widgets,
-    {
-      ...widget,
-      newState: newestState,
-    },
-  ]
-  return {
-    ...editor,
-    widgets: latestWidgets,
-  }
+  return AddWidgetToEditor.addWidgetToEditor(
+    WidgetId.CompletionDetail,
+    FocusKey.CompletionDetail,
+    editor,
+    CompletionDetailWidgetFactory.create,
+    newStateGenerator
+  )
 }
--- src/parts/FocusKey/FocusKey.ts
+++ src/parts/FocusKey/FocusKey.ts
@@ -1,3 +1,4 @@
 export const ColorPicker = 41
 export const FindWidget = 16
 export const EditorCompletion = 9
+export const CompletionDetail = 999
