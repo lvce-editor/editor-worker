6a43c0130bc9d7cc1adcc8c5d4c5c26d4654ec2e
Le Vivilet <levivlet@pm.me>
2024-10-10 22:03:30 +0200

    test

--- test-integration/find-widget.test.js
+++ test-integration/find-widget.test.js
@@ -1,13 +1,24 @@
 import * as assert from 'node:assert'
 
+const id = 0
+
 export const test = async (rpc) => {
-  const state = {
-    value: '',
-    matches: new Uint32Array([0, 0]),
-    matchIndex: 0,
-  }
-  const index = 1
-  const response = await rpc.invoke('FindWidget.focusIndex', state, index)
+  await rpc.invoke('Editor.create', {
+    id,
+    content: 'a',
+    fontFamily: '',
+    fontSize: 14,
+    lineHeight: 20,
+    fontWeight: 400,
+    letterSpacing: 0.5,
+    tabSize: 2,
+    width: 100,
+    isMonospaceFont: true,
+    charWidth: 9,
+  })
+  await rpc.invoke('Editor.openFind', id)
+  await rpc.invoke('Editor.selectAll', id)
+  const response = await rpc.invoke('FindWidget.focusIndex', id, 0)
   assert.deepEqual(response, {
     value: '',
     matches: new Uint32Array([0, 0]),
