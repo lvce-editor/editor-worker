415bedb6af5db525bb59b7cc7d8f41ef4dc3ad67
Le Vivilet <levivlet@pm.me>
2024-10-10 19:54:36 +0200

    tsc

--- src/parts/EditorCommand/EditorCommandOpenFind2.ts
+++ src/parts/EditorCommand/EditorCommandOpenFind2.ts
@@ -1,12 +1,9 @@
-import * as FindWidgetFactory from '../FindWidgetFactory/FindWidgetFactory.ts'
-import * as FocusKey from '../FocusKey/FocusKey.ts'
-import * as HasWidget from '../HasWidget/HasWidget.ts'
-import { IFindWidget } from '../IFindWidget/IFindWidget.ts'
-import * as SetAdditionalFocus from '../SetAdditionalFocus/SetAdditionalFocus.ts'
-import * as WidgetId from '../WidgetId/WidgetId.ts'
-import * as FindWidgetFunctions from '../FindWidgetFunctions/FindWidgetFunctions.ts'
 import * as AddWidgetToEditor from '../AddWidgetToEditor/AddWidgetToEditor.ts'
-import { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
+import * as FindWidgetFactory from '../FindWidgetFactory/FindWidgetFactory.ts'
+import * as FindWidgetFunctions from '../FindWidgetFunctions/FindWidgetFunctions.ts'
+import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
+import * as FocusKey from '../FocusKey/FocusKey.ts'
+import * as WidgetId from '../WidgetId/WidgetId.ts'
 
 export const openFind2 = async (editor: any) => {
   const newStateGenerator = async (state: FindWidgetState): Promise<FindWidgetState> => {
