1aad5bb71286899bb58ac37b79dc785c88869f5c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-10 19:37:36 +0200

    feature: load find widget content (#219)

--- src/parts/EditorCommand/EditorCommandOpenFind2.ts
+++ src/parts/EditorCommand/EditorCommandOpenFind2.ts
@@ -4,6 +4,8 @@ import * as HasWidget from '../HasWidget/HasWidget.ts'
 import { IFindWidget } from '../IFindWidget/IFindWidget.ts'
 import * as SetAdditionalFocus from '../SetAdditionalFocus/SetAdditionalFocus.ts'
 import * as WidgetId from '../WidgetId/WidgetId.ts'
+import * as FindWidgetFunctions from '../FindWidgetFunctions/FindWidgetFunctions.ts'
+import { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 
 export const openFind2 = async (editor: any) => {
   const { widgets } = editor
@@ -11,9 +13,22 @@ export const openFind2 = async (editor: any) => {
     return editor
   }
   const findWidget: IFindWidget = FindWidgetFactory.create()
-  const newWidget = {
-    ...findWidget,
+
+  const { value, matches, matchCount, matchIndex } = await FindWidgetFunctions.loadContent(editor.uid)
+
+  const latestState: FindWidgetState = {
+    ...findWidget.newState,
+    value,
+    matches,
+    matchCount,
+    matchIndex,
   }
+
+  const newWidget: IFindWidget = {
+    ...findWidget,
+    newState: latestState,
+  }
+
   const newWidgets = [...widgets, newWidget]
   // TODO avoid side effect, apply focus shift during render
   await SetAdditionalFocus.setAdditionalFocus(FocusKey.FindWidget)
