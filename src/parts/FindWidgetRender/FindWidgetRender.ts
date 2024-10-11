9723da620a659ed93869d612026573ffc0731246
Le Vivilet <levivlet@pm.me>
2024-10-11 22:05:55 +0200

    log

--- src/parts/FindWidgetRender/FindWidgetRender.ts
+++ src/parts/FindWidgetRender/FindWidgetRender.ts
@@ -17,7 +17,6 @@ const renderValue = {
 
 const renderDetails = {
   isEqual(oldState: FindWidgetState, newState: FindWidgetState) {
-    console.log('check')
     return (
       oldState.matchIndex === newState.matchIndex &&
       oldState.matchCount === newState.matchCount &&
@@ -52,7 +51,6 @@ const renderDetails = {
       newState.matchWholeWord,
       newState.useRegularExpression
     )
-    console.log({ dom })
     return ['Viewlet.setDom2', dom]
   },
 }
