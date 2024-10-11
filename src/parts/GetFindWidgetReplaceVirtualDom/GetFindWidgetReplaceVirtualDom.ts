3dea277afacc15a82d05cc0137b4050734dfc209
Le Vivilet <levivlet@pm.me>
2024-10-11 22:17:52 +0200

    feature: move find widget find virtual dom into separate file

---
+++ src/parts/GetFindWidgetReplaceVirtualDom/GetFindWidgetReplaceVirtualDom.ts
@@ -0,0 +1,19 @@
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as EditorStrings from '../EditorStrings/EditorStrings.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
+
+export const getFindWidgetReplaceVirtualDom = (replaceExpanded: boolean) => {
+  const dom = []
+  if (replaceExpanded) {
+    dom.push(
+      {
+        type: VirtualDomElements.Div,
+        className: ClassNames.FindWidgetReplace,
+        childCount: 1,
+      },
+      text(EditorStrings.replace())
+    )
+  }
+  return dom
+}
