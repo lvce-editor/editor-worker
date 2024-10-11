3cd4e02d6994ec5acc4b793be0f292b12bdbfa9f
Le Vivilet <levivlet@pm.me>
2024-10-11 22:58:33 +0200

    update icon

--- src/parts/Icon/Icon.ts
+++ src/parts/Icon/Icon.ts
@@ -1,27 +1,29 @@
 import * as AssetDir from '../AssetDir/AssetDir.ts'
 
-export const ExtensionDefaultIcon = `${AssetDir.assetDir}/icons/extensionDefaultIcon.png`
-export const ExtensionLanguageBasics = `${AssetDir.assetDir}/icons/language-icon.svg`
-export const ExtensionTheme = `${AssetDir.assetDir}/icons/theme-icon.png`
-export const ArrowDown = 'ArrowDown'
 export const Add = 'Add'
+export const ArrowDown = 'ArrowDown'
 export const ArrowUp = 'ArrowUp'
-export const Tag = 'Tag'
 export const Blank = 'Blank'
 export const ClearAll = 'ClearAll'
 export const Close = 'Close'
 export const Cloud = 'Cloud'
 export const CollapseAll = 'CollapseAll'
 export const DebugAlt2 = 'DebugAlt2'
-export const Ellipsis = 'Ellipsis'
 export const Edit = 'Edit'
+export const Ellipsis = 'Ellipsis'
+export const ExtensionDefaultIcon = `${AssetDir.assetDir}/icons/extensionDefaultIcon.png`
+export const ExtensionLanguageBasics = `${AssetDir.assetDir}/icons/language-icon.svg`
 export const Extensions = 'Extensions'
+export const ExtensionTheme = `${AssetDir.assetDir}/icons/theme-icon.png`
 export const Files = 'Files'
-export const Refresh = 'Refresh'
+export const None = ''
 export const RecordKey = 'RecordKey'
+export const Refresh = 'Refresh'
+export const Replace = 'Replace'
+export const ReplaceAll = 'ReplaceAll'
 export const Search = 'Search'
 export const SettingsGear = 'SettingsGear'
 export const SourceControl = 'SourceControl'
 export const SplitHorizontal = 'SplitHorizontal'
+export const Tag = 'Tag'
 export const Trash = 'Trash'
-export const None = ''
