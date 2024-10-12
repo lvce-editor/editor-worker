6c705ef21fb91a66959b587c313a834577c43cba
Le Vivilet <levivlet@pm.me>
2024-10-12 19:04:51 +0200

    test

---
+++ test/GetFindWidgetFocusSelector.test.ts
@@ -0,0 +1,33 @@
+import { expect, test } from '@jest/globals'
+import * as GetFindWidgetFocusSelector from '../src/parts/GetFindWidgetFocusSelector/GetFindWidgetFocusSelector.ts'
+import * as FocusKey from '../src/parts/FocusKey/FocusKey.ts'
+
+test('find', () => {
+  const focus = FocusKey.FindWidget
+  expect(GetFindWidgetFocusSelector.getFindWidgetFocusSelector(focus)).toBe(`[name="search-value"]`)
+})
+
+test('replace', () => {
+  const focus = FocusKey.FindWidgetReplace
+  expect(GetFindWidgetFocusSelector.getFindWidgetFocusSelector(focus)).toBe('[name="replace-value"]')
+})
+
+test('toggleReplace', () => {
+  const focus = FocusKey.FindWidgetToggleReplace
+  expect(GetFindWidgetFocusSelector.getFindWidgetFocusSelector(focus)).toBe('[name="toggleReplace"]')
+})
+
+test('replaceAll', () => {
+  const focus = FocusKey.FindWidgetReplaceAll
+  expect(GetFindWidgetFocusSelector.getFindWidgetFocusSelector(focus)).toBe('[name="replaceAll"]')
+})
+
+test('close', () => {
+  const focus = FocusKey.FindWidgetClose
+  expect(GetFindWidgetFocusSelector.getFindWidgetFocusSelector(focus)).toBe('[name="close"]')
+})
+
+test('other', () => {
+  const focus = FocusKey.Empty
+  expect(GetFindWidgetFocusSelector.getFindWidgetFocusSelector(focus)).toBe('')
+})
