e891789b5e1cd7ff826f5944331d7b76b9caf39c
Le Vivilet <levivlet@pm.me>
2024-10-12 19:27:34 +0200

    test

--- test/FindRegexMatches.test.ts
+++ test/FindRegexMatches.test.ts
@@ -6,3 +6,21 @@ test('empty lines', () => {
   const regex = new RegExp('')
   expect(FindRegexMatches.findRegexMatches(lines, regex)).toEqual(new Uint32Array([]))
 })
+
+test.only('match in one line', () => {
+  const lines: readonly string[] = ['a']
+  const regex = new RegExp('a')
+  expect(FindRegexMatches.findRegexMatches(lines, regex)).toEqual(new Uint32Array([0, 0]))
+})
+
+test('two matches in one line', () => {
+  const lines: readonly string[] = ['a a']
+  const regex = new RegExp('a')
+  expect(FindRegexMatches.findRegexMatches(lines, regex)).toEqual(new Uint32Array([0, 0]))
+})
+
+test('three matches in one line', () => {
+  const lines: readonly string[] = ['a a a']
+  const regex = new RegExp('a')
+  expect(FindRegexMatches.findRegexMatches(lines, regex)).toEqual(new Uint32Array([0, 0]))
+})
