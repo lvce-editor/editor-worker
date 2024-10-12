be40c3c35011cb545f99b6b7e0d497849e97177a
Le Vivilet <levivlet@pm.me>
2024-10-12 23:47:53 +0200

    feature: simplify command implementation

--- src/parts/HandleMessage/HandleMessage.ts
+++ src/parts/HandleMessage/HandleMessage.ts
@@ -1,5 +1,5 @@
+import * as Command from '@lvce-editor/command'
 import * as Callback from '../Callback/Callback.ts'
-import * as Command from '../Command/Command.ts'
 import * as HandleJsonRpcMessage from '../JsonRpc/JsonRpc.ts'
 
 const requiresSocket = () => {
@@ -22,6 +22,6 @@ export const handleMessage = (event: any) => {
     Callback.resolve,
     preparePrettyError,
     logError,
-    requiresSocket,
+    requiresSocket
   )
 }
--- src/parts/Listen/Listen.ts
+++ src/parts/Listen/Listen.ts
@@ -1,5 +1,5 @@
+import * as Command from '@lvce-editor/command'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
-import * as CommandState from '../CommandState/CommandState.ts'
 import * as EditorCompletionWidget from '../EditorCompletionWidget/EditorCompletionWidget.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 import * as IpcChild from '../IpcChild/IpcChild.ts'
@@ -9,7 +9,7 @@ import * as WidgetId from '../WidgetId/WidgetId.ts'
 import * as WidgetModules from '../WidgetModules/WidgetModules.ts'
 
 export const listen = async () => {
-  CommandState.registerCommands(CommandMap.commandMap)
+  Command.register(CommandMap.commandMap)
   WidgetModules.register(WidgetId.Completion, EditorCompletionWidget)
   const ipc = await IpcChild.listen({ method: IpcChildType.Auto() })
   HandleIpc.handleIpc(ipc)
