7bad0863272b42faa8b5bae5ed647a7f3adb122e
Le Vivilet <levivlet@pm.me>
2024-10-13 09:25:27 +0200

    Merge remote-tracking branch 'origin/main' into feature/find-sync

--- src/parts/FindWidgetFocusCloseButton/FindWidgetFocusCloseButton.ts
+++ src/parts/FindWidgetFocusCloseButton/FindWidgetFocusCloseButton.ts
@@ -2,6 +2,6 @@ import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 import * as FocusKey from '../FocusKey/FocusKey.ts'
 import * as SetFindWidgetFocus from '../SetFindWidgetFocus/SetFindWidgetFocus.ts'
 
-export const focusCloseButton = (state: FindWidgetState): Promise<FindWidgetState> => {
+export const focusCloseButton = async (state: FindWidgetState): Promise<FindWidgetState> => {
   return SetFindWidgetFocus.setFindWidgetFocus(state, FocusKey.FocusFindWidgetCloseButton)
 }
