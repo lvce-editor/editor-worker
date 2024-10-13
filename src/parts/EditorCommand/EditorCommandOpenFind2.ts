a741d098e5fc610d5aa2b33d707d4e936f4d11df
Le Vivilet <levivlet@pm.me>
2024-10-13 10:47:22 +0200

    feature: move find widget position into separate file

--- src/parts/EditorCommand/EditorCommandOpenFind2.ts
+++ src/parts/EditorCommand/EditorCommandOpenFind2.ts
@@ -4,12 +4,13 @@ import * as FindWidgetFunctions from '../FindWidgetFunctions/FindWidgetFunctions
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 import * as FocusKey from '../FocusKey/FocusKey.ts'
 import * as FocusSource from '../FocusSource/FocusSource.ts'
+import * as GetFindWidgetPosition from '../GetFindWidgetPosition/GetFindWidgetPosition.ts'
 import * as WidgetId from '../WidgetId/WidgetId.ts'
 
 export const openFind2 = async (editor: any) => {
   const newStateGenerator = async (state: FindWidgetState): Promise<FindWidgetState> => {
     const { value, matches, matchCount, matchIndex } = await FindWidgetFunctions.loadContent(editor.uid)
-    const { x, y, width, height } = FindWidgetFunctions.getPosition(editor)
+    const { x, y, width, height } = GetFindWidgetPosition.getFindWidgetPosition(editor)
     const latestState: FindWidgetState = {
       ...state,
       value,
