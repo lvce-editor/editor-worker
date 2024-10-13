2300a03990fd8b842c9c2aee26ac814e53ed78a7
Le Vivilet <levivlet@pm.me>
2024-10-13 11:17:11 +0200

    fix error

--- src/parts/SetFocus/SetFocus.ts
+++ src/parts/SetFocus/SetFocus.ts
@@ -1,5 +1,8 @@
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const setFocus = async (focusKey: number) => {
+  if (!focusKey) {
+    return
+  }
   await RendererWorker.invoke('Focus.setFocus', focusKey)
 }
