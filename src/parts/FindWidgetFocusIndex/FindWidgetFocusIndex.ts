df047dc9d35733cef95d6f7e0ec1a45f786d519c
Le Vivilet <levivlet@pm.me>
2024-10-13 11:15:29 +0200

    feature: render editor selections and find widget at the same time for improved performance

--- src/parts/FindWidgetFocusIndex/FindWidgetFocusIndex.ts
+++ src/parts/FindWidgetFocusIndex/FindWidgetFocusIndex.ts
@@ -1,49 +1,82 @@
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
-import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+import * as GetFindState from '../GetFindState/GetFindState.ts'
+import * as WidgetId from '../WidgetId/WidgetId.ts'
 
+const isFind = (widget: any) => {
+  return widget.id === WidgetId.Find
+}
 // TODO don't call renderer worker, set editor state
 // TODO this function should be synchronous
-export const focusIndex = async (state: FindWidgetState, index: number): Promise<FindWidgetState> => {
-  const { value, matches, matchIndex } = state
-  if (index === matchIndex) {
-    return state
+export const focusIndex = (editor: any, index: number): any => {
+  const findState = GetFindState.getFindState(editor)
+  if (!findState) {
+    return editor
   }
+  const { value, matches, matchIndex } = findState
+  if (index === matchIndex) {
+    return editor
+  }
+  const { widgets } = editor
+  const childIndex = widgets.findIndex(isFind)
+  const childWidget = widgets[childIndex]
+
   // TODO find next match and highlight it
   const matchRowIndex = matches[index * 2]
   const matchColumnIndex = matches[index * 2 + 1]
-  // @ts-ignore
   const newSelections = new Uint32Array([matchRowIndex, matchColumnIndex, matchRowIndex, matchColumnIndex + value.length])
-  // TODO set selections synchronously and render input match index,
-  // input value and new selections at the same time
-  // TODO
-  await RendererWorker.invoke('Editor.setSelections', newSelections)
-  return {
-    ...state,
+  const newState: FindWidgetState = {
+    ...findState,
     matchIndex: index,
   }
+  const newWidget = {
+    ...childWidget,
+    newState,
+  }
+  const newWidgets = [...widgets.slice(0, childIndex), newWidget, ...widgets.slice(childIndex + 1)]
+  return {
+    ...editor,
+    selections: newSelections,
+    widgets: newWidgets,
+  }
 }
 
-export const focusFirst = (state: FindWidgetState) => {
-  return focusIndex(state, 0)
+export const focusFirst = (editor: any): any => {
+  const findState = GetFindState.getFindState(editor)
+  if (!findState) {
+    return editor
+  }
+  return focusIndex(editor, 0)
 }
 
-export const focusLast = (state: FindWidgetState) => {
-  const { matchCount } = state
-  return focusIndex(state, matchCount - 1)
+export const focusLast = (editor: any): any => {
+  const findState = GetFindState.getFindState(editor)
+  if (!findState) {
+    return editor
+  }
+  const { matchCount } = findState
+  return focusIndex(editor, matchCount - 1)
 }
 
-export const focusNext = (state: FindWidgetState) => {
-  const { matchIndex, matchCount } = state
+export const focusNext = (editor: any): any => {
+  const findState = GetFindState.getFindState(editor)
+  if (!findState) {
+    return editor
+  }
+  const { matchIndex, matchCount } = findState
   if (matchIndex === matchCount - 1) {
-    return focusFirst(state)
+    return focusFirst(editor)
   }
-  return focusIndex(state, matchIndex + 1)
+  return focusIndex(editor, matchIndex + 1)
 }
 
-export const focusPrevious = (state: FindWidgetState) => {
-  const { matchIndex } = state
-  if (matchIndex === 0) {
-    return focusLast(state)
+export const focusPrevious = (editor: any): any => {
+  const findState = GetFindState.getFindState(editor)
+  if (!findState) {
+    return editor
   }
-  return focusIndex(state, matchIndex - 1)
+  const { matchIndex } = findState
+  if (matchIndex === 0) {
+    return focusLast(editor)
+  }
+  return focusIndex(editor, matchIndex - 1)
 }
