fa8c7a984c8c3bb6aa78f5d3a8d5c39848d96e9a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-14 20:21:04 +0200

    feature: add function to create hover widget (#281)

---
+++ src/parts/HoverState/HoverState.ts
@@ -0,0 +1,8 @@
+export interface HoverState {
+  readonly x: number
+  readonly y: number
+  readonly width: number
+  readonly height: number
+  readonly uid: number
+  readonly content: string
+}
---
+++ src/parts/HoverWidgetFactory/HoverWidgetFactory.ts
@@ -0,0 +1,27 @@
+import type { HoverWidget } from '../HoverWidget/HoverWidget.ts'
+import * as Id from '../Id/Id.ts'
+import * as WidgetId from '../WidgetId/WidgetId.ts'
+
+export const create = (): HoverWidget => {
+  const uid = Id.create()
+  const widget: HoverWidget = {
+    id: WidgetId.Hover,
+    oldState: {
+      uid: uid,
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+      content: '',
+    },
+    newState: {
+      uid: uid,
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+      content: '',
+    },
+  }
+  return widget
+}
