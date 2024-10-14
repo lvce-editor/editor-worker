31814e1113ab742bf617619a9ecf74cd2cae38cd
Le Vivilet <levivlet@pm.me>
2024-10-14 20:13:17 +0200

    feature: add function to create hover widget

---
+++ src/parts/HoverWidget/HoverWidget.ts
@@ -0,0 +1,4 @@
+import type { HoverState } from '../ColorPickerState/ColorPickerState.ts'
+import type { Widget } from '../Widget/Widget.ts'
+
+export interface HoverWidget extends Widget<HoverState> {}
