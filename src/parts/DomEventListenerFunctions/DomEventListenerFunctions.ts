fbc9465bcd2e298e52416072e745bda02548d1ab
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-17 19:42:12 +0200

    feature: add source action dom functions (#287)

--- src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts
+++ src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts
@@ -1,3 +1,5 @@
 export const HandlePointerDown = 'handlePointerDown'
 export const HandleSashPointerDown = 'handleSashPointerDown'
 export const HandleClose = 'handleClose'
+export const HandleFocusIn = 'handleFocusIn'
+export const HandleClick = 'handleClick'
