361edbe6f137c3334c32ac72566313effa7b19ff
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-17 19:59:50 +0200

    feature: add source action widget factory (#291)

---
+++ src/parts/SourceActionWidgetFactory/SourceActionWidgetFactory.ts
@@ -0,0 +1,31 @@
+import * as Id from '../Id/Id.ts'
+import type { SourceActionWidget } from '../SourceActionWidget/SourceActionWidget.ts'
+import * as WidgetId from '../WidgetId/WidgetId.ts'
+
+export const create = (): SourceActionWidget => {
+  const completionUid = Id.create()
+  const widget: SourceActionWidget = {
+    id: WidgetId.SourceAction,
+    oldState: {
+      uid: completionUid,
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+      focusedIndex: 0,
+      sourceActions: [],
+      maxHeight: 0,
+    },
+    newState: {
+      uid: completionUid,
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+      focusedIndex: 0,
+      sourceActions: [],
+      maxHeight: 0,
+    },
+  }
+  return widget
+}
