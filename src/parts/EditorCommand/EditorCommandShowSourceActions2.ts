1835c418994a03f7ac38e678b9c541b477c023b6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-17 20:09:07 +0200

    feature: add command to open editor worker source actions (#293)

---
+++ src/parts/EditorCommand/EditorCommandShowSourceActions2.ts
@@ -0,0 +1,20 @@
+import * as AddWidgetToEditor from '../AddWidgetToEditor/AddWidgetToEditor.ts'
+import * as FocusKey from '../FocusKey/FocusKey.ts'
+import * as LoadSourceActions from '../LoadSourceActions/LoadSourceActions.ts'
+import type { SourceActionState } from '../SourceActionState/SourceActionState.ts'
+import * as SourceActionWidgetFactory from '../SourceActionWidgetFactory/SourceActionWidgetFactory.ts'
+import * as WidgetId from '../WidgetId/WidgetId.ts'
+
+export const showSourceActions = async (editor: any) => {
+  const uid = editor.uid
+  const newStateGenerator = (state: SourceActionState): Promise<SourceActionState> => {
+    return LoadSourceActions.loadSourceActions(uid, state)
+  }
+  return AddWidgetToEditor.addWidgetToEditor(
+    WidgetId.SourceAction,
+    FocusKey.SourceActions,
+    editor,
+    SourceActionWidgetFactory.create,
+    newStateGenerator
+  )
+}
