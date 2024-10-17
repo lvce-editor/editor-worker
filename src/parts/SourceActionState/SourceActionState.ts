6f1aeafac757258d170f1020c1e548cdf9c79fc1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-17 19:46:13 +0200

    feature: add source action render function (#288)

---
+++ src/parts/SourceActionState/SourceActionState.ts
@@ -0,0 +1,8 @@
+export interface SourceActionState {
+  readonly sourceActions: readonly any[]
+  readonly focusedIndex: number
+  readonly x: number
+  readonly y: number
+  readonly width: number
+  readonly height: number
+}
