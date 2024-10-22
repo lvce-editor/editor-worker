013150ed53411230f9e28caed06fe62c36dc5caf
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-22 20:16:30 +0200

    feature: move completion detail widget together with completion widget (#321)

--- src/parts/EditorCompletionDetailWidget/EditorCompletionDetailWidget.ts
+++ src/parts/EditorCompletionDetailWidget/EditorCompletionDetailWidget.ts
@@ -1,4 +1,5 @@
 import * as AddWidget from '../AddWidget/AddWidget.ts'
+import type { CompletionDetailState } from '../CompletionDetailState/CompletionDetailState.ts'
 import type { CompletionDetailWidget } from '../CompletionDetailWidget/CompletionDetailWidget.ts'
 import * as EditorCompletionDetailRender from '../EditorCompletionDetailRender/EditorCompletionDetailRender.ts'
 import * as GetCompletionState from '../GetCompletionState/GetCompletionState.ts'
@@ -25,13 +26,26 @@ export const add = (widget: CompletionDetailWidget) => {
 
 export const remove = RemoveWidget.removeWidget
 
-export const handleEditorType = (editor: any, state: any) => {
+export const handleEditorType = (editor: any, state: CompletionDetailState): CompletionDetailState => {
   const completionState = GetCompletionState.getCompletionState(editor)
   if (!completionState) {
     return editor
   }
   const { x } = GetPositionAtCursor.getPositionAtCursor(editor)
-  const detailX = x + completionState.width
+  const detailX = x + completionState.width - state.borderSize
+  return {
+    ...state,
+    x: detailX,
+  }
+}
+
+export const handleEditorDeleteLeft = (editor: any, state: CompletionDetailState): CompletionDetailState => {
+  const completionState = GetCompletionState.getCompletionState(editor)
+  if (!completionState) {
+    return editor
+  }
+  const { x } = GetPositionAtCursor.getPositionAtCursor(editor)
+  const detailX = x + completionState.width - state.borderSize
   return {
     ...state,
     x: detailX,
