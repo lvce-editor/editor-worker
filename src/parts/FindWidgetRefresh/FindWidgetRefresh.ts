195c82bc967b075e7a97bab3b77264726c5a95c2
Le Vivilet <levivlet@pm.me>
2024-10-23 00:28:38 +0200

    feature: update find widget match text after replace operation

--- src/parts/FindWidgetRefresh/FindWidgetRefresh.ts
+++ src/parts/FindWidgetRefresh/FindWidgetRefresh.ts
@@ -1,21 +1,11 @@
-import * as FindMatchesCaseInsensitive from '../FindMatchesCaseInsensitive/FindMatchesCaseInsensitive.ts'
+import * as FindWidgetRefreshWithEditor from '../FindWidgetRefreshWithEditor/FindWidgetRefreshWithEditor.ts'
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 import * as GetEditor from '../GetEditor/GetEditor.ts'
-import * as GetMatchCount from '../GetMatchCount/GetMatchCount.ts'
 
 export const refresh = (state: FindWidgetState, value = state.value): FindWidgetState => {
   // TODO get focused editor
   const { editorUid } = state
   // highlight locations that match value
   const editor = GetEditor.getEditor(editorUid)
-  const { lines } = editor
-  const matches = FindMatchesCaseInsensitive.findMatchesCaseInsensitive(lines, value)
-  const matchCount = GetMatchCount.getMatchCount(matches)
-  return {
-    ...state,
-    matches,
-    matchIndex: 0,
-    matchCount,
-    value,
-  }
+  return FindWidgetRefreshWithEditor.refresh(editor, state, value)
 }
