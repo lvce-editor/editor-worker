c96e2f8bffae2deeae6f35691e154d5fd827b8c8
Le Vivilet <levivlet@pm.me>
2024-10-22 20:14:55 +0200

    feature: move completion detail widget together with completion widget

--- src/parts/CompletionDetailState/CompletionDetailState.ts
+++ src/parts/CompletionDetailState/CompletionDetailState.ts
@@ -3,4 +3,5 @@ import type { Rectangle } from '../Rectangle/Rectangle.ts'
 export interface CompletionDetailState extends Rectangle {
   readonly content: string
   readonly uid: number
+  readonly borderSize: number
 }
