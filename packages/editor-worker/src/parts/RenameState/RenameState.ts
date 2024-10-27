c3ce4fc704949bae6dab80a5757d91710b797d7b
Le Vivilet <levivlet@pm.me>
2024-10-27 20:19:08 +0100

    feature: add rename widget

---
+++ packages/editor-worker/src/parts/RenameState/RenameState.ts
@@ -0,0 +1,8 @@
+export interface RenameState {
+  readonly maxHeight: number
+  readonly uid: number
+  readonly focusedIndex: number
+  readonly focused: boolean
+  readonly oldValue: string
+  readonly newValue: string
+}
