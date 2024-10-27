5341caecc19294a139657f635755b031efce5675
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-27 21:15:22 +0100

    feature: set word at cursor as value for rename input (#351)
    * feature: set word at cursor as input value for rename box
    
    * set value

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenRename.ts
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenRename.ts
@@ -1,21 +1,29 @@
 import * as AddWidgetToEditor from '../AddWidgetToEditor/AddWidgetToEditor.ts'
+import * as EditorCommandGetWordAt from '../EditorCommand/EditorCommandGetWordAt.ts'
 import * as FocusKey from '../FocusKey/FocusKey.ts'
+import * as GetPositionAtCursor from '../GetPositionAtCursor/GetPositionAtCursor.ts'
 import * as GetRenamePosition from '../GetRenamePosition/GetRenamePosition.ts'
 import type { RenameState } from '../RenameState/RenameState.ts'
 import * as RenameWidgetFactory from '../RenameWidgetFactory/RenameWidgetFactory.ts'
 import * as WidgetId from '../WidgetId/WidgetId.ts'
 
 export const openRename = async (editor: any) => {
+  const { rowIndex, columnIndex } = GetPositionAtCursor.getPositionAtCursor(editor)
+  const { word } = EditorCommandGetWordAt.getWordAt(editor, rowIndex, columnIndex)
+  if (!word) {
+    return editor
+  }
   const newStateGenerator = async (state: RenameState): Promise<RenameState> => {
     // TODO query if can rename from extension host
     const { x, y, width, height } = GetRenamePosition.getRenamePosition(editor)
-
     const latestState: RenameState = {
       ...state,
       x,
       y,
       width,
       height,
+      oldValue: word,
+      newValue: word,
     }
     return latestState
   }
