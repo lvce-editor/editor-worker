193c30b818504e193d698cb090e0265535e272d6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-27 21:08:21 +0100

    feature: improve rename position (#350)

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenRename.ts
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenRename.ts
@@ -1,21 +1,21 @@
 import * as AddWidgetToEditor from '../AddWidgetToEditor/AddWidgetToEditor.ts'
 import * as FocusKey from '../FocusKey/FocusKey.ts'
+import * as GetRenamePosition from '../GetRenamePosition/GetRenamePosition.ts'
 import type { RenameState } from '../RenameState/RenameState.ts'
 import * as RenameWidgetFactory from '../RenameWidgetFactory/RenameWidgetFactory.ts'
-import * as GetPositionAtCursor from '../GetPositionAtCursor/GetPositionAtCursor.ts'
 import * as WidgetId from '../WidgetId/WidgetId.ts'
 
 export const openRename = async (editor: any) => {
   const newStateGenerator = async (state: RenameState): Promise<RenameState> => {
     // TODO query if can rename from extension host
-    const { x, y } = GetPositionAtCursor.getPositionAtCursor(editor)
+    const { x, y, width, height } = GetRenamePosition.getRenamePosition(editor)
 
     const latestState: RenameState = {
       ...state,
       x,
       y,
-      width: 200,
-      height: 30,
+      width,
+      height,
     }
     return latestState
   }
