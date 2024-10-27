33817f9dcb4c924aeb13a5a494c603d0ea48ff03
Le Vivilet <levivlet@pm.me>
2024-10-27 09:00:46 +0100

    feature: add handleTab command

---
+++ src/parts/HandleTab/HandleTab.ts
@@ -0,0 +1,9 @@
+export const handleTab = (editor: any) => {
+  // TODO
+  // 1. if completion is open, insert completion item
+  // 2. try to execute tab completion provider
+  // 3. enter tab or two spaces
+
+  console.log('tab')
+  return editor
+}
