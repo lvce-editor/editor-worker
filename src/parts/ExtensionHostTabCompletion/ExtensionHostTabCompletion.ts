dd530e5f68de3d06c3bfc0c0ef5fe39ada2d2a0c
Le Vivilet <levivlet@pm.me>
2024-10-27 09:12:49 +0100

    feature: apply tab completion result

--- src/parts/ExtensionHostTabCompletion/ExtensionHostTabCompletion.ts
+++ src/parts/ExtensionHostTabCompletion/ExtensionHostTabCompletion.ts
@@ -1,12 +1,13 @@
 import * as ExtensionHostActivationEvent from '../ExtensionHostActivationEvent/ExtensionHostActivationEvent.ts'
 import * as ExtensionHostCommandType from '../ExtensionHostCommandType/ExtensionHostCommandType.ts'
 import * as ExtensionHostEditor from '../ExtensionHostEditor/ExtensionHostEditor.ts'
+import type { TabCompletionResult } from '../TabCompletionResult/TabCompletionResult.ts'
 
 const combineResults = (results: any) => {
   return results[0]
 }
 
-export const executeTabCompletionProvider = (editor: any, offset: number) => {
+export const executeTabCompletionProvider = (editor: any, offset: number): Promise<TabCompletionResult | undefined> => {
   return ExtensionHostEditor.execute({
     editor,
     event: ExtensionHostActivationEvent.OnTabCompletion,
