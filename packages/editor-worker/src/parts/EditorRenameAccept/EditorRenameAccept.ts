b81ed2dc0f7c2bb5a4d4ddf76919c13ef64f2290
Le Vivilet <levivlet@pm.me>
2024-10-29 21:33:52 +0100

    feature: when accepting rename, query edits from extension host worker

--- packages/editor-worker/src/parts/EditorRenameAccept/EditorRenameAccept.ts
+++ packages/editor-worker/src/parts/EditorRenameAccept/EditorRenameAccept.ts
@@ -1,10 +1,22 @@
+import * as ExtensionHostRename from '../ExtensionHostRename/ExtensionHostRename.ts'
+import * as GetOffsetAtCursor from '../GetOffsetAtCursor/GetOffsetAtCursor.ts'
+import * as GetRenameState from '../GetRenameState/GetRenameState.ts'
 import * as RemoveEditorWidget from '../RemoveEditorWidget/RemoveEditorWidget.ts'
 import * as WidgetId from '../WidgetId/WidgetId.ts'
 
-export const accept = (editor: any): any => {
+export const accept = async (editor: any): Promise<any> => {
+  const child = GetRenameState.getRenameState(editor)
+  if (!child) {
+    return editor
+  }
+
   const { widgets } = editor
   const newWidgets = RemoveEditorWidget.removeEditorWidget(widgets, WidgetId.Rename)
   // TODO
+  const offset = GetOffsetAtCursor.getOffsetAtCursor(editor)
+
+  const result = await ExtensionHostRename.executeRenameProvider(editor, offset, child.newValue)
+  console.log({ result })
   // 1. ask extension host for rename edits
   // 2. apply rename edit across editor (and whole workspace)
   // 3. close rename widget
