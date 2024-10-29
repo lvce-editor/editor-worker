ee7406f5a8afeb1e6295af8a388dedce42480d38
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-29 21:10:23 +0100

    feature: improve acceptRename command (#372)

--- packages/editor-worker/src/parts/EditorRenameAccept/EditorRenameAccept.ts
+++ packages/editor-worker/src/parts/EditorRenameAccept/EditorRenameAccept.ts
@@ -1,9 +1,16 @@
-import * as EditorCommandCloseRename from '../EditorCommand/EditorCommandCloseRename.ts'
+import * as RemoveEditorWidget from '../RemoveEditorWidget/RemoveEditorWidget.ts'
+import * as WidgetId from '../WidgetId/WidgetId.ts'
 
 export const accept = (editor: any): any => {
+  const { widgets } = editor
+  const newWidgets = RemoveEditorWidget.removeEditorWidget(widgets, WidgetId.Rename)
   // TODO
   // 1. ask extension host for rename edits
   // 2. apply rename edit across editor (and whole workspace)
   // 3. close rename widget
-  return EditorCommandCloseRename.closeRename(editor)
+  return {
+    ...editor,
+    focused: true,
+    widgets: newWidgets,
+  }
 }
