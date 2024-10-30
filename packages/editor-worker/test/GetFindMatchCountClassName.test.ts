8426b4ae90daecbde24016170cf28204e571a2b8
Le Vivilet <levivlet@pm.me>
2024-10-30 22:28:08 +0100

    test

--- packages/editor-worker/test/GetFindMatchCountClassName.test.ts
+++ packages/editor-worker/test/GetFindMatchCountClassName.test.ts
@@ -4,17 +4,17 @@ import * as GetFindMatchCountClassName from '../src/parts/GetFindMatchCountClass
 test('empty value, no results', () => {
   const matchCount = 0
   const value = ''
-  expect(GetFindMatchCountClassName.getFindMatchCountClassName(matchCount, value)).toBe('FindMatchCount')
+  expect(GetFindMatchCountClassName.getFindMatchCountClassName(matchCount, value)).toBe('FindWidgetMatchCount')
 })
 
 test('value, no results', () => {
   const matchCount = 0
   const value = 'a'
-  expect(GetFindMatchCountClassName.getFindMatchCountClassName(matchCount, value)).toBe('FindMatchCount FindMatchCountEmpty')
+  expect(GetFindMatchCountClassName.getFindMatchCountClassName(matchCount, value)).toBe('FindWidgetMatchCount FindWidgetMatchCountEmpty')
 })
 
 test('value, results', () => {
   const matchCount = 1
   const value = 'a'
-  expect(GetFindMatchCountClassName.getFindMatchCountClassName(matchCount, value)).toBe('FindMatchCount')
+  expect(GetFindMatchCountClassName.getFindMatchCountClassName(matchCount, value)).toBe('FindWidgetMatchCount')
 })
