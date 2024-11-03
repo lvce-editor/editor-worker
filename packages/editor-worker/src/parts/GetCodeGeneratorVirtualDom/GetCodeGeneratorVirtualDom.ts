38640ec03170c0f231e5eb36608b67b5372ec8cd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-03 19:36:19 +0100

    feature: add input element to code generator widget (#411)

--- packages/editor-worker/src/parts/GetCodeGeneratorVirtualDom/GetCodeGeneratorVirtualDom.ts
+++ packages/editor-worker/src/parts/GetCodeGeneratorVirtualDom/GetCodeGeneratorVirtualDom.ts
@@ -2,6 +2,7 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import type { CodeGeneratorState } from '../CodeGeneratorState/CodeGeneratorState.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
 export const getCodeGeneratorVirtualDom = (state: CodeGeneratorState): readonly VirtualDomNode[] => {
@@ -9,7 +10,13 @@ export const getCodeGeneratorVirtualDom = (state: CodeGeneratorState): readonly 
     {
       type: VirtualDomElements.Div,
       className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.CodeGeneratorWidget),
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Input,
+      className: 'CodeGeneratorInput',
       childCount: 0,
     },
+    text('Escape to close'),
   ]
 }
