1db36b43fff8ac18b1f48ca78b725c8e5d5fa8f4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-03 19:34:04 +0100

    feature: add initial bounds to code generator widget (#410)
    * feature: add initial bounds to code generator widget
    
    * tsc

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenCodeGenerator.ts
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenCodeGenerator.ts
@@ -1,5 +1,5 @@
 import * as AddWidgetToEditor from '../AddWidgetToEditor/AddWidgetToEditor.ts'
-import { CodeGeneratorState } from '../CodeGeneratorState/CodeGeneratorState.ts'
+import type { CodeGeneratorState } from '../CodeGeneratorState/CodeGeneratorState.ts'
 import * as CodeGeneratorWidgetFactory from '../CodeGeneratorWidgetFactory/CodeGeneratorWidgetFactory.ts'
 import * as FocusKey from '../FocusKey/FocusKey.ts'
 import * as WidgetId from '../WidgetId/WidgetId.ts'
@@ -7,11 +7,15 @@ import * as WidgetId from '../WidgetId/WidgetId.ts'
 const newStateGenerator = async (state: CodeGeneratorState): Promise<CodeGeneratorState> => {
   const latestState: CodeGeneratorState = {
     ...state,
+    x: 100,
+    y: 100,
+    width: 100,
+    height: 25,
   }
   return latestState
 }
 
-export const openCodeGenerator = async (editor: any) => {
+export const openCodeGenerator = async (editor: any): Promise<any> => {
   const fullFocus = true
   return AddWidgetToEditor.addWidgetToEditor(
     WidgetId.CodeGenerator,
