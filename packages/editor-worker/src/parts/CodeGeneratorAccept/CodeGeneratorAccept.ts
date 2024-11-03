e9e2413ff4e57248c7ddf8ca7ddb80c11d8d19b4
Le Vivilet <levivlet@pm.me>
2024-11-03 22:21:24 +0100

    feature: add code generator accept command

---
+++ packages/editor-worker/src/parts/CodeGeneratorAccept/CodeGeneratorAccept.ts
@@ -0,0 +1,6 @@
+import type { CodeGeneratorState } from '../CodeGeneratorState/CodeGeneratorState.ts'
+
+export const codeGeneratorAccept = (state: CodeGeneratorState): CodeGeneratorState => {
+  // TODO close code generator widget
+  return state
+}
