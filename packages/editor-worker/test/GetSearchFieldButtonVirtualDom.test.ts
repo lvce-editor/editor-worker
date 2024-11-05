694e3cf0be1ec9c112149b74715e07b26538079d
Le Vivilet <levivlet@pm.me>
2024-11-05 21:52:26 +0100

    test

--- packages/editor-worker/test/GetSearchFieldButtonVirtualDom.test.ts
+++ packages/editor-worker/test/GetSearchFieldButtonVirtualDom.test.ts
@@ -14,7 +14,7 @@ test('getIconVirtualDom', () => {
   expect(dom).toEqual([
     {
       type: VirtualDomElements.Div,
-      className: 'SearchFieldButton ',
+      className: 'SearchFieldButton',
       title: 'Test',
       role: AriaRoles.CheckBox,
       ariaChecked: false,
