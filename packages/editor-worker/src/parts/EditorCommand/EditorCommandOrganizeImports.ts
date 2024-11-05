aa08182a1ea9a3be0130e119bc2eeba17708f7ce
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-05 22:23:04 +0100

    feature: add getDocumentEdits test (#439)

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandOrganizeImports.ts
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandOrganizeImports.ts
@@ -3,7 +3,6 @@ import * as ApplyDocumentEdits from './EditorCommandApplyDocumentEdits.ts'
 
 export const organizeImports = async (editor: any): Promise<any> => {
   const edits = await GetOrganizeImportEdits.getOrganizeImportEdits(editor)
-  // console.log({edits})
-  // const positionBasedEdits = ToPositionBasedEdits.toPositionBasedEdits(editor, edits)
+  console.log({ edits })
   return ApplyDocumentEdits.applyDocumentEdits(editor, edits)
 }
---
+++ packages/editor-worker/test/GetDocumentEdits.test.ts
@@ -0,0 +1,67 @@
+import { expect, test } from '@jest/globals'
+import * as GetDocumentEdits from '../src/parts/GetDocumentEdits/GetDocumentEdits.ts'
+import { OffsetBasedEdit } from '../src/parts/OffsetBasedEdit/OffsetBasedEdit.ts'
+
+test('multiple edits', () => {
+  const edits: readonly OffsetBasedEdit[] = [
+    {
+      startOffset: 0,
+      endOffset: 11,
+      inserted: "import 'a'\nimport 'b'\n",
+    },
+    {
+      startOffset: 11,
+      endOffset: 22,
+      inserted: '',
+    },
+    {
+      startOffset: 38,
+      endOffset: 47,
+      inserted: 'export { }\n',
+    },
+  ]
+  const editor = {
+    lines: [`import 'b'`, `import 'a'`, ``, `let   x = 1`, ``, ``, ``, `export {}`],
+  }
+  expect(GetDocumentEdits.getDocumentEdits(editor, edits)).toEqual([
+    {
+      deleted: ["import 'b'", ''],
+      end: {
+        columnIndex: 0,
+        rowIndex: 1,
+      },
+      inserted: ["import 'a'", "import 'b'", ''],
+      origin: 'format',
+      start: {
+        columnIndex: 0,
+        rowIndex: 0,
+      },
+    },
+    {
+      deleted: ["import 'a'", ''],
+      end: {
+        columnIndex: 0,
+        rowIndex: 2,
+      },
+      inserted: [''],
+      origin: 'format',
+      start: {
+        columnIndex: 0,
+        rowIndex: 1,
+      },
+    },
+    {
+      deleted: ['export {}'],
+      end: {
+        columnIndex: 9,
+        rowIndex: 7,
+      },
+      inserted: ['export { }', ''],
+      origin: 'format',
+      start: {
+        columnIndex: 0,
+        rowIndex: 7,
+      },
+    },
+  ])
+})
