9a94c18e89cc4de6f25072337c58ecc9e10197ad
Le Vivilet <levivlet@pm.me>
2024-11-05 22:17:32 +0100

    feature: move getDocumentEdits into separate file

---
+++ packages/editor-worker/src/parts/GetDocumentEdits/GetDocumentEdits.ts
@@ -0,0 +1,28 @@
+import * as EditOrigin from '../EditOrigin/EditOrigin.ts'
+import type { OffsetBasedEdit } from '../OffsetBasedEdit/OffsetBasedEdit.ts'
+import * as SplitLines from '../SplitLines/SplitLines.ts'
+import * as TextDocument from '../TextDocument/TextDocument.ts'
+
+export const getDocumentEdits = (editor: any, edits: OffsetBasedEdit[]): readonly any[] => {
+  const documentEdits: any[] = []
+  for (const edit of edits) {
+    const start = TextDocument.positionAt(editor, edit.startOffset)
+    const end = TextDocument.positionAt(editor, edit.endOffset)
+    const deleted = TextDocument.getSelectionText(editor, {
+      start,
+      end,
+    })
+    const documentEdit = {
+      start,
+      end,
+      inserted: SplitLines.splitLines(edit.inserted),
+      deleted,
+      origin: EditOrigin.Format,
+    }
+    if (documentEdit.inserted.length === 0) {
+      documentEdit.inserted = ['']
+    }
+    documentEdits.push(documentEdit)
+  }
+  return documentEdits
+}
