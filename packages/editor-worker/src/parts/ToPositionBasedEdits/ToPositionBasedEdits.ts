fbfc491ef6a8348e7e4d8ccc0cead44b5362d894
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-05 22:03:06 +0100

    feature: prepare to convert offset based organizeImport edits to position based edits (#436)

---
+++ packages/editor-worker/src/parts/ToPositionBasedEdits/ToPositionBasedEdits.ts
@@ -0,0 +1,6 @@
+import type { OffsetBasedEdit } from '../OffsetBasedEdit/OffsetBasedEdit.ts'
+
+export const toPositionBasedEdits = (editor: any, offsetBasedEdits: readonly OffsetBasedEdit[]): readonly any[] => {
+  const newEdits: any[] = []
+  return newEdits
+}
