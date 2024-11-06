2b2b5a2ee1ad674c663ed897bdf198c271a253ba
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-06 21:37:10 +0100

    feature: rerender editor when updating diagnostics (#446)

--- packages/editor-worker/src/parts/UpdateDiagnostics/UpdateDiagnostics.ts
+++ packages/editor-worker/src/parts/UpdateDiagnostics/UpdateDiagnostics.ts
@@ -1,13 +1,25 @@
 import * as EditorState from '../Editors/Editors.ts'
 import * as ExtensionHostDiagnostic from '../ExtensionHostDiagnostic/ExtensionHostDiagnostic.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const updateDiagnostics = async (uid: number): Promise<void> => {
-  // TODO handle error
-  // TODO handle race condition
-  const { newState } = EditorState.get(uid)
-  // TODO request diagnostics from extension host
-  const diagnostics = await ExtensionHostDiagnostic.executeDiagnosticProvider(newState)
-  console.log({ diagnostics })
-  // TODO inform renderer worker to rerender?
-  console.log({ newState })
+  try {
+    // TODO handle error
+    // TODO handle race condition
+    const { newState } = EditorState.get(uid)
+    // TODO request diagnostics from extension host
+    const diagnostics = await ExtensionHostDiagnostic.executeDiagnosticProvider(newState)
+    const latest = EditorState.get(uid)
+    if (!latest) {
+      return
+    }
+    const newEditor = {
+      ...latest.newstate,
+      diagnostics,
+    }
+    EditorState.set(uid, latest.oldState, newEditor)
+    await RendererWorker.invoke('Editor.rerender', uid)
+  } catch (error) {
+    console.error(`Failed to updated diagnostics: ${error}`)
+  }
 }
