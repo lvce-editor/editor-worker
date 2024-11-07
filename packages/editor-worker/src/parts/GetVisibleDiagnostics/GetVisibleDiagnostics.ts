f07199bce2c8fc79123caaf1beba0f78ca642919
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-07 21:44:51 +0100

    feature: compute more accurate position for diagnostics (#459)
    * feature: compute more accurate position for diagnostics
    
    * improve position
    
    * Discard changes to packages/editor-worker/src/parts/MeasureCharacterWidth/MeasureCharacterWidth.ts
    
    * Discard changes to packages/editor-worker/src/parts/MeasureTextWidth/MeasureTextWidth.ts
    
    * Discard changes to packages/editor-worker/src/parts/Character/Character.ts
    
    * log

--- packages/editor-worker/src/parts/GetVisibleDiagnostics/GetVisibleDiagnostics.ts
+++ packages/editor-worker/src/parts/GetVisibleDiagnostics/GetVisibleDiagnostics.ts
@@ -1,17 +1,36 @@
 import type { Diagnostic } from '../Diagnostic/Diagnostic.ts'
 import * as GetDiagnosticType from '../GetDiagnosticType/GetDiagnosticType.ts'
+import * as GetX from '../GetX/GetX.ts'
+import * as GetY from '../GetY/GetY.ts'
 
 export const getVisibleDiagnostics = (editor: any, diagnostics: readonly Diagnostic[]): readonly any[] => {
   const visibleDiagnostics = []
-  const { columnWidth, rowHeight, minLineY, charWidth } = editor
+  const { width, rowHeight, minLineY, charWidth, letterSpacing, lines, fontWeight, fontSize, fontFamily, isMonospaceFont, tabSize } = editor
   for (const diagnostic of diagnostics) {
     const { rowIndex, columnIndex, endColumnIndex } = diagnostic
     const columnDelta = endColumnIndex - columnIndex
-    const width = columnDelta * charWidth
+    const diagnosticWidth = columnDelta * charWidth
+    const endLineDifference = 0
+    const halfCursorWidth = 0
+    const x = GetX.getX(
+      lines[rowIndex],
+      columnIndex,
+      fontWeight,
+      fontSize,
+      fontFamily,
+      isMonospaceFont,
+      letterSpacing,
+      tabSize,
+      halfCursorWidth,
+      width,
+      charWidth,
+      endLineDifference,
+    )
+    const y = GetY.getY(rowIndex, minLineY, rowHeight)
     visibleDiagnostics.push({
-      x: columnIndex * columnWidth,
-      y: (rowIndex - minLineY) * rowHeight,
-      width: width,
+      x,
+      y,
+      width: diagnosticWidth,
       height: rowHeight,
       type: GetDiagnosticType.getDiagnosticType(diagnostic),
     })
