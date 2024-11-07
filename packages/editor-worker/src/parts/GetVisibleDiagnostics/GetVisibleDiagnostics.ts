cc5c338106b28e2fb263e1d3e42d9d80878f2783
Le Vivilet <levivlet@pm.me>
2024-11-07 19:41:45 +0100

    feature: compute more accurate position for diagnostics

--- packages/editor-worker/src/parts/GetVisibleDiagnostics/GetVisibleDiagnostics.ts
+++ packages/editor-worker/src/parts/GetVisibleDiagnostics/GetVisibleDiagnostics.ts
@@ -1,15 +1,34 @@
 import type { Diagnostic } from '../Diagnostic/Diagnostic.ts'
 import * as GetDiagnosticType from '../GetDiagnosticType/GetDiagnosticType.ts'
+import * as GetX from '../GetX/GetX.ts'
 
 export const getVisibleDiagnostics = (editor: any, diagnostics: readonly Diagnostic[]): readonly any[] => {
   const visibleDiagnostics = []
-  const { columnWidth, rowHeight, minLineY, charWidth } = editor
+  const { columnWidth, rowHeight, minLineY, charWidth, letterSpacing, lines, fontWeight, fontSize, fontFamily, isMonospaceFont, tabSize, charWidth } =
+    editor
   for (const diagnostic of diagnostics) {
     const { rowIndex, columnIndex, endColumnIndex } = diagnostic
     const columnDelta = endColumnIndex - columnIndex
     const width = columnDelta * charWidth
+    const endLineDifference = 0
+    const halfCursorWidth = 0
+    const x = GetX.getX(
+      lines[rowIndex],
+      columnIndex,
+      fontWeight,
+      fontSize,
+      fontFamily,
+      isMonospaceFont,
+      letterSpacing,
+      tabSize,
+      halfCursorWidth,
+      width,
+      charWidth,
+      endLineDifference,
+    )
+
     visibleDiagnostics.push({
-      x: columnIndex * columnWidth,
+      x,
       y: (rowIndex - minLineY) * rowHeight,
       width: width,
       height: rowHeight,
--- packages/editor-worker/src/parts/MeasureTextWidth/MeasureTextWidth.ts
+++ packages/editor-worker/src/parts/MeasureTextWidth/MeasureTextWidth.ts
@@ -27,9 +27,17 @@ export const measureTextWidth = (
   const letterSpacingString = GetLetterSpacingString.getLetterSpacingString(letterSpacing)
   const fontString = GetFontString.getFontString(fontWeight, fontSize, fontFamily)
   const ctx = GetTextMeasureContext.getContext()
+  console.log({ fontString, letterSpacingString })
   ctx.letterSpacing = letterSpacingString
   ctx.font = fontString
   const metrics = ctx.measureText(text)
   const width = metrics.width
+  console.log({ metrics })
+  setTimeout(() => {
+    const metrics2 = ctx.measureText(text)
+    console.log({ metrics2, fontFamily, fontSize, letterSpacing })
+    const metrics3 = ctx.measureText(' '.repeat(46))
+    console.log({ width: metrics3.width, height: metrics3.emHeightAscent })
+  }, 1000)
   return width
 }
