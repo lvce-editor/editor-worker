245a20d4969ee904fc6bb2c5655c9cf64dc1bb67
Le Vivilet <levivlet@pm.me>
2024-11-18 18:53:50 +0100

    feature: improve cursor left e2e test

--- packages/e2e/src/editor.cursor-left.js
+++ packages/e2e/src/editor.cursor-left.js
@@ -1,5 +1,22 @@
-export const name = 'editor.cursor-left'
+export const name = 'viewlet.editor-cursor-word-left'
 
 export const test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
-  // TODO add cursor left test
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, `<title>Document</title>`)
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+
+  // act
+  await Editor.setCursor(0, 15)
+
+  // assert
+  const cursor = Locator('.EditorCursor')
+  await expect(cursor).toHaveCSS('translate', /^(134|135|136|137|138|139|140|141|142|143|144|145|146).*?px$/)
+
+  // act
+  await Editor.cursorWordLeft()
+
+  // assert
+  await expect(cursor).toHaveCSS('translate', /^(63|64|65|66|67|68|69).*?px$/)
 }
