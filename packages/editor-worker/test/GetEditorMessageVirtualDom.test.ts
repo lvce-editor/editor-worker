d9a17c877a9361373a443f59b60caa35214f105d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-08 21:17:53 +0100

    feature: add editor message test (#510)
    * feature: add editor message test
    
    * test
    
    * fix

---
+++ packages/editor-worker/test/GetEditorMessageVirtualDom.test.ts
@@ -0,0 +1,27 @@
+import { expect, test } from '@jest/globals'
+import * as GetEditorMessageVirtualDom from '../src/parts/GetEditorMessageVirtualDom/GetEditorMessageVirtualDom.ts'
+import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.ts'
+
+test('getEditorMessageVirtualDom - basic message', () => {
+  const message = 'Test message'
+  const dom = GetEditorMessageVirtualDom.getEditorMessageVirtualDom(message)
+  expect(dom).toEqual([
+    {
+      type: 4,
+      className: 'Viewlet EditorMessage',
+      tabIndex: -1,
+      childCount: 2,
+    },
+    {
+      type: 4,
+      className: 'EditorMessageText',
+      childCount: 1,
+    },
+    text('Test message'),
+    {
+      type: 4,
+      className: 'EditorMessageTriangle',
+      childCount: 0,
+    },
+  ])
+})
