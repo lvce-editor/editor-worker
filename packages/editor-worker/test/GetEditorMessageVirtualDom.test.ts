ad60075a45060c9624e617c68d435b08f2373907
Le Vivilet <levivlet@pm.me>
2024-12-08 20:56:51 +0100

    test

--- packages/editor-worker/test/GetEditorMessageVirtualDom.test.ts
+++ packages/editor-worker/test/GetEditorMessageVirtualDom.test.ts
@@ -1,50 +1,29 @@
 import { expect, test } from '@jest/globals'
 import * as GetEditorMessageVirtualDom from '../src/parts/GetEditorMessageVirtualDom/GetEditorMessageVirtualDom.ts'
+import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.ts'
 
 test('getEditorMessageVirtualDom - basic message', () => {
   const message = 'Test message'
   const dom = GetEditorMessageVirtualDom.getEditorMessageVirtualDom(message)
-  expect(dom).toEqual({
-    type: 'div',
-    className: 'EditorMessage',
-    childNodes: [
+  expect(dom).toEqual([
+    [
       {
-        type: 'div',
+        type: 4,
+        className: 'Viewlet EditorMessage',
+        tabIndex: -1,
+        childCount: 2,
+      },
+      {
+        type: 4,
         className: 'EditorMessageText',
-        text: 'Test message',
+        childCount: 1,
+      },
+      text('Test message'),
+      {
+        type: 4,
+        className: 'EditorMessageTriangle',
+        childCount: 0,
       },
     ],
-  })
-})
-
-test('getEditorMessageVirtualDom - empty message', () => {
-  const message = ''
-  const dom = GetEditorMessageVirtualDom.getEditorMessageVirtualDom(message)
-  expect(dom).toEqual({
-    type: 'div',
-    className: 'EditorMessage',
-    childNodes: [
-      {
-        type: 'div',
-        className: 'EditorMessageText',
-        text: '',
-      },
-    ],
-  })
-})
-
-test('getEditorMessageVirtualDom - message with special characters', () => {
-  const message = 'Test & message < > "'
-  const dom = GetEditorMessageVirtualDom.getEditorMessageVirtualDom(message)
-  expect(dom).toEqual({
-    type: 'div',
-    className: 'EditorMessage',
-    childNodes: [
-      {
-        type: 'div',
-        className: 'EditorMessageText',
-        text: 'Test & message < > "',
-      },
-    ],
-  })
+  ])
 })
