3e1aff90566d218abf35a74edea613e9b216d661
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-09 18:42:42 +0100

    Discard changes to packages/editor-worker/src/parts/EditorFindWidget/EditorFindWidget.ts

--- packages/editor-worker/src/parts/EditorFindWidget/EditorFindWidget.ts
+++ packages/editor-worker/src/parts/EditorFindWidget/EditorFindWidget.ts
@@ -1,7 +1,6 @@
 import * as AddWidget from '../AddWidget/AddWidget.ts'
 import * as FindWidgetFunctions from '../FindWidgetFunctions/FindWidgetFunctions.ts'
 import * as FindWidgetRender from '../FindWidgetRender/FindWidgetRender.ts'
-import { renderEventListeners } from '../GetFindWidgetEventListeners/GetFindWidgetEventListeners.ts'
 import type { IFindWidget } from '../IFindWidget/IFindWidget.ts'
 import * as RemoveWidget from '../RemoveWidget/RemoveWidget.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
@@ -9,7 +8,6 @@ import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
 export const render = (widget: IFindWidget) => {
   const commands: readonly any[] = FindWidgetRender.apply(widget.oldState, widget.newState)
   const wrappedCommands = []
-  wrappedCommands.push()
   const uid = widget.newState.uid
   for (const command of commands) {
     if (command[0] === RenderMethod.SetDom2) {
@@ -22,7 +20,7 @@ export const render = (widget: IFindWidget) => {
 }
 
 export const add = (widget: IFindWidget) => {
-  return AddWidget.addWidget(widget, 'FindWidget', render, renderEventListeners)
+  return AddWidget.addWidget(widget, 'FindWidget', render)
 }
 
 export const remove = RemoveWidget.removeWidget
