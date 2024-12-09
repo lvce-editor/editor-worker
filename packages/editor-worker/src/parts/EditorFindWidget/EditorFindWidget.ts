c6987d9806991c182499930b5f01734a38bf2f8c
Le Vivilet <levivlet@pm.me>
2024-12-09 18:41:55 +0100

    move fn

--- packages/editor-worker/src/parts/EditorFindWidget/EditorFindWidget.ts
+++ packages/editor-worker/src/parts/EditorFindWidget/EditorFindWidget.ts
@@ -1,6 +1,7 @@
 import * as AddWidget from '../AddWidget/AddWidget.ts'
 import * as FindWidgetFunctions from '../FindWidgetFunctions/FindWidgetFunctions.ts'
 import * as FindWidgetRender from '../FindWidgetRender/FindWidgetRender.ts'
+import { renderEventListeners } from '../GetFindWidgetEventListeners/GetFindWidgetEventListeners.ts'
 import type { IFindWidget } from '../IFindWidget/IFindWidget.ts'
 import * as RemoveWidget from '../RemoveWidget/RemoveWidget.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
@@ -18,75 +19,6 @@ export const render = (widget: IFindWidget) => {
     }
   }
   return wrappedCommands
-}
-
-const renderEventListeners = () => {
-  return [
-    {
-      name: 'handleInput',
-      params: ['FindWidget.handleInput', 'event.target.value'],
-    },
-    {
-      name: 'handleClickClose',
-      params: ['FindWidget.close'],
-    },
-    {
-      name: 'handleClickPreviousMatch',
-      params: ['FindWidget.focusPrevious'],
-    },
-    {
-      name: 'handleClickNextMatch',
-      params: ['FindWidget.focusNext'],
-    },
-    {
-      name: 'handleClickReplace',
-      params: ['FindWidget.replace'],
-    },
-    {
-      name: 'handleClickReplaceAll',
-      params: ['FindWidget.replaceAll'],
-    },
-    {
-      name: 'handleClickToggleReplace',
-      params: ['FindWidget.toggleReplace'],
-    },
-    {
-      name: 'handleInputBlur',
-      params: ['FindWidget.handleBlur'],
-    },
-    {
-      name: 'handleReplaceInput',
-      params: ['FindWidget.handleReplaceInput', 'event.target.value'],
-    },
-    {
-      name: 'handleReplaceFocus',
-      params: ['FindWidget.handleReplaceFocus'],
-    },
-    {
-      name: 'handleFocus',
-      params: ['FindWidget.handleFocus'],
-    },
-    {
-      name: 'handleToggleReplaceFocus',
-      params: ['FindWidget.handleToggleReplaceFocus'],
-    },
-    {
-      name: 'handleFocusPrevious',
-      params: ['FindWidget.handleFocusPrevious'],
-    },
-    {
-      name: 'handleFocusNext',
-      params: ['FindWidget.handleFocusNext'],
-    },
-    {
-      name: 'handleFocusClose',
-      params: ['FindWidget.handleFocusClose'],
-    },
-    {
-      name: 'handleFocusReplaceAll',
-      params: ['FindWidget.handleFocusReplaceAll'],
-    },
-  ]
 }
 
 export const add = (widget: IFindWidget) => {
