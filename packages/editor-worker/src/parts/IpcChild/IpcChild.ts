7ffef0addab9e2c5f15c4b98bfe0a99fd7105900
Le Vivilet <levivlet@pm.me>
2025-01-04 17:11:59 +0100

    tsc

--- packages/editor-worker/src/parts/IpcChild/IpcChild.ts
+++ packages/editor-worker/src/parts/IpcChild/IpcChild.ts
@@ -3,12 +3,14 @@ import * as IpcChildModule from '../IpcChildModule/IpcChildModule.ts'
 // @ts-ignore
 export const listen = async ({ method }) => {
   const module = await IpcChildModule.getModule(method)
+  // @ts-ignore
   const rawIpc = await module.listen()
   // @ts-ignore
   if (module.signal) {
     // @ts-ignore
     module.signal(rawIpc)
   }
+  // @ts-ignore
   const ipc = module.wrap(rawIpc)
   return ipc
 }
