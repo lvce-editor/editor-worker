38daef57f3ffce9937086cea3fa763d12ade275c
Le Vivilet <levivlet@pm.me>
2025-01-24 23:33:01 +0100

    feature: use typescript for e2e tests

--- packages/e2e/src/editor.cursor-up.ts
+++ packages/e2e/src/editor.cursor-up.ts
@@ -1,12 +1,14 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
 export const name = 'viewlet.editor-cursor-up'
 
-export const test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(
     `${tmpDir}/file1.txt`,
     `content 1
-content 2`
+content 2`,
   )
   await Workspace.setPath(tmpDir)
   await Main.openUri(`${tmpDir}/file1.txt`)
