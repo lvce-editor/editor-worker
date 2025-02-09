a0a2f2dfebebe082c03d213ece7c7c483fc720cd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-09 20:22:47 +0100

    feature: add completion click e2e test (#553)
    * feature: add completion click e2e test
    
    * improve test

---
+++ packages/e2e/src/editor.completion-click.ts
@@ -0,0 +1,29 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.completion-click'
+
+export const skip = 1
+
+export const test: Test = async ({ Extension, FileSystem, Workspace, Main, Editor, Locator, expect, Command }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/editor.completion-click')
+  await Extension.addWebExtension(extensionUri)
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.xyz`, 'content 1')
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.xyz`)
+  await Editor.setCursor(0, 0)
+
+  // act
+  await Editor.openCompletion()
+
+  // assert
+  const completions = Locator('.EditorCompletion')
+  await expect(completions).toBeVisible()
+  const items = Locator('.EditorCompletionItem')
+  await expect(items).toHaveCount(1)
+  await expect(items).toHaveText('test')
+
+  // act
+  await Command.execute('EditorCompletion.selectIndex', 0)
+}
