5ec9c426ba980360233d1e04f2c9fbc94846ada0
Le Vivilet <levivlet@pm.me>
2025-05-17 23:31:17 +0200

    rename

--- packages/editor-worker/src/parts/ColorPicker/ColorPicker.ts
+++ packages/editor-worker/src/parts/ColorPicker/ColorPicker.ts
@@ -3,13 +3,10 @@ import * as ColorPickerWorker from '../ColorPickerWorker/ColorPickerWorker.ts'
 
 export const loadContent = async (state: ColorPickerState, parentUid: number): Promise<ColorPickerState> => {
   const { uid, x, y, width, height } = state
-
-  console.log({ x, y, width, height })
   await ColorPickerWorker.invoke('ColorPicker.create', uid, x, y, width, height, parentUid)
   await ColorPickerWorker.invoke('ColorPicker.loadContent', uid)
   const diff = await ColorPickerWorker.invoke('ColorPicker.diff2', uid)
   const commands = await ColorPickerWorker.invoke('ColorPicker.render2', uid, diff)
-  console.log({ commands })
   return {
     ...state,
     commands,
