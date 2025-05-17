808b2844523b01d716cdb6708d270400a57b6cd0
Le Vivilet <levivlet@pm.me>
2025-05-17 09:27:51 +0200

    feature: add function to launch color picker worker

---
+++ packages/editor-worker/src/parts/LaunchColorPickerWorker/LaunchColorPickerWorker.ts
@@ -0,0 +1,21 @@
+import { MessagePortRpcParent } from '@lvce-editor/rpc'
+import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+import * as RendererWorkerIpcParentType from '../RendererWorkerIpcParentType/RendererWorkerIpcParentType.ts'
+
+export const launchColorPickerWorker = async () => {
+  const name = 'Color Picker Worker'
+  const { port1, port2 } = GetPortTuple.getPortTuple()
+  await RendererWorker.invokeAndTransfer('IpcParent.create', {
+    method: RendererWorkerIpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
+    url: 'colorPickerWorkerMain.js',
+    name: name,
+    raw: true,
+    port: port1,
+  })
+  const rpc = await MessagePortRpcParent.create({
+    commandMap: {},
+    messagePort: port2,
+  })
+  return rpc
+}
