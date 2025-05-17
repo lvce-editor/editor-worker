e006120a21b1c155d2ff34172165ff638809f743
Le Vivilet <levivlet@pm.me>
2025-05-17 23:56:10 +0200

    fix fn

--- packages/editor-worker/src/parts/ExternalGetPositionAtCursor/ExternalGetPositionAtCursor.ts
+++ packages/editor-worker/src/parts/ExternalGetPositionAtCursor/ExternalGetPositionAtCursor.ts
@@ -1,13 +1,13 @@
+import * as EditorCommandGetWordAt from '../EditorCommand/EditorCommandGetWordAt.ts'
 import * as GetEditor from '../GetEditor/GetEditor.ts'
 import * as GetPositionAtCursor from '../GetPositionAtCursor/GetPositionAtCursor.ts'
-import * as GetWordAt from '../GetWordAt/GetWordAt.ts'
 
 export const getPositionAtCursor = (editorUid: number): any => {
   const editor = GetEditor.getEditor(editorUid)
   return GetPositionAtCursor.getPositionAtCursor(editor)
 }
 
-export const getWordAt = (editorUid: number): any => {
+export const getWordAt = (editorUid: number, rowIndex: number, columnIndex: number): any => {
   const editor = GetEditor.getEditor(editorUid)
-  return GetWordAt.getWordAt(editor)
+  return EditorCommandGetWordAt.getWordAt(editor, rowIndex, columnIndex)
 }
