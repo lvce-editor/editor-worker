b2968dc0805d57712ae236fd713952dc4390be09
Le Vivilet <levivlet@pm.me>
2025-05-17 20:28:19 +0200

    feature: simplify eslint config

--- packages/editor-worker/test/EditorCopyLineUp.test.ts
+++ packages/editor-worker/test/EditorCopyLineUp.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@jest/globals'
 import * as EditorCopyLineUp from '../src/parts/EditorCommand/EditorCommandCopyLineUp.ts'
 import * as TokenizePlainText from '../src/parts/TokenizePlainText/TokenizePlainText.ts'
 
-test.only('editorCopyLineUp', () => {
+test('editorCopyLineUp', () => {
   const editor = {
     lines: ['line 1', 'line 2', 'line 3'],
     selections: new Uint32Array([2, 0, 2, 0]),
