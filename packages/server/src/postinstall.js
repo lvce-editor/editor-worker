cf25490a2bfcd197cde55bdd15abba809eba258f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-25 09:41:23 +0200

    feature: update editor worker path during postinstall (#592)
    * feature: update editor worker path during postinstall
    
    * fix path
    
    * tsc
    
    * path

--- packages/server/src/postinstall.js
+++ packages/server/src/postinstall.js
@@ -1,4 +1,4 @@
-import { readFile, writeFile } from 'node:fs/promises'
+import { readdir, readFile, writeFile } from 'node:fs/promises'
 import { dirname, join } from 'node:path'
 import { fileURLToPath, pathToFileURL } from 'node:url'
 
@@ -13,12 +13,10 @@ export const getRemoteUrl = (path) => {
 
 const nodeModulesPath = join(root, 'packages', 'server', 'node_modules')
 
-const editorWorkerPath = join(root, 'dist', 'dist', 'editorWorkerMain.js')
+const editorWorkerPath = join(root, '.tmp', 'dist', 'dist', 'editorWorkerMain.js')
 
 const staticPath = join(nodeModulesPath, '@lvce-editor', 'static-server', 'static')
 const indexHtmlPath = join(staticPath, 'index.html')
-
-const indexHtmlContent = await readFile(indexHtmlPath, 'utf8')
 
 const remoteUrl = getRemoteUrl(editorWorkerPath)
 
@@ -26,10 +24,26 @@ const config = {
   'develop.editorWorkerPath': remoteUrl,
 }
 const stringifiedConfig = JSON.stringify(config, null, 2)
-const newContent = indexHtmlContent.replace(
-  '</title>',
-  `</title>
-  <script type="application/json" id="Config">${stringifiedConfig}</script>`,
-)
 
-await writeFile(indexHtmlPath, newContent)
+const replace = async ({ uri, occurrence, replacement }) => {
+  const content = await readFile(uri, 'utf8')
+  const newContent = content.replace(occurrence, replacement)
+  await writeFile(uri, newContent)
+}
+
+await replace({
+  uri: indexHtmlPath,
+  occurrence: '</title>',
+  replacement: `</title>
+ <script type="application/json" id="Config">${stringifiedConfig}</script>`,
+})
+
+const folders = await readdir(staticPath, { withFileTypes: true })
+const commitHash = folders.find((item) => item.isDirectory())?.name || ''
+const rendererProcessPath = join(staticPath, commitHash, 'packages', 'renderer-process', 'dist', 'rendererProcessMain.js')
+
+await replace({
+  uri: rendererProcessPath,
+  occurrence: 'const editorWorkerUrl = `${assetDir}/packages/editor-worker/dist/editorWorkerMain.js`',
+  replacement: `const editorWorkerUrl = \`${remoteUrl}\``,
+})
