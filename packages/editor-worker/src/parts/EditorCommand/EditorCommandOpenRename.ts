1289ecda69fc722df6bbfd068fb1ca21a732df33
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-26 20:37:25 +0200

    Discard changes to packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenRename.ts

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenRename.ts
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenRename.ts
@@ -1,24 +1,11 @@
+import type { RenameState } from '../RenameState/RenameState.ts'
 import * as AddWidgetToEditor from '../AddWidgetToEditor/AddWidgetToEditor.ts'
 import * as FocusKey from '../FocusKey/FocusKey.ts'
 import * as GetPositionAtCursor from '../GetPositionAtCursor/GetPositionAtCursor.ts'
-import type { RenameState } from '../RenameState/RenameState.ts'
+import * as GetRenamePosition from '../GetRenamePosition/GetRenamePosition.ts'
 import * as RenameWidgetFactory from '../RenameWidgetFactory/RenameWidgetFactory.ts'
-import * as RenameWorker from '../RenameWorker/RenameWorker.ts'
 import * as WidgetId from '../WidgetId/WidgetId.ts'
 import * as EditorCommandGetWordAt from './EditorCommandGetWordAt.ts'
-
-const newStateGenerator = async (state: RenameState, parentUid: number): Promise<RenameState> => {
-  // const editor: any = {}
-  const { uid, x, y, width, height } = state
-  await RenameWorker.invoke('Rename.create', uid, x, y, width, height, parentUid)
-  await RenameWorker.invoke('Rename.loadContent', uid)
-  const diff = await RenameWorker.invoke('Rename.diff2', uid)
-  const commands = await RenameWorker.invoke('Rename.render2', uid, diff)
-  return {
-    ...state,
-    commands,
-  }
-}
 
 export const openRename = async (editor: any) => {
   const { rowIndex, columnIndex } = GetPositionAtCursor.getPositionAtCursor(editor)
@@ -26,7 +13,20 @@ export const openRename = async (editor: any) => {
   if (!word) {
     return editor
   }
-
+  const newStateGenerator = async (state: RenameState): Promise<RenameState> => {
+    // TODO query if can rename from extension host
+    const { x, y, width, height } = GetRenamePosition.getRenamePosition(editor)
+    const latestState: RenameState = {
+      ...state,
+      x,
+      y,
+      width,
+      height,
+      oldValue: word,
+      newValue: word,
+    }
+    return latestState
+  }
   const fullFocus = true
   return AddWidgetToEditor.addWidgetToEditor(
     WidgetId.Rename,
