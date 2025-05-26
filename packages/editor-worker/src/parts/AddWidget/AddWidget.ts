86892656a12e67378e950289225cf521d7312c6b
Le Vivilet <levivlet@pm.me>
2025-05-26 20:35:35 +0200

    fix

--- packages/editor-worker/src/parts/AddWidget/AddWidget.ts
+++ packages/editor-worker/src/parts/AddWidget/AddWidget.ts
@@ -4,7 +4,7 @@ import * as WidgetId from '../WidgetId/WidgetId.ts'
 const isFunctional = (widgetId: number | string): boolean => {
   switch (widgetId) {
     case WidgetId.ColorPicker:
-    case WidgetId.Rename:
+      // case WidgetId.Rename: // TODO
       return true
     default:
       return false
