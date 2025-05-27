88012ab5d5a2cf60ad1b520a45087c20d26cff1e
Le Vivilet <levivlet@pm.me>
2025-05-27 22:51:55 +0200

    key

--- packages/editor-worker/src/parts/GetKeyBindings/GetKeyBindings.ts
+++ packages/editor-worker/src/parts/GetKeyBindings/GetKeyBindings.ts
@@ -123,39 +123,10 @@ export const getKeyBindings = async (): Promise<readonly any[]> => {
       command: 'FindWidget.focusReplaceButton',
       when: WhenExpression.FocusFindWidgetReplaceAllButton,
     },
-    {
-      key: KeyCode.DownArrow,
-      command: 'EditorCompletion.focusNext',
-      when: WhenExpression.FocusEditorCompletions,
-    },
-    {
-      key: KeyCode.UpArrow,
-      command: 'EditorCompletion.focusPrevious',
-      when: WhenExpression.FocusEditorCompletions,
-    },
-    {
-      key: KeyCode.Enter,
-      command: 'EditorCompletion.selectCurrent',
-      when: WhenExpression.FocusEditorCompletions,
-    },
+
     {
       key: KeyCode.Escape,
       command: 'Editor.closeCompletion',
-      when: WhenExpression.FocusEditorCompletions,
-    },
-    {
-      key: KeyCode.End,
-      command: 'EditorCompletion.focusLast',
-      when: WhenExpression.FocusEditorCompletions,
-    },
-    {
-      key: KeyCode.Home,
-      command: 'EditorCompletion.focusFirst',
-      when: WhenExpression.FocusEditorCompletions,
-    },
-    {
-      key: KeyModifier.CtrlCmd | KeyCode.Space,
-      command: 'EditorCompletion.toggleDetails',
       when: WhenExpression.FocusEditorCompletions,
     },
     {
