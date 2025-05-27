6727782bc714972398936c2a660beef7f3cf46c4
Le Vivilet <levivlet@pm.me>
2025-05-27 20:00:40 +0200

    tsc

--- packages/editor-worker/src/parts/EditorRenameHandleBlur/EditorRenameHandleBlur.ts
+++
@@ -1,5 +0,0 @@
-import * as EditorCommandCloseRename from '../EditorCommand/EditorCommandCloseRename.ts'
-
-export const handleBlur = (editor: any): any => {
-  return EditorCommandCloseRename.closeRename(editor)
-}
