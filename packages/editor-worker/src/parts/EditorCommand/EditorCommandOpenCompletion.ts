a5a3ebc2ff118517f5d47407d64871e5baeead48
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-27 19:46:51 +0200

    Discard changes to packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenCompletion.ts

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenCompletion.ts
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenCompletion.ts
@@ -1,31 +1,43 @@
-import type { CompletionState } from '../CompletionState/CompletionState.ts'
-import * as AddWidgetToEditor from '../AddWidgetToEditor/AddWidgetToEditor.ts'
+import type { CompletionWidget } from '../CompletionWidget/CompletionWidget.ts'
 import * as CompletionWidgetFactory from '../CompletionWidgetFactory/CompletionWidgetFactory.ts'
-import * as CompletionWorker from '../CompletionWorker/CompletionWorker.ts'
+import * as EditorCompletion from '../EditorCompletion/EditorCompletion.ts'
+import * as Editors from '../Editors/Editors.ts'
 import * as FocusKey from '../FocusKey/FocusKey.ts'
+import * as GetEditor from '../GetEditor/GetEditor.ts'
+import * as HasWidget from '../HasWidget/HasWidget.ts'
+import * as SetAdditionalFocus from '../SetAdditionalFocus/SetAdditionalFocus.ts'
 import * as WidgetId from '../WidgetId/WidgetId.ts'
 
-const newStateGenerator = async (state: CompletionState, parentUid: number): Promise<CompletionState> => {
-  // const editor: any = {}
-  const { uid, x, y, width, height } = state
-  await CompletionWorker.invoke('Completions.create', uid, x, y, width, height, parentUid)
-  await CompletionWorker.invoke('Completions.loadContent', uid)
-  const diff = await CompletionWorker.invoke('Completions.diff2', uid)
-  const commands = await CompletionWorker.invoke('Completions.render2', uid, diff)
-  return {
-    ...state,
-    commands,
-  }
-}
-
 export const openCompletion = async (editor: any) => {
-  const fullFocus = false
-  return AddWidgetToEditor.addWidgetToEditor(
-    WidgetId.Completion,
-    FocusKey.EditorCompletion,
-    editor,
-    CompletionWidgetFactory.create,
-    newStateGenerator,
-    fullFocus,
-  )
+  const { widgets, uid } = editor
+  if (HasWidget.hasWidget(widgets, WidgetId.Completion)) {
+    return editor
+  }
+  const completionWidget: CompletionWidget = CompletionWidgetFactory.create()
+  const newWidgets = [...widgets, completionWidget]
+  const newEditor = {
+    ...editor,
+    widgets: newWidgets,
+  }
+  Editors.set(uid, editor, newEditor)
+  const newCompletionWidget = await EditorCompletion.loadContent(uid, completionWidget.newState)
+  const FocusEditorCompletions = FocusKey.EditorCompletion
+  await SetAdditionalFocus.setAdditionalFocus(FocusEditorCompletions)
+  const latestEditor = GetEditor.getEditor(uid)
+  if (!latestEditor.widgets.includes(completionWidget)) {
+    return editor
+  }
+  const index = latestEditor.widgets.indexOf(completionWidget)
+  const latestWidgets = [
+    ...latestEditor.widgets.slice(0, index),
+    {
+      ...completionWidget,
+      newState: newCompletionWidget,
+    },
+    ...latestEditor.widgets.slice(index + 1),
+  ]
+  return {
+    ...latestEditor,
+    widgets: latestWidgets,
+  }
 }
