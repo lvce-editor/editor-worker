0e9acb827568614c9406bc4776473e3819689734
Le Vivilet <levivlet@pm.me>
2025-05-28 20:10:44 +0200

    pass position

--- packages/editor-worker/src/parts/FindWidgetFunctions/FindWidgetFunctions.ts
+++ packages/editor-worker/src/parts/FindWidgetFunctions/FindWidgetFunctions.ts
@@ -2,9 +2,12 @@ import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 import * as FindWidgetWorker from '../FindWidgetWorker/FindWidgetWorker.ts'
 import * as FocusKey from '../FocusKey/FocusKey.ts'
 import * as SetFocus from '../SetFocus/SetFocus.ts'
+import * as GetEditor from '../GetEditor/GetEditor.ts'
 
 export const loadContent = async (state: FindWidgetState, parentUid: number): Promise<FindWidgetState> => {
-  const { uid, x, y, width, height } = state
+  const { uid } = state
+  const editor = GetEditor.getEditor(parentUid)
+  const { x, y, width, height } = editor
   await FindWidgetWorker.invoke('FindWidget.create', uid, x, y, width, height, parentUid)
   await FindWidgetWorker.invoke('FindWidget.loadContent', uid)
   const diff = await FindWidgetWorker.invoke('FindWidget.diff2', uid)
