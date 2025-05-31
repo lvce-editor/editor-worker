07f704df7be70922e837cedd1de530ecf23df82d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-31 09:33:05 +0200

    Discard changes to packages/editor-worker/src/parts/FindWidgetFunctions/FindWidgetFunctions.ts

--- packages/editor-worker/src/parts/FindWidgetFunctions/FindWidgetFunctions.ts
+++ packages/editor-worker/src/parts/FindWidgetFunctions/FindWidgetFunctions.ts
@@ -1,18 +1,26 @@
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
-import * as FindWidgetWorker from '../FindWidgetWorker/FindWidgetWorker.ts'
+import * as FindMatchesCaseInsensitive from '../FindMatchesCaseInsensitive/FindMatchesCaseInsensitive.ts'
+import * as FocusKey from '../FocusKey/FocusKey.ts'
 import * as GetEditor from '../GetEditor/GetEditor.ts'
+import * as GetMatchCount from '../GetMatchCount/GetMatchCount.ts'
+import * as SetFocus from '../SetFocus/SetFocus.ts'
 
-export const loadContent = async (state: FindWidgetState, parentUid: number): Promise<FindWidgetState> => {
-  const { uid } = state
-  const editor = GetEditor.getEditor(parentUid)
-  const { x, y, width, height } = editor
-  await FindWidgetWorker.invoke('FindWidget.create', uid, x, y, width, height, parentUid)
-  await FindWidgetWorker.invoke('FindWidget.loadContent', uid)
-  const diff = await FindWidgetWorker.invoke('FindWidget.diff2', uid)
-  const commands = await FindWidgetWorker.invoke('FindWidget.render2', uid, diff)
+export const loadContent = (editorId: number) => {
+  const editor = GetEditor.getEditor(editorId)
+  const { selections, lines } = editor
+  const startRowIndex = selections[0]
+  const startColumnIndex = selections[1]
+  const endColumnIndex = selections[3]
+  const line = lines[startRowIndex]
+  const value = line.slice(startColumnIndex, endColumnIndex)
+  const matches = FindMatchesCaseInsensitive.findMatchesCaseInsensitive(lines, value)
+  const matchCount = GetMatchCount.getMatchCount(matches)
   return {
-    ...state,
-    commands,
+    value,
+    matches,
+    matchIndex: 0,
+    matchCount,
+    editorUid: editor.uid,
   }
 }
 
@@ -24,3 +32,38 @@ export const close = async (state: FindWidgetState) => {
     disposed: true,
   }
 }
+
+export const handleToggleReplaceFocus = async (state: FindWidgetState): Promise<FindWidgetState> => {
+  if (state.focus === FocusKey.FocusFindWidgetToggleReplace) {
+    return state
+  }
+  await SetFocus.setFocus(FocusKey.FocusFindWidgetToggleReplace)
+  return {
+    ...state,
+    focus: FocusKey.FocusFindWidgetToggleReplace,
+  }
+}
+
+export const handleReplaceInput = (state: FindWidgetState, value: string): FindWidgetState => {
+  return {
+    ...state,
+    replacement: value,
+  }
+}
+
+export * from '../FindWidgetFocusCloseButton/FindWidgetFocusCloseButton.ts'
+export * from '../FindWidgetFocusFind/FindWidgetFocusFind.ts'
+export * from '../FindWidgetFocusIndex/FindWidgetFocusIndex.ts'
+export * from '../FindWidgetFocusNextMatchButton/FindWidgetFocusNextMatchButton.ts'
+export * from '../FindWidgetFocusPreviousMatchButton/FindWidgetFocusPreviousMatchButton.ts'
+export * from '../FindWidgetFocusReplace/FindWidgetFocusReplace.ts'
+export * from '../FindWidgetFocusReplaceAllButton/FindWidgetFocusReplaceAllButton.ts'
+export * from '../FindWidgetFocusReplaceButton/FindWidgetFocusReplaceButton.ts'
+export * from '../FindWidgetFocusToggleReplaceButton/FindWidgetFocusToggleReplaceButton.ts'
+export * from '../FindWidgetHandleBlur/FindWidgetHandleBlur.ts'
+export * from '../FindWidgetHandleInput/FindWidgetHandleInput.ts'
+export * from '../FindWidgetHandleReplaceAllFocus/FindWidgetHandleReplaceAllFocus.ts'
+export * from '../FindWidgetHandleReplaceFocus/FindWidgetHandleReplaceFocus.ts'
+export * from '../FindWidgetRefresh/FindWidgetRefresh.ts'
+export * from '../FindWidgetToggleReplace/FindWidgetToggleReplace.ts'
+export * from '../HandleFindWidgetFocus/HandleFindWidgetFocus.ts'
