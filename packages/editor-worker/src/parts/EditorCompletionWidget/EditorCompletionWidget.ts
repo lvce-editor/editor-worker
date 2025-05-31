506d69e1f9ee55fbc674d5fe7c9d3175d6013bbc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-31 21:42:25 +0200

    Discard changes to packages/editor-worker/src/parts/EditorCompletionWidget/EditorCompletionWidget.ts

--- packages/editor-worker/src/parts/EditorCompletionWidget/EditorCompletionWidget.ts
+++ packages/editor-worker/src/parts/EditorCompletionWidget/EditorCompletionWidget.ts
@@ -1,64 +1,73 @@
-import * as AddWidget from '../AddWidget/AddWidget.ts'
-import type { CompletionWidget } from '../CompletionWidget/CompletionWidget.ts'
 import * as EditorCommandGetWordAt from '../EditorCommand/EditorCommandGetWordAt.ts'
+import * as EditorCompletionRender from '../EditorCompletionRender/EditorCompletionRender.ts'
 import * as FilterCompletionItems from '../FilterCompletionItems/FilterCompletionItems.ts'
 import * as GetListHeight from '../GetListHeight/GetListHeight.ts'
 import * as GetPositionAtCursor from '../GetPositionAtCursor/GetPositionAtCursor.ts'
-import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
-import * as RenderRename from '../RenderRename/RenderRename.ts'
-import * as CompletionWorker from '../CompletionWorker/CompletionWorker.ts'
+import * as RemoveWidget from '../RemoveWidget/RemoveWidget.ts'
 
-export const render = (widget: CompletionWidget) => {
-  const commands: readonly any[] = RenderRename.renderFull(widget.oldState, widget.newState)
+export const render = (widget: any) => {
+  const commands = EditorCompletionRender.renderCompletion(widget.oldState, widget.newState)
   const wrappedCommands = []
   const { uid } = widget.newState
   for (const command of commands) {
-    if (
-      command[0] === RenderMethod.SetDom2 ||
-      command[0] === RenderMethod.SetCss ||
-      command[0] === RenderMethod.AppendToBody ||
-      command[0] === RenderMethod.SetBounds2 ||
-      command[0] === RenderMethod.RegisterEventListeners ||
-      command[0] === RenderMethod.SetSelectionByName ||
-      command[0] === RenderMethod.SetValueByName ||
-      command[0] === RenderMethod.SetFocusContext ||
-      command[0] === RenderMethod.SetUid ||
-      command[0] === 'Viewlet.focusSelector'
-    ) {
-      wrappedCommands.push(command)
-    } else {
-      wrappedCommands.push(['Viewlet.send', uid, ...command])
-    }
+    wrappedCommands.push(['Viewlet.send', uid, ...command])
   }
   return wrappedCommands
 }
 
-export const add = (widget: CompletionWidget) => {
-  return AddWidget.addWidget(widget, 'EditorRename', render)
+export const add = (widget: any) => {
+  const commands = render(widget)
+  const id = 'EditorCompletion'
+  // TODO how to generate a unique integer id
+  // that doesn't collide with ids created in renderer worker?
+  const { uid } = widget.newState
+  const allCommands: any[] = []
+  allCommands.push(['Viewlet.create', id, uid])
+  allCommands.push(...commands)
+  return allCommands
 }
 
-export const remove = (widget: CompletionWidget) => {
-  return [['Viewlet.dispose', widget.newState.uid]]
-}
+export const remove = RemoveWidget.removeWidget
 
-export const handleEditorType = async (editor: any, state: any) => {
-  const { uid } = state
-  await CompletionWorker.invoke('Completions.handleEditorType', uid)
-  const diff = await CompletionWorker.invoke('Completions.diff2', uid)
-  const commands = await CompletionWorker.invoke('Completions.render2', uid, diff)
+export const handleEditorType = (editor: any, state: any) => {
+  const { unfilteredItems, itemHeight, maxHeight } = state
+  const { x, y, rowIndex, columnIndex } = GetPositionAtCursor.getPositionAtCursor(editor)
+  const wordAtOffset = EditorCommandGetWordAt.getWordBefore(editor, rowIndex, columnIndex)
+  const items = FilterCompletionItems.filterCompletionItems(unfilteredItems, wordAtOffset)
+  const newMinLineY = 0
+  const newMaxLineY = Math.min(items.length, 8)
+  const height = GetListHeight.getListHeight(items.length, itemHeight, maxHeight)
+  const finalDeltaY = items.length * itemHeight - height
   return {
     ...state,
-    commands,
+    items,
+    x,
+    y,
+    minLineY: newMinLineY,
+    maxLineY: newMaxLineY,
+    leadingWord: wordAtOffset,
+    height,
+    finalDeltaY,
   }
 }
 
-export const handleEditorDeleteLeft = async (editor: any, state: any) => {
-  const { uid } = state
-  await CompletionWorker.invoke('Completions.handleEditorDeleteLeft', uid)
-  const diff = await CompletionWorker.invoke('Completions.diff2', uid)
-  const commands = await CompletionWorker.invoke('Completions.render2', uid, diff)
+export const handleEditorDeleteLeft = (editor: any, state: any) => {
+  const { unfilteredItems, itemHeight, maxHeight } = state
+  const { x, y, rowIndex, columnIndex } = GetPositionAtCursor.getPositionAtCursor(editor)
+  const wordAtOffset = EditorCommandGetWordAt.getWordBefore(editor, rowIndex, columnIndex)
+  if (!wordAtOffset) {
+    return undefined
+  }
+  const items = FilterCompletionItems.filterCompletionItems(unfilteredItems, wordAtOffset)
+  const newMaxLineY = Math.min(items.length, 8)
+  const height = GetListHeight.getListHeight(items.length, itemHeight, maxHeight)
   return {
     ...state,
-    commands,
+    items,
+    x,
+    y,
+    maxLineY: newMaxLineY,
+    leadingWord: wordAtOffset,
+    height,
   }
 }
