f4579d392473e920f122ce0e0f5409da505c93cc
Le Vivilet <levivlet@pm.me>
2025-05-31 09:52:10 +0200

    tsc

--- packages/editor-worker/src/parts/FindWidgetHandleInput/FindWidgetHandleInput.ts
+++
@@ -1,6 +0,0 @@
-import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
-import * as FindWidgetRefresh from '../FindWidgetRefresh/FindWidgetRefresh.ts'
-
-export const handleInput = (state: FindWidgetState, value: string): FindWidgetState => {
-  return FindWidgetRefresh.refresh(state, value)
-}
