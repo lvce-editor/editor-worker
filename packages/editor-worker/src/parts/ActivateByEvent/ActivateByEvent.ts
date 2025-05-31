81e515dab6eb50abb71ade6665e496a8b65c038f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-31 22:10:51 +0200

    feature: add types for activate (#611)

--- packages/editor-worker/src/parts/ActivateByEvent/ActivateByEvent.ts
+++ packages/editor-worker/src/parts/ActivateByEvent/ActivateByEvent.ts
@@ -1,6 +1,6 @@
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 // TODO add tests for this
-export const activateByEvent = async (event: any) => {
+export const activateByEvent = async (event: string): Promise<void> => {
   await RendererWorker.invoke('ExtensionHostManagement.activateByEvent', event)
 }
