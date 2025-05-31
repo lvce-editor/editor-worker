9e4d6b387b44651e7a0e71144054013f16804812
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-31 09:32:22 +0200

    Discard changes to packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenFind2.ts

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenFind2.ts
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandOpenFind2.ts
@@ -3,20 +3,30 @@ import * as AddWidgetToEditor from '../AddWidgetToEditor/AddWidgetToEditor.ts'
 import * as FindWidgetFactory from '../FindWidgetFactory/FindWidgetFactory.ts'
 import * as FindWidgetFunctions from '../FindWidgetFunctions/FindWidgetFunctions.ts'
 import * as FocusKey from '../FocusKey/FocusKey.ts'
+import * as FocusSource from '../FocusSource/FocusSource.ts'
+import * as GetFindWidgetPosition from '../GetFindWidgetPosition/GetFindWidgetPosition.ts'
 import * as WidgetId from '../WidgetId/WidgetId.ts'
 
-const newStateGenerator = (state: FindWidgetState, parentUid: number): Promise<FindWidgetState> => {
-  return FindWidgetFunctions.loadContent(state, parentUid)
-}
-
 export const openFind2 = async (editor: any) => {
+  const newStateGenerator = async (state: FindWidgetState): Promise<FindWidgetState> => {
+    const { value, matches, matchCount, matchIndex } = await FindWidgetFunctions.loadContent(editor.uid)
+    const { x, y, width, height } = GetFindWidgetPosition.getFindWidgetPosition(editor)
+    const latestState: FindWidgetState = {
+      ...state,
+      value,
+      matches,
+      matchCount,
+      matchIndex,
+      x,
+      y,
+      width,
+      height,
+      editorUid: editor.uid || editor.id,
+      focusSource: FocusSource.Script,
+      focus: FocusKey.FindWidget,
+    }
+    return latestState
+  }
   const fullFocus = true
-  return AddWidgetToEditor.addWidgetToEditor(
-    WidgetId.ColorPicker,
-    FocusKey.FindWidget,
-    editor,
-    FindWidgetFactory.create,
-    newStateGenerator,
-    fullFocus,
-  )
+  return AddWidgetToEditor.addWidgetToEditor(WidgetId.Find, FocusKey.FindWidget, editor, FindWidgetFactory.create, newStateGenerator, fullFocus)
 }
