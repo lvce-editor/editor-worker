4b8f1c0cb9435d27fa4b143f1e4b529d632d4d49
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-31 09:32:49 +0200

    Discard changes to packages/editor-worker/src/parts/ExecuteWidgetCommand/ExecuteWidgetCommand.ts

--- packages/editor-worker/src/parts/ExecuteWidgetCommand/ExecuteWidgetCommand.ts
+++ packages/editor-worker/src/parts/ExecuteWidgetCommand/ExecuteWidgetCommand.ts
@@ -1,57 +1,31 @@
-import { number } from '@lvce-editor/assert'
 import * as ColorPickerWorker from '../ColorPickerWorker/ColorPickerWorker.ts'
-import * as CompletionWorker from '../CompletionWorker/CompletionWorker.ts'
-import * as FindWidgetWorker from '../FindWidgetWorker/FindWidgetWorker.ts'
-import * as GetEditor from '../GetEditor/GetEditor.ts'
 import * as UpdateWidget from '../UpdateWidget/UpdateWidget.ts'
-import * as WidgetId from '../WidgetId/WidgetId.ts'
 
-const getInvoke = (widgetId: number): any => {
-  switch (widgetId) {
-    case WidgetId.ColorPicker:
-      return ColorPickerWorker.invoke
-    case WidgetId.Completion:
-      return CompletionWorker.invoke
-    case WidgetId.Find:
-      return FindWidgetWorker.invoke
-    default:
-      return undefined
-  }
+const getInvoke = (): any => {
+  return ColorPickerWorker.invoke
 }
 
 export const executeWidgetCommand = async (
   editor: any,
   name: string,
   method: string,
-  _uid: number,
+  uid: number,
   widgetId: number,
   ...params: readonly any[]
 ): Promise<any> => {
-  const invoke = getInvoke(widgetId)
+  const invoke = getInvoke()
   const actualMethod = method.slice(name.length + 1)
-  const widget = editor.widgets.find((widget: any) => widget.id === widgetId)
-  if (!widget) {
-    return editor
-  }
-  const { uid } = widget.newState
-  number(uid)
   await invoke(`${name}.${actualMethod}`, uid, ...params)
-  const isWidget = (widget: any) => {
-    return widget.id === widgetId
-  }
-  const latestEditor = GetEditor.getEditor(editor.uid)
-  const childIndex1 = latestEditor.widgets.findIndex(isWidget)
-  if (childIndex1 === -1) {
-    return latestEditor
-  }
-
   const diff = await invoke(`${name}.diff2`, uid)
   const commands = await invoke(`${name}.render2`, uid, diff)
 
+  const isWidget = (widget: any) => {
+    return widget.id === widgetId
+  }
   const childIndex = editor.widgets.findIndex(isWidget)
   const childWidget = editor.widgets[childIndex]
   const newState = {
-    ...childWidget.newState,
+    ...childWidget.state,
     commands,
   }
   const newEditor = UpdateWidget.updateWidget(editor, widgetId, newState)
