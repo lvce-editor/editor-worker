96d8e216400e274b31d6f681300ef3d3a794ac49
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-31 09:32:36 +0200

    Discard changes to packages/editor-worker/src/parts/EditorFindWidget/EditorFindWidget.ts

--- packages/editor-worker/src/parts/EditorFindWidget/EditorFindWidget.ts
+++ packages/editor-worker/src/parts/EditorFindWidget/EditorFindWidget.ts
@@ -1,38 +1,37 @@
 import type { IFindWidget } from '../IFindWidget/IFindWidget.ts'
 import * as AddWidget from '../AddWidget/AddWidget.ts'
+import * as FindWidgetFunctions from '../FindWidgetFunctions/FindWidgetFunctions.ts'
 import * as FindWidgetRender from '../FindWidgetRender/FindWidgetRender.ts'
+import * as RemoveWidget from '../RemoveWidget/RemoveWidget.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
 
 export const render = (widget: IFindWidget) => {
-  const commands: readonly any[] = FindWidgetRender.renderFull(widget.oldState, widget.newState)
+  const commands: readonly any[] = FindWidgetRender.apply(widget.oldState, widget.newState)
   const wrappedCommands = []
   const { uid } = widget.newState
   for (const command of commands) {
-    if (
-      command[0] === RenderMethod.SetDom2 ||
-      command[0] === RenderMethod.SetCss ||
-      command[0] === RenderMethod.AppendToBody ||
-      command[0] === RenderMethod.SetBounds2 ||
-      command[0] === RenderMethod.RegisterEventListeners ||
-      command[0] === RenderMethod.SetSelectionByName ||
-      command[0] === RenderMethod.SetValueByName ||
-      command[0] === RenderMethod.SetFocusContext ||
-      command[0] === RenderMethod.SetUid ||
-      command[0] === 'Viewlet.focusSelector'
-    ) {
-      wrappedCommands.push(command)
+    if (command[0] === RenderMethod.SetDom2) {
+      wrappedCommands.push([command[0], uid, ...command.slice(1)])
     } else {
       wrappedCommands.push(['Viewlet.send', uid, ...command])
     }
   }
   return wrappedCommands
 }
+
 export const add = (widget: IFindWidget) => {
   return AddWidget.addWidget(widget, 'FindWidget', render)
 }
 
-export const remove = (widget: IFindWidget) => {
-  return [['Viewlet.dispose', widget.newState.uid]]
-}
+export const remove = RemoveWidget.removeWidget
 
-export const Commands = {}
+export const Commands = {
+  'FindWidget.close': FindWidgetFunctions.close,
+  'FindWidget.focusNext': FindWidgetFunctions.focusNext,
+  'FindWidget.focusPrevious': FindWidgetFunctions.focusPrevious,
+  'FindWidget.focusIndex': FindWidgetFunctions.focusIndex,
+  'FindWidget.focusLast': FindWidgetFunctions.focusLast,
+  'FindWidget.toggleReplace': FindWidgetFunctions.toggleReplace,
+  'FindWidget.handleFocus': FindWidgetFunctions.focusFind,
+  'FindWidget.handleBlur': FindWidgetFunctions.handleBlur,
+}
