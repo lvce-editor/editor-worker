6fc1cec9d990496ed13453f554b31207b828c313
Le Vivilet <levivlet@pm.me>
2025-06-03 20:09:00 +0200

    feature: remove unused fuzzysearch code

--- packages/editor-worker/src/parts/EditorCompletion/EditorCompletion.ts
+++ packages/editor-worker/src/parts/EditorCompletion/EditorCompletion.ts
@@ -1,5 +1,4 @@
 import * as EditorCompletionState from '../EditorCompletionState/EditorCompletionState.ts'
-import * as FilterCompletionItems from '../FilterCompletionItems/FilterCompletionItems.ts'
 
 const disposeWithEditor = (state: any, editor: any) => {
   editor.completionState = EditorCompletionState.None
@@ -14,11 +13,3 @@ export const handleEditorBlur = disposeWithEditor
 
 // @ts-ignore
 const handleSelectionChange = (state, selectionChanges) => {}
-
-export const advance = (state: any, word: string) => {
-  const filteredItems = FilterCompletionItems.filterCompletionItems(state.items, word)
-  return {
-    ...state,
-    filteredItems,
-  }
-}
