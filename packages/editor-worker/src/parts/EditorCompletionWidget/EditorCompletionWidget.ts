ad83e9e10c4d0e89e0d0295f568a7813307b188d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-05 07:59:20 +0200

    feature: move createFns into separate file (#624)

--- packages/editor-worker/src/parts/EditorCompletionWidget/EditorCompletionWidget.ts
+++ packages/editor-worker/src/parts/EditorCompletionWidget/EditorCompletionWidget.ts
@@ -1,10 +1,8 @@
 import type { CompletionWidget } from '../CompletionWidget/CompletionWidget.ts'
 import * as AddWidget from '../AddWidget/AddWidget.ts'
-import * as CompletionWorker from '../CompletionWorker/CompletionWorker.ts'
-import * as Editors from '../Editors/Editors.ts'
+import { createFns } from '../CreateFns/CreateFns.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
 import * as RenderRename from '../RenderRename/RenderRename.ts'
-import * as UpdateWidget from '../UpdateWidget/UpdateWidget.ts'
 import * as WidgetId from '../WidgetId/WidgetId.ts'
 
 export const render = (widget: CompletionWidget) => {
@@ -40,39 +38,6 @@ export const remove = (widget: CompletionWidget) => {
   return [['Viewlet.dispose', widget.newState.uid]]
 }
 
-const createFn = (key: string) => {
-  const widgetId = WidgetId.Completion
-  const isWidget = (widget: any) => {
-    return widget.id === widgetId
-  }
-  const fn = async (editor: any, ...args: readonly any[]) => {
-    const childIndex = editor.widgets.findIndex(isWidget)
-    // TODO scroll up/down if necessary
-    const childWidget = editor.widgets[childIndex]
-    const state = childWidget.newState
-    const { uid } = state
-    await CompletionWorker.invoke(`Completions.${key}`, uid, ...args)
-    const diff = await CompletionWorker.invoke('Completions.diff2', uid)
-    const commands = await CompletionWorker.invoke('Completions.render2', uid, diff)
-    const newState = {
-      ...state,
-      commands,
-    }
-    const latest = Editors.get(editor.uid).newState
-    const newEditor = UpdateWidget.updateWidget(latest, widgetId, newState)
-    return newEditor
-  }
-  return fn
-}
-
-const createFns = (keys: readonly string[]): any => {
-  const fns = Object.create(null)
-  for (const key of keys) {
-    fns[key] = createFn(key)
-  }
-  return fns
-}
-
 export const {
   focusFirst,
   focusIndex,
@@ -90,22 +55,26 @@ export const {
   closeDetails,
   handleWheel,
   close,
-} = createFns([
-  'handleEditorType',
-  'focusFirst',
-  'focusNext',
-  'focusPrevious',
-  'focusLast',
-  'handleEditorDeleteLeft',
-  'openDetails',
-  'focusIndex',
-  'handleEditorBlur',
-  'handleEditorClick',
-  'openDetails',
-  'selectCurrent',
-  'selectIndex',
-  'toggleDetails',
-  'closeDetails',
-  'handleWheel',
-  'close',
-])
+} = createFns(
+  [
+    'handleEditorType',
+    'focusFirst',
+    'focusNext',
+    'focusPrevious',
+    'focusLast',
+    'handleEditorDeleteLeft',
+    'openDetails',
+    'focusIndex',
+    'handleEditorBlur',
+    'handleEditorClick',
+    'openDetails',
+    'selectCurrent',
+    'selectIndex',
+    'toggleDetails',
+    'closeDetails',
+    'handleWheel',
+    'close',
+  ],
+  'Completions',
+  WidgetId.Completion,
+)
