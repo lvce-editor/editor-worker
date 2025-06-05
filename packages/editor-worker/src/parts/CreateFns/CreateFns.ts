01cf024243553fb4ffe887c52d2af0dbac880bb8
Le Vivilet <levivlet@pm.me>
2025-06-05 07:56:58 +0200

    feature: move createFns into separate file

---
+++ packages/editor-worker/src/parts/CreateFns/CreateFns.ts
@@ -0,0 +1,35 @@
+import * as CompletionWorker from '../CompletionWorker/CompletionWorker.ts'
+import * as Editors from '../Editors/Editors.ts'
+import * as UpdateWidget from '../UpdateWidget/UpdateWidget.ts'
+
+const createFn = (key: string, name: string, widgetId: number) => {
+  const isWidget = (widget: any) => {
+    return widget.id === widgetId
+  }
+  const fn = async (editor: any, ...args: readonly any[]) => {
+    const childIndex = editor.widgets.findIndex(isWidget)
+    // TODO scroll up/down if necessary
+    const childWidget = editor.widgets[childIndex]
+    const state = childWidget.newState
+    const { uid } = state
+    await CompletionWorker.invoke(`${name}.${key}`, uid, ...args)
+    const diff = await CompletionWorker.invoke(`${name}.diff2`, uid)
+    const commands = await CompletionWorker.invoke(`${name}.render2`, uid, diff)
+    const newState = {
+      ...state,
+      commands,
+    }
+    const latest = Editors.get(editor.uid).newState
+    const newEditor = UpdateWidget.updateWidget(latest, widgetId, newState)
+    return newEditor
+  }
+  return fn
+}
+
+export const createFns = (keys: readonly string[], name: string, widgetId: number): any => {
+  const fns = Object.create(null)
+  for (const key of keys) {
+    fns[key] = createFn(key, name, widgetId)
+  }
+  return fns
+}
