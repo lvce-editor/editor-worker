d65f0df3a381b2e8a7f09728720db15420b3ecfa
Le Vivilet <levivlet@pm.me>
2025-06-09 12:03:36 +0200

    feature: rename function

--- packages/editor-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
+++ packages/editor-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
@@ -1,7 +1,7 @@
 import { PlainMessagePortRpcParent } from '@lvce-editor/rpc'
 import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
 
-export const handleMessagePort2 = async (port: MessagePort, rpcId?: number) => {
+export const handleMessagePort = async (port: MessagePort, rpcId?: number) => {
   const rpc = await PlainMessagePortRpcParent.create({
     messagePort: port,
     commandMap: {},
