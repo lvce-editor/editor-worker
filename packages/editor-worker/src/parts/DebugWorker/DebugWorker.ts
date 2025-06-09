a8f6006b93317a1f110ad50dc06d37f626f7c463
Le Vivilet <levivlet@pm.me>
2025-06-09 13:40:32 +0200

    feature: query debug highlight from worker

--- packages/editor-worker/src/parts/DebugWorker/DebugWorker.ts
+++ packages/editor-worker/src/parts/DebugWorker/DebugWorker.ts
@@ -1,10 +1,6 @@
-import type { Rpc } from '@lvce-editor/rpc'
-
-const getOrCreate = async (): Promise<Rpc> => {
-  return {} as any
-}
+import { get, RpcId } from '@lvce-editor/rpc-registry'
 
 export const invoke = async (method: string, ...params: readonly any[]): Promise<any> => {
-  const worker = await getOrCreate()
+  const worker = get(RpcId.DebugWorker)
   return worker.invoke(method, ...params)
 }
