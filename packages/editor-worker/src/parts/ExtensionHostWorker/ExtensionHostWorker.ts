1c2ed1bee01d66cee3dc8694464fb8d8604cb643
Le Vivilet <levivlet@pm.me>
2025-06-09 11:07:34 +0200

    update api

--- packages/editor-worker/src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
+++ packages/editor-worker/src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
@@ -1,4 +1,3 @@
-import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
-import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
+import { ExtensionHost } from '@lvce-editor/rpc-registry'
 
-export const { listen, invoke, invokeAndTransfer } = CreateRpc.createRpc(IpcParentType.ExtensionHostWorker)
+export const { set, invoke, invokeAndTransfer } = ExtensionHost
