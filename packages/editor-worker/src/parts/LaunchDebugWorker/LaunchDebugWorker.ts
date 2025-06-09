966884c0cc0259193040245a06ee8befa5acb336
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-09 13:01:48 +0200

    feature: remove unused code (#656)

--- packages/editor-worker/src/parts/LaunchDebugWorker/LaunchDebugWorker.ts
+++
@@ -1,8 +0,0 @@
-import type { Rpc } from '@lvce-editor/rpc-registry'
-import { launchWorker } from '../LaunchWorker/LaunchWorker.ts'
-
-export const launchDebugWorker = async (): Promise<Rpc> => {
-  const name = 'Debug Worker'
-  const url = 'debugWorkerMain.js'
-  return launchWorker(name, url)
-}
