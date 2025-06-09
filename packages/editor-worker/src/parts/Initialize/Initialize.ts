3ea3c2b03bd974bb8f46f9560cdc25bd483b7c36
Le Vivilet <levivlet@pm.me>
2025-06-09 11:11:36 +0200

    remove unused code

--- packages/editor-worker/src/parts/Initialize/Initialize.ts
+++ packages/editor-worker/src/parts/Initialize/Initialize.ts
@@ -1,9 +1,9 @@
+import * as CreateExtensionHostRpc from '../CreateExtensionHostRpc/CreateExtensionHostRpc.ts'
+import * as CreateSyntaxHighlightingWorkerRpc from '../CreateSyntaxHighlightingWorkerRpc/CreateSyntaxHighlightingWorkerRpc.ts'
 import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
 import * as SyncIncremental from '../SyncIncremental/SyncIncremental.ts'
 import * as SyntaxHighlightingState from '../SyntaxHighlightingState/SyntaxHighlightingState.ts'
 import * as SyntaxHighlightingWorker from '../SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts'
-import * as CreateExtensionHostRpc from '../CreateExtensionHostRpc/CreateExtensionHostRpc.ts'
-import * as CreateSyntaxHighlightingWorkerRpc from '../CreateSyntaxHighlightingWorkerRpc/CreateSyntaxHighlightingWorkerRpc.ts'
 
 export const intialize = async (syntaxHighlightingEnabled: boolean, syncIncremental: boolean) => {
   if (syntaxHighlightingEnabled) {
