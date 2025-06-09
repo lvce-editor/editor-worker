078467279071315d1ae1b7d0dbcf356eb9ffc53c
Le Vivilet <levivlet@pm.me>
2025-06-09 12:59:55 +0200

    feature: use simpler syntax highlighting worker rpc

--- packages/editor-worker/src/parts/SendMessagePortToSyntaxHighlightingWorker/SendMessagePortToSyntaxHighlightingWorker.ts
+++ packages/editor-worker/src/parts/SendMessagePortToSyntaxHighlightingWorker/SendMessagePortToSyntaxHighlightingWorker.ts
@@ -1,10 +1,19 @@
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const sendMessagePortToSyntaxHighlightingWorker = async (port: MessagePort) => {
-  await RendererWorker.invokeAndTransfer(
-    // @ts-ignore
-    'SendMessagePortToSyntaxHighlightingWorker.sendMessagePortToSyntaxHighlightingWorker',
-    port,
-    'HandleMessagePort.handleMessagePort',
-  )
+  try {
+    await RendererWorker.invokeAndTransfer(
+      // @ts-ignore
+      'SendMessagePortToSyntaxHighlightingWorker.sendMessagePortToSyntaxHighlightingWorker',
+      port,
+      'HandleMessagePort.handleMessagePort2',
+    )
+  } catch {
+    await RendererWorker.invokeAndTransfer(
+      // @ts-ignore
+      'SendMessagePortToSyntaxHighlightingWorker.sendMessagePortToSyntaxHighlightingWorker',
+      port,
+      'HandleMessagePort.handleMessagePort',
+    )
+  }
 }
