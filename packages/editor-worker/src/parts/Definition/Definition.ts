59b78a93bde5820c4920658e87a3aed50b4cb1c5
Le Vivilet <levivlet@pm.me>
2025-06-09 11:15:29 +0200

    tsc

--- packages/editor-worker/src/parts/Definition/Definition.ts
+++ packages/editor-worker/src/parts/Definition/Definition.ts
@@ -2,6 +2,7 @@ import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 // @ts-ignore
 export const getDefinition = async (editor, offset) => {
+  // @ts-ignore
   const definition = await RendererWorker.invoke('ExtensionHostDefinition.executeDefinitionProvider', editor, offset)
   return definition
 }
