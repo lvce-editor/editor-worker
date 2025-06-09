59f3f518f22cda77e63a266fc821ce1f49e201dd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-09 18:56:05 +0200

    feature: set highlighted line when debug is paused (#663)
    * feature: set highlighted line when debug is paused
    
    * lint
    
    * log
    
    * types
    
    * lint

---
+++ packages/editor-worker/src/parts/DebugHighlight/DebugHighlight.ts
@@ -0,0 +1,5 @@
+export interface DebugHighlight {
+  readonly uri: string
+  readonly rowIndex: number
+  readonly columnIndex: number
+}
--- packages/editor-worker/src/parts/UpdateDebugInfo/UpdateDebugInfo.ts
+++ packages/editor-worker/src/parts/UpdateDebugInfo/UpdateDebugInfo.ts
@@ -1,3 +1,21 @@
-export const updateDebugInfo = (info: any): any => {
-  // TODO query all editors and update them with debug information if needed, then rerender them
+import * as Editors from '../Editors/Editors.ts'
+import * as GetDebugHighlight from '../GetDebugHighlight/GetDebugHighlight.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+
+const getKey = (): number => {
+  const keys = Editors.getKeys()
+  return parseInt(keys[0])
+}
+
+export const updateDebugInfo = async (debugId: number): Promise<void> => {
+  const newInfo = await GetDebugHighlight.getDebugHighlight(debugId)
+  const key = getKey()
+  const { oldState, newState } = Editors.get(key)
+  const newEditor = {
+    ...newState,
+    highlightedLine: newInfo.rowIndex,
+  }
+  Editors.set(key, oldState, newEditor)
+  // @ts-ignore
+  await RendererWorker.invoke('Editor.rerender', key)
 }
