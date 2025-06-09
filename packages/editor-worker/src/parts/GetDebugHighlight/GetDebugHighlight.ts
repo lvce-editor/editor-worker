52b5976fe66fbe8d11fbaffffd77498961e0a676
Le Vivilet <levivlet@pm.me>
2025-06-09 13:44:46 +0200

    feature: pass id on debug update

--- packages/editor-worker/src/parts/GetDebugHighlight/GetDebugHighlight.ts
+++ packages/editor-worker/src/parts/GetDebugHighlight/GetDebugHighlight.ts
@@ -1,6 +1,6 @@
 import * as DebugWorker from '../DebugWorker/DebugWorker.ts'
 
-export const getDebugHighlight = async (): Promise<any> => {
-  const newInfo = await DebugWorker.invoke('RunAndDebug.getHighlight')
+export const getDebugHighlight = async (debugId: number): Promise<any> => {
+  const newInfo = await DebugWorker.invoke('RunAndDebug.getHighlight', debugId)
   return newInfo
 }
