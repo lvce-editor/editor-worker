a6011ddb2c8f3b24aa5996e4d36f7701395af815
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-09 11:46:47 +0200

    fix: extension host rpc (#649)

--- packages/editor-worker/src/parts/CreateExtensionHostRpc/CreateExtensionHostRpc.ts
+++ packages/editor-worker/src/parts/CreateExtensionHostRpc/CreateExtensionHostRpc.ts
@@ -1,12 +1,14 @@
 import { type Rpc, PlainMessagePortRpcParent } from '@lvce-editor/rpc'
+import { RpcId } from '@lvce-editor/rpc-registry'
 import { VError } from '@lvce-editor/verror'
 import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
-import { sendMessagePortToExtensionHostWorker } from '../SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts'
+import { sendMessagePortToExtensionHostWorker2 } from '../SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts'
 
 export const createExtensionHostRpc = async (): Promise<Rpc> => {
   try {
     const { port1, port2 } = GetPortTuple.getPortTuple()
-    await sendMessagePortToExtensionHostWorker(port2)
+    const initialCommand = 'HandleMessagePort.handleMessagePort2'
+    await sendMessagePortToExtensionHostWorker2(port2, initialCommand, RpcId.EditorWorker)
     const rpc = await PlainMessagePortRpcParent.create({
       commandMap: {},
       messagePort: port1,
