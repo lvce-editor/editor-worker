ab73bf5d212693f187da1a8cd3d61da6bed0fb55
Le Vivilet <levivlet@pm.me>
2025-06-09 13:30:22 +0200

    fix port error

--- packages/editor-worker/src/parts/SendMessagePortToSyntaxHighlightingWorker/SendMessagePortToSyntaxHighlightingWorker.ts
+++ packages/editor-worker/src/parts/SendMessagePortToSyntaxHighlightingWorker/SendMessagePortToSyntaxHighlightingWorker.ts
@@ -1,19 +1,25 @@
+import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
-export const sendMessagePortToSyntaxHighlightingWorker = async (port: MessagePort) => {
+export const sendMessagePortToSyntaxHighlightingWorker = async (): Promise<MessagePort> => {
   try {
+    const { port1, port2 } = GetPortTuple.getPortTuple()
+
     await RendererWorker.invokeAndTransfer(
       // @ts-ignore
       'SendMessagePortToSyntaxHighlightingWorker.sendMessagePortToSyntaxHighlightingWorker',
-      port,
+      port1,
       'HandleMessagePort.handleMessagePort2',
     )
+    return port2
   } catch {
+    const { port1, port2 } = GetPortTuple.getPortTuple()
     await RendererWorker.invokeAndTransfer(
       // @ts-ignore
       'SendMessagePortToSyntaxHighlightingWorker.sendMessagePortToSyntaxHighlightingWorker',
-      port,
+      port1,
       'HandleMessagePort.handleMessagePort',
     )
+    return port2
   }
 }
