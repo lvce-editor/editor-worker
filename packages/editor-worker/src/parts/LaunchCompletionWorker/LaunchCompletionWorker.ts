a275e5f4e4449f04ba3234d3a536515e3b386da3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-13 22:12:03 +0200

    fix: initialize completion worker (#670)

--- packages/editor-worker/src/parts/LaunchCompletionWorker/LaunchCompletionWorker.ts
+++ packages/editor-worker/src/parts/LaunchCompletionWorker/LaunchCompletionWorker.ts
@@ -4,5 +4,7 @@ import { launchWorker } from '../LaunchWorker/LaunchWorker.ts'
 export const launchCompletionWorker = async (): Promise<Rpc> => {
   const name = 'Completion Worker'
   const url = 'completionWorkerMain.js'
-  return launchWorker(name, url)
+  const rpc = await launchWorker(name, url)
+  await rpc.invoke('Completions.initialize')
+  return rpc
 }
