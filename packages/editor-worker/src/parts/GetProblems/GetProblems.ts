b3736c2952924f8ac2ffc37c6d0075e2c28069eb
Le Vivilet <levivlet@pm.me>
2025-07-17 22:32:05 +0200

    feature: add function to query diagnostics for all open editors

--- packages/editor-worker/src/parts/GetProblems/GetProblems.ts
+++ packages/editor-worker/src/parts/GetProblems/GetProblems.ts
@@ -1,6 +1,22 @@
 import type { Problem } from '../Problem/Problem.ts'
+import * as Editors from '../Editors/Editors.ts'
+import * as ExtensionHostDiagnostic from '../ExtensionHostDiagnostic/ExtensionHostDiagnostic.ts'
+
+const getDiagnostics = (editor: any): Promise<any> => {
+  return ExtensionHostDiagnostic.executeDiagnosticProvider(editor)
+}
 
 export const getProblems = async (): Promise<readonly Problem[]> => {
-  // TODO return problems from currently open editors or ask extension host worker for problems
-  return []
+  // TODO maybe combine querying diagnostics for problems view with diagnostics for editor
+  // or query the diagnostics for the problems view directtly from the extension host worker
+  const keys = Editors.getKeys()
+  const editors = keys.map((key) => {
+    const numericKey = parseInt(key)
+    const editor = Editors.get(numericKey)
+    return editor
+  })
+  const newEditors = editors.map((editor) => editor.newState)
+  const diagnostics = await Promise.all(newEditors.map(getDiagnostics))
+  const flatDiagnostics = diagnostics.flat()
+  return flatDiagnostics
 }
