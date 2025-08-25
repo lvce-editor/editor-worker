6eeed7802383db9b088e67756f44cd304dd5439f
Le Vivilet <levivlet@pm.me>
2025-08-25 15:01:30 +0200

    skip for now

--- packages/e2e/src/editor.source-actions-open.ts
+++ packages/e2e/src/editor.source-actions-open.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'editor.source-actions-open'
 
+export const skip = 1
+
 export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
