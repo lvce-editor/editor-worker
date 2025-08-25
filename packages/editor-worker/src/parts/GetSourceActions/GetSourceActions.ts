d17c9bf556bf48ae2d23d2da4dfdec7414194bf7
Le Vivilet <levivlet@pm.me>
2025-08-25 22:07:42 +0200

    feature: get source actions based on language id

--- packages/editor-worker/src/parts/GetSourceActions/GetSourceActions.ts
+++ packages/editor-worker/src/parts/GetSourceActions/GetSourceActions.ts
@@ -1,8 +1,19 @@
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+import * as Editors from '../Editors/Editors.ts'
+
+const filterActions = (sourceActions: readonly any[], languageId: string) => {
+  return sourceActions.filter((action) => action.languageId === languageId)
+}
 
 // TODO ask extension host worker instead
-export const getEditorSourceActions = async () => {
+export const getEditorSourceActions = async (editorId: number): Promise<readonly any[]> => {
+  if (!editorId) {
+    return []
+  }
+  const { newState } = Editors.get(editorId)
+  const languageId = newState.languageId
   // @ts-ignore
-  const sourceActions = await RendererWorker.invoke('GetEditorSourceActions.getEditorSourceActions')
-  return sourceActions
+  const allActions = await RendererWorker.invoke('GetEditorSourceActions.getEditorSourceActions')
+  const filtered = filterActions(allActions, languageId)
+  return filtered
 }
