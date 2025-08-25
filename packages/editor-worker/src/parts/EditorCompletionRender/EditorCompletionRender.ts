9878cd164da1634bc478a83058014450387d0707
Le Vivilet <levivlet@pm.me>
2025-08-25 15:04:37 +0200

    feature: remove unused code

--- packages/editor-worker/src/parts/EditorCompletionRender/EditorCompletionRender.ts
+++
@@ -1,8 +0,0 @@
-import type { CompletionState } from '../CompletionState/CompletionState.ts'
-
-export const renderCompletion = (oldState: CompletionState, newState: CompletionState) => {
-  const commands = [...newState.commands]
-  // @ts-ignore
-  newState.commands = []
-  return commands
-}
