8f1b850c2d957dd946f9d31646dd1a0d89325501
Le Vivilet <levivlet@pm.me>
2025-08-25 16:08:04 +0200

    remove unused code

--- packages/editor-worker/src/parts/GetEmptySourceActionsVirtualDom/GetEmptySourceActionsVirtualDom.ts
+++
@@ -1,6 +0,0 @@
-import * as EditorStrings from '../EditorStrings/EditorStrings.ts'
-import * as GetEditorMessageVirtualDom from '../GetEditorMessageVirtualDom/GetEditorMessageVirtualDom.ts'
-
-export const getEmptySourceActionsVirtualDom = () => {
-  return GetEditorMessageVirtualDom.getEditorMessageVirtualDom(EditorStrings.noCodeActionsAvailable())
-}
