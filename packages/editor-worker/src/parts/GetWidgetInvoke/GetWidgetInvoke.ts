0213de39f23b5f8f4a448959806b5baf8db9739b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-25 15:07:24 +0200

    feature: add source action invoke (#734)

--- packages/editor-worker/src/parts/GetWidgetInvoke/GetWidgetInvoke.ts
+++ packages/editor-worker/src/parts/GetWidgetInvoke/GetWidgetInvoke.ts
@@ -2,6 +2,7 @@ import * as ColorPickerWorker from '../ColorPickerWorker/ColorPickerWorker.ts'
 import * as CompletionWorker from '../CompletionWorker/CompletionWorker.ts'
 import * as FindWidgetWorker from '../FindWidgetWorker/FindWidgetWorker.ts'
 import * as RenameWorker from '../RenameWorker/RenameWorker.ts'
+import * as SourceActionWorker from '../SourceActionWorker/SourceActionWorker.ts'
 import * as WidgetId from '../WidgetId/WidgetId.ts'
 
 export const getWidgetInvoke = (widgetId: number): any => {
@@ -14,6 +15,8 @@ export const getWidgetInvoke = (widgetId: number): any => {
       return FindWidgetWorker.invoke
     case WidgetId.Rename:
       return RenameWorker.invoke
+    case WidgetId.SourceAction:
+      return SourceActionWorker.invoke
     default:
       return undefined
   }
