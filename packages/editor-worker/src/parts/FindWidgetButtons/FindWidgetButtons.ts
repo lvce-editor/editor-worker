8f1d380a3422a495d6688684aac956ea854bbe2f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-04 14:59:58 +0200

    feature: remove unused code (#747)
    Co-authored-by: Le Vivilet <levivlet@pm.me>

--- packages/editor-worker/src/parts/FindWidgetButtons/FindWidgetButtons.ts
+++
@@ -1,6 +0,0 @@
-import type { FindWidgetButton } from '../FindWidgetButton/FindWidgetButton.ts'
-
-export interface FindWidgetButtons {
-  readonly findButtons: readonly FindWidgetButton[]
-  readonly replaceButtons: readonly FindWidgetButton[]
-}
--- packages/editor-worker/src/parts/GetFindMatchCountClassName/GetFindMatchCountClassName.ts
+++
@@ -1,9 +0,0 @@
-import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
-
-export const getFindMatchCountClassName = (matchCount: number, value: string): string => {
-  if (value && matchCount === 0) {
-    return MergeClassNames.mergeClassNames(ClassNames.FindWidgetMatchCount, ClassNames.FindWidgetMatchCountEmpty)
-  }
-  return ClassNames.FindWidgetMatchCount
-}
