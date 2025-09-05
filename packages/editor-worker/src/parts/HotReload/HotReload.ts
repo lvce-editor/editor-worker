27d484cdb5eab6dba04ebb195feba699d6e30f32
Le Vivilet <levivilet@pm.me>
2025-09-05 15:59:57 +0200

    feature: improve hotreload save (#759)
    * feature: improve hotreload save
    
    * lint

--- packages/editor-worker/src/parts/HotReload/HotReload.ts
+++ packages/editor-worker/src/parts/HotReload/HotReload.ts
@@ -1,4 +1,60 @@
+import { WidgetId } from '@lvce-editor/constants'
+import { get, getKeys } from '../Editors/Editors.ts'
+import * as FindWidgetWorker from '../FindWidgetWorker/FindWidgetWorker.ts'
+import { getWidgetInvoke } from '../GetWidgetInvoke/GetWidgetInvoke.ts'
+
+let isReloading = false
+
+const getWidgetName = (widgetId: number): string => {
+  switch (widgetId) {
+    case WidgetId.Find:
+      return `FindWidget`
+    default:
+      return ''
+  }
+}
+
+const saveState = async () => {
+  const keys = getKeys()
+  const savedStates = Object.create(null)
+  for (const key of keys) {
+    const editor = get(parseInt(key))
+    const { widgets } = editor.newState
+    for (const widget of widgets) {
+      const invoke = getWidgetInvoke(widget.id)
+      const fullKey = `${key}:${widget.uid}`
+      const name = getWidgetName(widget.id)
+      const savedState = await invoke(`${name}.saveState`, widget.uid)
+      savedStates[fullKey] = savedState
+    }
+  }
+  return savedStates
+}
+
+const relaunchWorkers = async () => {
+  await FindWidgetWorker.dispose()
+  await FindWidgetWorker.launch()
+}
+
+const restoreState = async (savedState: any) => {
+  // TODO
+}
+
 export const hotReload = async (): Promise<void> => {
+  if (isReloading) {
+    return
+  }
+  isReloading = true
+
+  // TODO save state
+
+  const savedStates = await saveState()
+
+  await relaunchWorkers()
+
+  await restoreState(savedStates)
+
+  isReloading = false
   // TODO
   // 1. find all editors
   // 2. find all widgets
