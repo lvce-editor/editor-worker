a60c217ee4325d83a081157e3a2776e130bb69ef
Le Vivilet <levivilet@pm.me>
2025-09-09 21:23:12 +0200

    feature: use worker for hover

--- packages/editor-worker/src/parts/EditorHoverWidget/EditorHoverWidget.ts
+++ packages/editor-worker/src/parts/EditorHoverWidget/EditorHoverWidget.ts
@@ -1,26 +1,42 @@
-import type { HoverWidget } from '../HoverWidget/HoverWidget.ts'
-import type { WidgetLifeCycleFunction } from '../WidgetLifeCycleFunction/WidgetLifeCycleFunction.ts'
+import { WidgetId } from '@lvce-editor/constants'
 import * as AddWidget from '../AddWidget/AddWidget.ts'
-import * as EditorHoverRender from '../EditorHoverRender/EditorHoverRender.ts'
-import * as RemoveWidget from '../RemoveWidget/RemoveWidget.ts'
+import { createFns } from '../CreateFns/CreateFns.ts'
+import type { HoverWidget } from '../HoverWidget/HoverWidget.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
+import * as RenderRename from '../RenderRename/RenderRename.ts'
 
-export const render: WidgetLifeCycleFunction<HoverWidget> = (widget) => {
-  const commands: readonly any[] = EditorHoverRender.renderHover(widget.oldState, widget.newState)
+export const render = (widget: HoverWidget) => {
+  const commands: readonly any[] = RenderRename.renderFull(widget.oldState, widget.newState)
   const wrappedCommands = []
   const { uid } = widget.newState
   for (const command of commands) {
-    if (command[0] === RenderMethod.SetDom2) {
-      wrappedCommands.push([command[0], uid, ...command.slice(1)])
+    if (
+      command[0] === RenderMethod.SetDom2 ||
+      command[0] === RenderMethod.SetCss ||
+      command[0] === RenderMethod.AppendToBody ||
+      command[0] === RenderMethod.SetBounds2 ||
+      command[0] === RenderMethod.RegisterEventListeners ||
+      command[0] === RenderMethod.SetSelectionByName ||
+      command[0] === RenderMethod.SetValueByName ||
+      command[0] === RenderMethod.SetFocusContext ||
+      command[0] === RenderMethod.SetUid ||
+      command[0] === 'Viewlet.focusSelector'
+    ) {
+      wrappedCommands.push(command)
     } else {
       wrappedCommands.push(['Viewlet.send', uid, ...command])
     }
   }
+  console.log({ wrappedCommands })
   return wrappedCommands
 }
 
-export const add: WidgetLifeCycleFunction<HoverWidget> = (widget) => {
-  return AddWidget.addWidget(widget, 'EditorHover', render)
+export const add = (widget: HoverWidget) => {
+  return AddWidget.addWidget(widget, 'EditorCompletion', render)
 }
 
-export const remove = RemoveWidget.removeWidget
+export const remove = (widget: HoverWidget) => {
+  return [['Viewlet.dispose', widget.newState.uid]]
+}
+
+export const { close } = createFns(['close'], '', WidgetId.Hover)
