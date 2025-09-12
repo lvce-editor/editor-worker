ff25f4d0dbf0017f7d5c657a2414bf802ec36e1f
Le Vivilet <levivilet@pm.me>
2025-09-12 10:18:00 +0200

    feature: add assertion (#786)

--- packages/editor-worker/src/parts/GetText/GetText.ts
+++ packages/editor-worker/src/parts/GetText/GetText.ts
@@ -1,7 +1,9 @@
+import * as Assert from '../Assert/Assert.ts'
 import * as Character from '../Character/Character.ts'
 import * as GetEditor from '../GetEditor/GetEditor.ts'
 
 export const getText = (editorUid: number) => {
+  Assert.number(editorUid)
   const editor = GetEditor.getEditor(editorUid)
   const { lines } = editor
   return lines.join(Character.NewLine)
