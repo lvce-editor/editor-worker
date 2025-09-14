520ad83358425394535c78026ae310f0f3a34a03
Le Vivilet <levivilet@pm.me>
2025-09-14 09:42:41 +0200

    feature: improve static export (#795)

--- packages/build/src/build-static.ts
+++ packages/build/src/build-static.ts
@@ -1,4 +1,4 @@
-import { cp } from 'node:fs/promises'
+import { cp, readFile, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { pathToFileURL } from 'node:url'
 import { root } from './root.ts'
@@ -10,7 +10,7 @@ const sharedProcessUrl = pathToFileURL(sharedProcessPath).toString()
 const sharedProcess = await import(sharedProcessUrl)
 
 process.env.PATH_PREFIX = '/editor-worker'
-const { commitHash }: { commitHash: string } = await sharedProcess.exportStatic({
+const { commitHash } = await sharedProcess.exportStatic({
   root,
   extensionPath: '',
   testPath: 'packages/e2e',
@@ -20,5 +20,24 @@ await cp(
   join(root, '.tmp', 'dist', 'dist', 'editorWorkerMain.js'),
   join(root, 'dist', commitHash, 'packages', 'editor-worker', 'dist', 'editorWorkerMain.js'),
 )
+
+const rendererWorkerPath = join(root, 'dist', commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
+
+export const getRemoteUrl = (path: string): string => {
+  const url = pathToFileURL(path).toString().slice(8)
+  return `/remote/${url}`
+}
+
+const content = await readFile(rendererWorkerPath, 'utf8')
+const workerPath = join(root, '.tmp/dist/dist/editorWorkerMain.js')
+const remoteUrl = getRemoteUrl(workerPath)
+
+if (content.includes('// const editorWorkerUrl = ')) {
+  const occurrence = `// const editorWorkerUrl = \`\${assetDir}/packages/editor-worker/dist/editorWorkerMain.js\`
+const editorWorkerUrl = \`${remoteUrl}\``
+  const replacement = `const editorWorkerUrl = \`\${assetDir}/packages/editor-worker/dist/editorWorkerMain.js\``
+  const newContent = content.replace(occurrence, replacement)
+  await writeFile(rendererWorkerPath, newContent)
+}
 
 await cp(join(root, 'dist'), join(root, '.tmp', 'static'), { recursive: true })
