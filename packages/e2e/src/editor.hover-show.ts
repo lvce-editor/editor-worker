d5f4fa9fbbce613ea128a5e49bd2c08e9fc4561d
Le Vivilet <levivilet@pm.me>
2025-09-14 09:23:14 +0200

    test

--- packages/e2e/src/editor.hover-show.ts
+++ packages/e2e/src/editor.hover-show.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'editor.hover-show'
 
+export const skip = 1
+
 export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect, Extension }) => {
   // arrange
   const url = new URL('../fixtures/editor.hover-show', import.meta.url).toString()
