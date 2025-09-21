e79383d8dfe1a6d770a681b628314caf3842c36e
Le Vivilet <levivilet@pm.me>
2025-09-21 11:43:47 +0200

    feature: make focus context rendering more functional

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandHandleFocus.ts
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandHandleFocus.ts
@@ -1,10 +1,14 @@
-import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+import { WhenExpression } from '@lvce-editor/constants'
+import type { State } from '../State/State.ts'
 
-const WhenExpressionEditorText = 12
-
-export const handleFocus = (editor: any) => {
-  // TODO make change events functional,
-  // when rendering, send focus changes to renderer worker
-  RendererWorker.invoke('Focus.setFocus', WhenExpressionEditorText)
-  return editor
+export const handleFocus = (editor: State): State => {
+  if (editor.focused && editor.focus === WhenExpression.FocusEditorText) {
+    return editor
+  }
+  return {
+    ...editor,
+    focused: true,
+    focus: WhenExpression.FocusEditorText,
+    additionalFocus: 0,
+  }
 }
