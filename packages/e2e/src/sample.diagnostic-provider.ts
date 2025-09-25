64205159d5c643265daca729e3f3fa9eaeaf8257
Le Vivilet <levivilet@pm.me>
2025-09-25 19:27:45 +0200

    feature: add types for diagnostic e2e tests (#844)

--- packages/e2e/src/sample.diagnostic-provider.ts
+++ packages/e2e/src/sample.diagnostic-provider.ts
@@ -1,20 +1,26 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
 export const name = 'sample.diagnostic-provider'
 
 export const skip = 1
 
-export const test = async ({ Main, Panel, FileSystem, Workspace, Extension, Locator, expect }) => {
+export const test: Test = async ({ Settings, Main, Panel, FileSystem, Workspace, Extension, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
-  await FileSystem.writeFile(`${tmpDir}/test.js`, `abcdefgh`)
+  await FileSystem.writeFile(`${tmpDir}/test.xyz`, `abcdefgh`)
   await Workspace.setPath(tmpDir)
   await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
-  await Main.openUri(`${tmpDir}/test.js`)
+  await Settings.update({ 'Editor.diagnosticsEnabled': true })
+
+  await Main.openUri(`${tmpDir}/test.xyz`)
+
+  // TODO verify diagnostics squiggles are visible
 
   // act
-  await Panel.open()
+  // await Panel.open()
 
   // assert
-  const problems = Locator(`.Viewlet.Problems`)
-  await expect(problems).toBeVisible()
-  await expect(problems).toHaveText('No problems have been detected in the workspace.')
+  // const problems = Locator(`.Viewlet.Problems`)
+  // await expect(problems).toBeVisible()
+  // await expect(problems).toHaveText('No problems have been detected in the workspace.')
 }
