be3702c19d82f4b386a1cce6562e886c77a1e2db
Le Vivilet <levivilet@pm.me>
2025-09-25 19:42:33 +0200

    feature: update diagnostic e2e test

--- packages/e2e/src/sample.diagnostic-provider.ts
+++ packages/e2e/src/sample.diagnostic-provider.ts
@@ -3,16 +3,30 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 export const name = 'sample.diagnostic-provider'
 
 export const skip = 1
-
-export const test: Test = async ({ Settings, Main, Panel, FileSystem, Workspace, Extension, Locator, expect }) => {
+export const test: Test = async ({ Editor, Settings, Main, Panel, FileSystem, Workspace, Extension, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/test.xyz`, `abcdefgh`)
   await Workspace.setPath(tmpDir)
   await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
-  await Settings.update({ 'Editor.diagnosticsEnabled': true })
+  await Settings.update({ 'editor.diagnostics': true })
 
+  // act
   await Main.openUri(`${tmpDir}/test.xyz`)
+
+  // assert
+
+  // @ts-ignore
+  await Editor.shouldHaveDiagnostics([
+    {
+      rowIndex: 1,
+      columnIndex: 1,
+      endRowIndex: 1,
+      endColumnIndex: 4,
+      type: 'error',
+      message: 'error',
+    },
+  ])
 
   // TODO verify diagnostics squiggles are visible
 
