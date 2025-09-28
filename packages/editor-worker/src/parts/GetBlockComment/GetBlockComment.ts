76486507317dec46b20ffd3d4b11bea999202a60
Le Vivilet <levivilet@pm.me>
2025-09-28 16:39:38 +0200

    feature: query block comment from extension if available (#865)

--- packages/editor-worker/src/parts/GetBlockComment/GetBlockComment.ts
+++ packages/editor-worker/src/parts/GetBlockComment/GetBlockComment.ts
@@ -1,6 +1,21 @@
+import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
 import * as Languages from '../Languages/Languages.ts'
 
-export const getBlockComment = async (editor: any) => {
+export const getBlockComment = async (editor: any, offset: number) => {
+  // TODO ask extension host worker,
+  // execute block comment provider with
+  // uri, language id, offset
+  // and the extension returns a matching block comment or undefined
+  try {
+    // @ts-ignore
+    const blockComment = await ExtensionHostWorker.invoke(`ExensionHostLanguages.getBlockComment`, editor.languageId, editor.uri, offset)
+    if (blockComment) {
+      return blockComment
+    }
+  } catch {
+    // ignore
+  }
+
   const languageConfiguration = await Languages.getLanguageConfiguration(editor)
   if (!languageConfiguration?.comments?.blockComment) {
     return undefined
