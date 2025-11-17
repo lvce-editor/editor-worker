76d8554bc1cf0cc4b5e68a3a47905824655fbee1
Le Vivilet <levivilet@pm.me>
2025-11-17 21:17:56 +0100

    feature: open completions on type when setting is enabled (#902)

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandType.ts
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandType.ts
@@ -1,12 +1,16 @@
 import * as Editor from '../Editor/Editor.ts'
 import * as EditOrigin from '../EditOrigin/EditOrigin.ts'
+import { openCompletion } from './EditorCommandOpenCompletion.ts'
 import { editorReplaceSelections } from './EditorCommandReplaceSelection.ts'
 
 export const type = async (editor: any, text: string) => {
   const changes = editorReplaceSelections(editor, [text], EditOrigin.EditorType)
   const newEditor = await Editor.scheduleDocumentAndCursorsSelections(editor, changes)
   if (newEditor.completionsOnType) {
-    // TODO open it if it is not visible
+    // TODO if completion widget is already visible, advance
+    // TODO if character typed is space or tab or separator, close completions
+    const withCompletion = await openCompletion(newEditor)
+    return withCompletion
   }
   return newEditor
 }
