d887fc5a08c54358ef81a9a529505f495f83bbbc
Le Vivilet <levivilet@pm.me>
2025-11-17 20:44:14 +0100

    feature: use lazy rpc for completion worker

--- packages/editor-worker/src/parts/CompletionWorker/CompletionWorker.ts
+++ packages/editor-worker/src/parts/CompletionWorker/CompletionWorker.ts
@@ -1,16 +1,3 @@
-import type { Rpc } from '@lvce-editor/rpc'
-import * as LaunchCompletionWorker from '../LaunchCompletionWorker/LaunchCompletionWorker.ts'
+import { createLazyRpc, RpcId } from '@lvce-editor/rpc-registry'
 
-let workerPromise: any
-
-const getOrCreate = (): Promise<Rpc> => {
-  if (!workerPromise) {
-    workerPromise = LaunchCompletionWorker.launchCompletionWorker()
-  }
-  return workerPromise
-}
-
-export const invoke = async (method: string, ...params: readonly any[]): Promise<any> => {
-  const worker = await getOrCreate()
-  return await worker.invoke(method, ...params)
-}
+export const { setFactory, invoke } = createLazyRpc(RpcId.CompletionWorker)
--- packages/editor-worker/src/parts/Initialize/Initialize.ts
+++ packages/editor-worker/src/parts/Initialize/Initialize.ts
@@ -1,6 +1,9 @@
+import { setFactory } from '../CompletionWorker/CompletionWorker.ts'
 import { initializeExtensionHost } from '../InitializeExtensionHost/InitializeExtensionHost.ts'
 import { initializeSyntaxHighlighting } from '../InitializeSyntaxHighlighting/InitializeSyntaxHighlighting.ts'
+import { launchCompletionWorker } from '../LaunchCompletionWorker/LaunchCompletionWorker.ts'
 
 export const intialize = async (syntaxHighlightingEnabled: boolean, syncIncremental: boolean) => {
+  setFactory(launchCompletionWorker)
   await Promise.all([initializeSyntaxHighlighting(syntaxHighlightingEnabled, syncIncremental), initializeExtensionHost()])
 }
