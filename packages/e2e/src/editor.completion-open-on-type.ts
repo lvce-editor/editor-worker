13f3fcb53f90834abdda01bcd108cafe31ebf8e5
Le Vivilet <levivilet@pm.me>
2025-11-17 21:16:47 +0100

    feature: enable e2e test

--- packages/e2e/src/editor.completion-open-on-type.ts
+++ packages/e2e/src/editor.completion-open-on-type.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'editor.completion-open-on-type'
 
-export const skip = 1
-
 export const test: Test = async ({ Extension, FileSystem, Workspace, Main, Editor, Locator, expect }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/editor.completion-open-on-type')
@@ -18,7 +16,6 @@ export const test: Test = async ({ Extension, FileSystem, Workspace, Main, Edito
   await Editor.type('t')
 
   // assert
-  // TODO completions should be open
   const completions = Locator('.EditorCompletion')
   await expect(completions).toBeVisible()
   const items = Locator('.EditorCompletionItem')
