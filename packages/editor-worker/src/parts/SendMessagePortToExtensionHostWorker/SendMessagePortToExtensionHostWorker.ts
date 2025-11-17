7cd0e6d1e9afed5d3fdf57eca046213c71ccf3f5
Le Vivilet <levivilet@pm.me>
2025-11-17 20:22:15 +0100

    feature: remove unused code (#885)

--- packages/editor-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts
+++ packages/editor-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts
@@ -1,14 +1,5 @@
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
-export const sendMessagePortToExtensionHostWorker = async (port: MessagePort): Promise<void> => {
-  // @ts-ignore
-  await RendererWorker.invokeAndTransfer(
-    'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker',
-    port,
-    'HandleMessagePort.handleMessagePort',
-  )
-}
-
 export const sendMessagePortToExtensionHostWorker2 = async (port: MessagePort, initialCommand: string, rpcId: number): Promise<void> => {
   await RendererWorker.invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker', port, initialCommand, rpcId)
 }
