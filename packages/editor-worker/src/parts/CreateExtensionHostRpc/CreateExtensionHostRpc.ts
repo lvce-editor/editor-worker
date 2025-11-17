82fd557e2c394fe517f0647d184d054a8e3b7fad
Le Vivilet <levivilet@pm.me>
2025-11-17 20:20:35 +0100

    feature: simplify extension host rpc

--- packages/editor-worker/src/parts/CreateExtensionHostRpc/CreateExtensionHostRpc.ts
+++ packages/editor-worker/src/parts/CreateExtensionHostRpc/CreateExtensionHostRpc.ts
@@ -1,18 +1,16 @@
-import { type Rpc, PlainMessagePortRpcParent } from '@lvce-editor/rpc'
+import { type Rpc, TransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import { RpcId } from '@lvce-editor/rpc-registry'
 import { VError } from '@lvce-editor/verror'
-import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
 import { sendMessagePortToExtensionHostWorker2 } from '../SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts'
 
 export const createExtensionHostRpc = async (): Promise<Rpc> => {
   try {
-    const { port1, port2 } = GetPortTuple.getPortTuple()
     const initialCommand = 'HandleMessagePort.handleMessagePort2'
-    // TODO use transfer rpc
-    await sendMessagePortToExtensionHostWorker2(port2, initialCommand, RpcId.EditorWorker)
-    const rpc = await PlainMessagePortRpcParent.create({
+    const rpc = await TransferMessagePortRpcParent.create({
       commandMap: {},
-      messagePort: port1,
+      async send(port) {
+        await sendMessagePortToExtensionHostWorker2(port, initialCommand, RpcId.EditorWorker)
+      },
     })
     return rpc
   } catch (error) {
