88f189f7fbca244e41907b85f7d0e28c015bc2b8
Le Vivilet <levivilet@pm.me>
2025-11-20 19:38:11 +0100

    feature: add right emoji e2e test

---
+++ packages/e2e/src/editor.cursor-character-right-emoji.ts
@@ -0,0 +1,19 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.editor-cursor-character-right-emoji'
+
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, '👮🏽‍♀️👮🏽‍♀️👮🏽‍♀️👮🏽‍♀️👮🏽‍♀️')
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await Editor.setCursor(0, 14)
+  await Editor.shouldHaveSelections(new Uint32Array([0, 14, 0, 14]))
+
+  // act
+  await Editor.cursorCharacterRight()
+
+  // assert
+  await Editor.shouldHaveSelections(new Uint32Array([0, 21, 0, 21]))
+}
