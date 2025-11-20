f54b90f2703d8d635f8b4b094aa8b82a9ded3e2f
Le Vivilet <levivilet@pm.me>
2025-11-20 19:39:59 +0100

    feature: add cursor down emoji test

---
+++ packages/e2e/src/editor.cursor-character-down-emoji.ts
@@ -0,0 +1,19 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.editor-cursor-character-down-emoji'
+
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, '👮🏽‍♀️👮🏽‍♀️👮🏽‍♀️👮🏽‍♀️👮🏽‍♀️\nabcde')
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await Editor.setCursor(0, 14)
+  await Editor.shouldHaveSelections(new Uint32Array([0, 14, 0, 14]))
+
+  // act
+  await Editor.cursorDown()
+
+  // assert
+  await Editor.shouldHaveSelections(new Uint32Array([1, 14, 1, 14])) // TODO
+}
