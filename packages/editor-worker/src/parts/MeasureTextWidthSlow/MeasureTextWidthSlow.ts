11c0757ed58480b0a5f3a4ca576bf67821a98b14
Le Vivilet <levivilet@pm.me>
2025-11-20 19:42:40 +0100

    feature: use separate worker for measuring text width

--- packages/editor-worker/src/parts/MeasureTextWidthSlow/MeasureTextWidthSlow.ts
+++ packages/editor-worker/src/parts/MeasureTextWidthSlow/MeasureTextWidthSlow.ts
@@ -1,9 +1,4 @@
-import * as Assert from '../Assert/Assert.ts'
-import * as GetFontString from '../GetFontString/GetFontString.ts'
-import * as GetLetterSpacingString from '../GetLetterSpacingString/GetLetterSpacingString.ts'
-import * as GetTextMeasureContext from '../GetTextMeasureContext/GetTextMeasureContext.ts'
-
-// TODO for text editor, could dispose measuring canvas after editor has been initialized to free up offscreencanvas space
+import * as TextMeasurementWorker from '../TextMeasurementWorker/TextMeasurementWorker.ts'
 
 export const measureTextWidthSlow = async (
   text: string,
@@ -14,21 +9,15 @@ export const measureTextWidthSlow = async (
   isMonoSpaceFont: boolean,
   charWidth: number,
 ): Promise<number> => {
-  Assert.string(text)
-  Assert.number(fontWeight)
-  Assert.number(fontSize)
-  Assert.string(fontFamily)
-  Assert.boolean(isMonoSpaceFont)
-  Assert.number(charWidth)
-  if (typeof letterSpacing !== 'number') {
-    throw new TypeError('letterSpacing must be of type number')
-  }
-  const letterSpacingString = GetLetterSpacingString.getLetterSpacingString(letterSpacing)
-  const fontString = GetFontString.getFontString(fontWeight, fontSize, fontFamily)
-  const ctx = GetTextMeasureContext.getContext()
-  ctx.letterSpacing = letterSpacingString
-  ctx.font = fontString
-  const metrics = ctx.measureText(text)
-  const { width } = metrics
+  const width = await TextMeasurementWorker.invoke(
+    'TextMeasurement.measureTextWidth',
+    text,
+    fontWeight,
+    fontSize,
+    fontFamily,
+    letterSpacing,
+    isMonoSpaceFont,
+    charWidth,
+  )
   return width
 }
