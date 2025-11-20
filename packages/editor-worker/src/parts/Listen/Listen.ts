a74e820a3f33f8751c9c6d03b2ffaa25f08dc8de
Le Vivilet <levivilet@pm.me>
2025-11-20 19:30:14 +0100

    feature: add text measurement worker rpc (#918)
    * feature: add text measurement worker rpc
    
    * listen
    
    * format

--- packages/editor-worker/src/parts/Listen/Listen.ts
+++ packages/editor-worker/src/parts/Listen/Listen.ts
@@ -1,8 +1,11 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
+import { createTextMeasurementWorkerRpc } from '../CreateTextMeasurementWorkerRpc/CreateTextMeasurementWorkerRpc.ts'
+import { setFactory } from '../TextMeasurementWorker/TextMeasurementWorker.ts'
 
 export const listen = async () => {
+  setFactory(createTextMeasurementWorkerRpc)
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
