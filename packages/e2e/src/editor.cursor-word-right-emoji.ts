f2e14be335dc5866782c95119bd15e18747a1bc7
Le Vivilet <levivilet@pm.me>
2025-11-21 19:06:42 +0100

    feature: add word right emoji e2e test

---
+++ packages/e2e/src/editor.cursor-word-right-emoji.ts
@@ -0,0 +1,19 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.editor-cursor-word-right-emoji'
+
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, `<title>👮🏽‍♀️👮🏽‍♀️👮🏽‍♀️👮🏽‍♀️👮🏽‍♀️</title>`)
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await Editor.setCursor(0, 7)
+  await Editor.shouldHaveSelections(new Uint32Array([0, 7, 0, 7]))
+
+  // act
+  await Editor.cursorWordRight()
+
+  // assert
+  await Editor.shouldHaveSelections(new Uint32Array([0, 49, 0, 49]))
+}
