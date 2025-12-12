44f013a89cfb4b178a550ba1ce5d1ba672e0c4bb
Le Vivilet <levivilet@pm.me>
2025-12-12 14:58:43 +0100

    feature: add select word test (#952)

---
+++ packages/e2e/src/editor.select-word.ts
@@ -0,0 +1,18 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.editor-select-word'
+
+export const test: Test = async ({ Command, Editor, FileSystem, Main, Workspace }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, `command_exists\n`)
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await Editor.setCursor(0, 8)
+
+  // act
+  await Command.execute('Editor.selectNextOccurrence')
+
+  // assert
+  await Editor.shouldHaveSelections(new Uint32Array([0, 8, 0, 13])) //TODO
+}
