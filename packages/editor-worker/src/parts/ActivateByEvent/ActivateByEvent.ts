eb0f7e6ec945560d03b2e80ae764653b3bf5a860
Le Vivilet <levivilet@pm.me>
2026-01-04 15:03:52 +0100

    validate

--- packages/editor-worker/src/parts/ActivateByEvent/ActivateByEvent.ts
+++ packages/editor-worker/src/parts/ActivateByEvent/ActivateByEvent.ts
@@ -1,6 +1,10 @@
+import * as Assert from '@lvce-editor/assert'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 // TODO add tests for this
 export const activateByEvent = async (event: string, assetDir: string, platform: number): Promise<void> => {
+  Assert.string(event)
+  Assert.string(assetDir)
+  Assert.number(platform)
   await RendererWorker.invoke('ExtensionHostManagement.activateByEvent', event, assetDir, platform)
 }
