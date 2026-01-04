d4fdab2e7eb1551271e4925f5fcc0626e4a9a6b7
Le Vivilet <levivilet@pm.me>
2026-01-04 14:51:40 +0100

    fix: getLanguages command

--- packages/editor-worker/src/parts/GetLanguages/GetLanguages.ts
+++ packages/editor-worker/src/parts/GetLanguages/GetLanguages.ts
@@ -1,6 +1,6 @@
 import { ExtensionManagementWorker } from '@lvce-editor/rpc-registry'
 
 export const getLanguages = async (platform: number, assetDir: string): Promise<readonly any[]> => {
-  const languages = await ExtensionManagementWorker.invoke('Languages.getLanguages', platform, assetDir)
+  const languages = await ExtensionManagementWorker.invoke('Extensions.getLanguages', platform, assetDir)
   return languages
 }
