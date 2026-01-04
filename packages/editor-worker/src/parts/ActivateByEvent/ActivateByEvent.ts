2b9f6f6a86f298c7ea46766e190579624c03acaf
Le Vivilet <levivilet@pm.me>
2026-01-04 14:59:18 +0100

    feature: pass platform to activateByEvent

--- packages/editor-worker/src/parts/ActivateByEvent/ActivateByEvent.ts
+++ packages/editor-worker/src/parts/ActivateByEvent/ActivateByEvent.ts
@@ -1,6 +1,6 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 // TODO add tests for this
-export const activateByEvent = async (event: string): Promise<void> => {
-  await RendererWorker.invoke('ExtensionHostManagement.activateByEvent', event)
+export const activateByEvent = async (event: string, assetDir: string, platform: number): Promise<void> => {
+  await RendererWorker.invoke('ExtensionHostManagement.activateByEvent', event, assetDir, platform)
 }
--- packages/editor-worker/src/parts/ExtensionHostEditor/ExtensionHostEditor.ts
+++ packages/editor-worker/src/parts/ExtensionHostEditor/ExtensionHostEditor.ts
@@ -1,9 +1,18 @@
 import * as ActivateByEvent from '../ActivateByEvent/ActivateByEvent.ts'
 import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
 
-export const execute = async ({ args, editor, event, method, noProviderFoundMessage, noProviderFoundResult = undefined }: any) => {
+export const execute = async ({
+  args,
+  assetDir,
+  editor,
+  event,
+  method,
+  noProviderFoundMessage,
+  noProviderFoundResult = undefined,
+  platform,
+}: any) => {
   const fullEvent = `${event}:${editor.languageId}`
-  await ActivateByEvent.activateByEvent(fullEvent)
+  await ActivateByEvent.activateByEvent(fullEvent, assetDir, platform)
   const result = await ExtensionHostWorker.invoke(method, editor.uid, ...args)
   return result
 }
