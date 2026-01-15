518cf01a96f3c7de3fed63af984f51bcae70f293
Le Vivilet <levivilet@pm.me>
2026-01-15 12:46:52 +0100

    feature: don't make assetDir and platform required for calling activateByEvent

--- packages/editor-worker/src/parts/ActivateByEvent/ActivateByEvent.ts
+++ packages/editor-worker/src/parts/ActivateByEvent/ActivateByEvent.ts
@@ -4,7 +4,7 @@ import { RendererWorker } from '@lvce-editor/rpc-registry'
 // TODO add tests for this
 export const activateByEvent = async (event: string, assetDir: string, platform: number): Promise<void> => {
   Assert.string(event)
-  Assert.string(assetDir)
-  Assert.number(platform)
+  // Assert.string(assetDir)
+  // Assert.number(platform)
   await RendererWorker.invoke('ExtensionHostManagement.activateByEvent', event, assetDir, platform)
 }
