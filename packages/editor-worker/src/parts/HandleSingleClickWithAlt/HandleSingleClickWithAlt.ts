2400e117bb7b381c3d33c4132c865c2ceecd3ced
Le Vivilet <levivilet@pm.me>
2026-01-27 20:03:38 +0100

    feature: on alt click, open external link (#991)

--- packages/editor-worker/src/parts/HandleSingleClickWithAlt/HandleSingleClickWithAlt.ts
+++ packages/editor-worker/src/parts/HandleSingleClickWithAlt/HandleSingleClickWithAlt.ts
@@ -1,12 +1,25 @@
 import * as EditorGoToDefinition from '../EditorCommand/EditorCommandGoToDefinition.ts'
+import * as LinkDetection from '../LinkDetection/LinkDetection.ts'
+import * as TextDocument from '../TextDocument/TextDocument.ts'
+import { openExternal } from './OpenExternal.ts'
 
 // TODO first change cursor position, then run go to definition
 // cursor should appear at mousedown position immediately
 export const handleSingleClickWithAlt = async (editor: any, position: any) => {
   const { columnIndex, rowIndex } = position
+
+  // Check if the click is on a link
+  const offset = TextDocument.offsetAt(editor, rowIndex, columnIndex)
+  const url = LinkDetection.getUrlAtOffset(editor, offset)
+
+  if (url) {
+    // Open the link
+    await openExternal(url)
+    return editor
+  }
+
+  // Otherwise, perform the default go to definition
   const newEditor = { ...editor, selections: new Uint32Array([rowIndex, columnIndex, rowIndex, columnIndex]) }
-  // TODO rectangular selection with alt click,
-  // but also go to definition with alt click
   const newEditor2 = await EditorGoToDefinition.goToDefinition(newEditor)
   return newEditor2
 }
