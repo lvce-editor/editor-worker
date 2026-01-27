5e20faaf296e67a28b2f0a2368ec18fa4864e162
Le Vivilet <levivilet@pm.me>
2026-01-27 12:19:55 +0100

    feature: add link detection function

---
+++ packages/editor-worker/test/LinkDetection.test.ts
@@ -0,0 +1,80 @@
+import { expect, test } from '@jest/globals'
+import * as LinkDetection from '../src/parts/LinkDetection/LinkDetection.ts'
+
+test('detects simple http URL', () => {
+  const links = LinkDetection.detectLinks('Check out http://example.com for more')
+  expect(links).toEqual([{ length: 18, start: 10 }])
+})
+
+test('detects https URL', () => {
+  const links = LinkDetection.detectLinks('Visit https://github.com today')
+  expect(links).toEqual([{ length: 18, start: 6 }])
+})
+
+test('detects ftp URL', () => {
+  const links = LinkDetection.detectLinks('Download from ftp://files.example.com here')
+  expect(links).toEqual([{ length: 23, start: 14 }])
+})
+
+test('detects URL with path', () => {
+  const links = LinkDetection.detectLinks('See https://example.com/path/to/page')
+  expect(links).toEqual([{ length: 32, start: 4 }])
+})
+
+test('detects URL with query string', () => {
+  const links = LinkDetection.detectLinks('Visit https://example.com/search?q=test')
+  expect(links).toEqual([{ length: 33, start: 6 }])
+})
+
+test('detects www URL without scheme', () => {
+  const links = LinkDetection.detectLinks('Go to www.example.com for info')
+  expect(links).toEqual([{ length: 15, start: 6 }])
+})
+
+test('detects multiple URLs', () => {
+  const links = LinkDetection.detectLinks('Check https://example.com and http://test.org')
+  expect(links).toEqual([
+    { length: 19, start: 6 },
+    { length: 15, start: 30 },
+  ])
+})
+
+test('does not detect plain domain without www or scheme', () => {
+  const links = LinkDetection.detectLinks('example.com is not a link')
+  expect(links).toEqual([])
+})
+
+test('detects ftps URL', () => {
+  const links = LinkDetection.detectLinks('Secure: ftps://secure.example.com/file')
+  expect(links).toEqual([{ length: 30, start: 8 }])
+})
+
+test('handles URL at start of line', () => {
+  const links = LinkDetection.detectLinks('https://example.com/page is interesting')
+  expect(links).toEqual([{ length: 24, start: 0 }])
+})
+
+test('handles URL at end of line', () => {
+  const links = LinkDetection.detectLinks('Visit my site at https://example.com')
+  expect(links).toEqual([{ length: 19, start: 17 }])
+})
+
+test('detects no links in plain text', () => {
+  const links = LinkDetection.detectLinks('This is just plain text with no links')
+  expect(links).toEqual([])
+})
+
+test('handles empty string', () => {
+  const links = LinkDetection.detectLinks('')
+  expect(links).toEqual([])
+})
+
+test('detects URL with subdomain', () => {
+  const links = LinkDetection.detectLinks('Check https://api.github.com/repos')
+  expect(links).toEqual([{ length: 28, start: 6 }])
+})
+
+test('detects URL with hyphenated domain', () => {
+  const links = LinkDetection.detectLinks('Visit https://my-awesome-site.com here')
+  expect(links).toEqual([{ length: 27, start: 6 }])
+})
