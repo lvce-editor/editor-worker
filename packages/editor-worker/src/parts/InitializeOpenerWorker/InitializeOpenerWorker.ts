962e6468390cbb8ab62b568723e0ffd10bbcd434
Le Vivilet <levivilet@pm.me>
2026-01-29 14:42:39 +0100

    feature: add initializeOpenerWorker (#995)

---
+++ packages/editor-worker/src/parts/InitializeOpenerWorker/InitializeOpenerWorker.ts
@@ -0,0 +1,19 @@
+import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
+import { TextMeasurementWorker } from '@lvce-editor/rpc-registry'
+
+const send = (port: MessagePort): Promise<void> => {
+  // @ts-ignore
+  return RendererWorker.sendMessagePortToTextMeasurementWorker(port)
+}
+
+export const initializeOpenerWorker = async (): Promise<void> => {
+  try {
+    const rpc = await LazyTransferMessagePortRpcParent.create({
+      commandMap: {},
+      send,
+    })
+    TextMeasurementWorker.set(rpc)
+  } catch {
+    // ignore
+  }
+}
