e6e73ec255b17bb9713372092aa374ba3a480d36
Le Vivilet <levivilet@pm.me>
2026-02-01 22:13:27 +0100

    feature: update saveUntitledFile and showFilePicker to accept platform parameter (#1016)

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandSave/showFilePicker.ts
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandSave/showFilePicker.ts
@@ -1,8 +1,8 @@
 import { OpenerWorker } from '@lvce-editor/rpc-registry'
 
-export const showFilePicker = async (): Promise<string> => {
+export const showFilePicker = async (platform: number): Promise<string> => {
   const dialogTitle = 'Save File' // TODO use i18n string
-  const { canceled, filePath } = await OpenerWorker.invoke('Open.showSaveDialog', dialogTitle, [])
+  const { canceled, filePath } = await OpenerWorker.invoke('Open.showSaveDialog', dialogTitle, [], platform)
   if (canceled) {
     return ''
   }
