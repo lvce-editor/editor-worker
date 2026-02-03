ea0418eca38df1dba4c949fbc154e9ff318627f9
Le Vivilet <levivilet@pm.me>
2026-02-04 00:14:57 +0100

    feature: simplify font loading by removing FontState checks (#1031)

--- packages/editor-worker/src/parts/Font/Font.ts
+++ packages/editor-worker/src/parts/Font/Font.ts
@@ -1,16 +1,5 @@
-import * as FontState from '../FontState/FontState.ts'
-import * as LoadFont from '../LoadFont/LoadFont.ts'
+import * as TextMeasurementWorker from '../TextMeasurementWorker/TextMeasurementWorker.ts'
 
 export const ensure = async (fontName: string, fontUrl: string) => {
-  if (FontState.isLoaded(fontName)) {
-    return
-  }
-  if (FontState.hasPending(fontName)) {
-    return FontState.getPending(fontName)
-  }
-  const promise = LoadFont.loadFont(fontName, fontUrl)
-  FontState.setPending(fontName, promise)
-  await promise
-  FontState.removePending(fontName)
-  FontState.setLoaded(fontName)
+  await TextMeasurementWorker.invoke('TextMeasurement.ensureFont', fontName, fontUrl)
 }
