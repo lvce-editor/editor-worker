798848eaf609a259aacfa75e7ef65c0ae5e9ce01
Le Vivilet <levivilet@pm.me>
2026-02-03 17:28:57 +0100

    feature: implement command wrapping with unwrapped commands exception (#1028)

--- packages/editor-worker/src/parts/WrapCommands/WrapCommands.ts
+++ packages/editor-worker/src/parts/WrapCommands/WrapCommands.ts
@@ -1,18 +1,10 @@
-import { WidgetId } from '@lvce-editor/constants'
 import * as Editors from '../Editors/Editors.ts'
 import * as RenderEditor from '../RenderEditor/RenderEditor.ts'
-import * as UnwrappedCommands from '../UnwrappedCommands/UnwrappedCommands.ts'
-import * as WrapWidgetCommand from '../WrapWidgetCommand/WrapWidgetCommand.ts'
-
-const widgetCommands = {
-  'ColorPicker.handleSliderPointerDown': WidgetId.ColorPicker,
-  'ColorPicker.handleSliderPointerMove': WidgetId.ColorPicker,
-} as any
 
 // TODO wrap commands globally, not per editor
 // TODO only store editor state in editor worker, not in renderer worker also
 
-const wrapCommand =
+export const wrapCommand =
   (fn: any) =>
   async (editorUid: number, ...args: any[]) => {
     const oldInstance = Editors.get(editorUid)
@@ -32,15 +24,3 @@ const wrapCommand =
       commands,
     }
   }
-
-export const wrapCommands = (commands: any) => {
-  for (const [key, value] of Object.entries(commands)) {
-    if (UnwrappedCommands.keep.includes(key)) {
-      continue
-    }
-    // TODO avoid wrapping functions twice
-    const innerWrappedWidgetId = widgetCommands[key]
-    const innerWrappedFn = innerWrappedWidgetId ? WrapWidgetCommand.wrapWidgetCommand(innerWrappedWidgetId, value) : value
-    commands[key] = wrapCommand(innerWrappedFn)
-  }
-}
