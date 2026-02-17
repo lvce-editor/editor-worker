3410676c586fe8ef58050739928f1ab9223d7742
Le Vivilet <levivilet@pm.me>
2026-02-17 22:50:56 +0100

    feature: add deleteAll command test case

---
+++ packages/e2e/src/editor.delete-all.ts
@@ -0,0 +1,18 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'editor.delete-all'
+
+export const test: Test = async ({ Command, Editor, FileSystem, Main, Workspace }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, `first line\nsecond line\n`)
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await Editor.setCursor(1, 3)
+
+  // act
+  await Command.execute('Editor.deleteAll')
+
+  // assert
+  await Editor.shouldHaveText('')
+}
