8e3a464aaabfd23034fe30403d6253ff9bd82247
Le Vivilet <levivilet@pm.me>
2026-02-17 22:52:02 +0100

    fix

--- packages/e2e/src/editor.delete-all.ts
+++ packages/e2e/src/editor.delete-all.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'editor.delete-all'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, Editor, FileSystem, Main, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
