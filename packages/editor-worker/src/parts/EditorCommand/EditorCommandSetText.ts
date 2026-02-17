794cddec4815ee36050b73bd1b1aac8267b171fc
Le Vivilet <levivilet@pm.me>
2026-02-17 22:59:40 +0100

    feature: refactor setText command to handle text changes with selection management

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandSetText.ts
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandSetText.ts
@@ -1,7 +1,29 @@
 import * as Assert from '../Assert/Assert.ts'
 import * as Editor from '../Editor/Editor.ts'
+import * as EditOrigin from '../EditOrigin/EditOrigin.ts'
+import * as SplitLines from '../SplitLines/SplitLines.ts'
+import * as TextDocument from '../TextDocument/TextDocument.ts'
 
 export const setText = (editor: any, text: string) => {
   Assert.string(text)
-  return Editor.setText(editor, text)
+  const endRowIndex = editor.lines.length - 1
+  const endColumnIndex = editor.lines.at(-1).length
+  const start = {
+    columnIndex: 0,
+    rowIndex: 0,
+  }
+  const end = {
+    columnIndex: endColumnIndex,
+    rowIndex: endRowIndex,
+  }
+  const changes = [
+    {
+      deleted: TextDocument.getSelectionText(editor, { end, start }),
+      end,
+      inserted: SplitLines.splitLines(text),
+      origin: EditOrigin.EditorType,
+      start,
+    },
+  ]
+  return Editor.scheduleDocumentAndCursorsSelections(editor, changes)
 }
