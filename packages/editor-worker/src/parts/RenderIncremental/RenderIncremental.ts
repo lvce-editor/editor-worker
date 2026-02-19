dbf276ffa0a5b3a57360c249cef088229766eadd
Le Vivilet <levivilet@pm.me>
2026-02-19 11:28:29 +0100

    feature: refactor renderIncremental to utilize getEditorVirtualDom for improved DOM retrieval

--- packages/editor-worker/src/parts/RenderIncremental/RenderIncremental.ts
+++ packages/editor-worker/src/parts/RenderIncremental/RenderIncremental.ts
@@ -2,10 +2,15 @@ import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { ViewletCommand } from '@lvce-editor/constants'
 import { diffTree } from '@lvce-editor/virtual-dom-worker'
 import type { EditorState } from '../State/State.ts'
+import { getEditorVirtualDom } from '../GetEditorVirtualDom/GetEditorVirtualDom.ts'
+
+const getDom = (state: EditorState): readonly VirtualDomNode[] => {
+  return getEditorVirtualDom(state)
+}
 
 export const renderIncremental = (oldState: EditorState, newState: EditorState): any => {
-  const oldDom: readonly VirtualDomNode[] = []
-  const newDom: readonly VirtualDomNode[] = []
+  const oldDom: readonly VirtualDomNode[] = getDom(oldState)
+  const newDom: readonly VirtualDomNode[] = getDom(newState)
   const patches = diffTree(oldDom, newDom)
   return [ViewletCommand.SetPatches, newState.uid, patches]
 }
