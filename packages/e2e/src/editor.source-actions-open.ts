2a83d9865a9586d19b6f8d2c6b95e6fa4b7cd4fb
Le Vivilet <levivilet@pm.me>
2026-02-19 14:47:18 +0100

    e2e

--- packages/e2e/src/editor.source-actions-open.ts
+++ packages/e2e/src/editor.source-actions-open.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'editor.source-actions-open'
 
+export const skip = 1
+
 export const test: Test = async ({ Editor, expect, Extension, FileSystem, Locator, Main }) => {
   // arrange
   const url = new URL('../fixtures/editor.source-actions-open', import.meta.url).toString()
