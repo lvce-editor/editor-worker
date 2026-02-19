0a65d4be04c195bdd480997a7b0bd9c83a68ed8c
Le Vivilet <levivilet@pm.me>
2026-02-19 14:48:35 +0100

    feature: register commands in listen function for improved command handling (#1076)

--- packages/editor-worker/src/parts/Listen/Listen.ts
+++ packages/editor-worker/src/parts/Listen/Listen.ts
@@ -2,9 +2,11 @@ import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 import { createTextMeasurementWorkerRpc } from '../CreateTextMeasurementWorkerRpc/CreateTextMeasurementWorkerRpc.ts'
+import { registerCommands } from '../EditorStates/EditorStates.ts'
 import { setFactory } from '../TextMeasurementWorker/TextMeasurementWorker.ts'
 
 export const listen = async () => {
+  registerCommands(CommandMap.commandMap)
   setFactory(createTextMeasurementWorkerRpc)
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
