0ffd541494e56757b22771245cf804fc0f912221
Le Vivilet <levivilet@pm.me>
2026-02-19 13:54:30 +0100

    feature: enhance isEqual function to compare all properties of EditorState (#1073)

--- packages/editor-worker/src/parts/DiffItems/DiffItems.ts
+++ packages/editor-worker/src/parts/DiffItems/DiffItems.ts
@@ -1,5 +1,10 @@
 import type { EditorState } from '../State/State.ts'
 
 export const isEqual = (oldState: EditorState, newState: EditorState): boolean => {
-  return oldState.lines === newState.lines
+  return (
+    oldState.lines === newState.lines &&
+    oldState.textInfos === newState.textInfos &&
+    oldState.differences === newState.differences &&
+    oldState.initial === newState.initial
+  )
 }
