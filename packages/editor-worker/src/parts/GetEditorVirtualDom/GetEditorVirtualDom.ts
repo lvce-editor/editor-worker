208b1e96526b0f533e3086acb59b711b0168e9b2
Le Vivilet <levivilet@pm.me>
2026-02-19 01:14:28 +0100

    feature: implement getEditorVirtualDom function and corresponding tests (#1047)

---
+++ packages/editor-worker/src/parts/GetEditorVirtualDom/GetEditorVirtualDom.ts
@@ -0,0 +1,117 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as GetCursorsVirtualDom from '../GetCursorsVirtualDom/GetCursorsVirtualDom.ts'
+import * as GetDiagnosticsVirtualDom from '../GetDiagnosticsVirtualDom/GetDiagnosticsVirtualDom.ts'
+import * as GetEditorGutterVirtualDom from '../GetEditorGutterVirtualDom/GetEditorGutterVirtualDom.ts'
+import * as GetEditorRowsVirtualDom from '../GetEditorRowsVirtualDom/GetEditorRowsVirtualDom.ts'
+import * as GetSelectionsVirtualDom from '../GetSelectionsVirtualDom/GetSelectionsVirtualDom.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+interface EditorVirtualDomOptions {
+  readonly cursorInfos?: readonly any[]
+  readonly diagnostics?: readonly any[]
+  readonly differences: readonly number[]
+  readonly gutterInfos?: readonly any[]
+  readonly highlightedLine?: number
+  readonly lineNumbers?: boolean
+  readonly scrollBarDiagnostics?: readonly any[]
+  readonly selectionInfos?: readonly any[]
+  readonly textInfos: readonly any[]
+}
+
+export const getEditorVirtualDom = ({
+  cursorInfos = [],
+  diagnostics = [],
+  differences,
+  gutterInfos = [],
+  highlightedLine = -1,
+  lineNumbers = true,
+  scrollBarDiagnostics = [],
+  selectionInfos = [],
+  textInfos,
+}: EditorVirtualDomOptions): readonly VirtualDomNode[] => {
+  const cursorInfosArray = [...cursorInfos]
+  const diagnosticsArray = [...diagnostics]
+  const gutterInfosArray = [...gutterInfos]
+  const scrollBarDiagnosticsArray = [...scrollBarDiagnostics]
+  const rowsDom = GetEditorRowsVirtualDom.getEditorRowsVirtualDom(textInfos, differences, lineNumbers, highlightedLine)
+  const cursorsDom = GetCursorsVirtualDom.getCursorsVirtualDom(cursorInfosArray)
+  const selectionsDom = GetSelectionsVirtualDom.getSelectionsVirtualDom(selectionInfos)
+  const diagnosticsDom = GetDiagnosticsVirtualDom.getDiagnosticsVirtualDom(diagnosticsArray)
+  const gutterDom = GetEditorGutterVirtualDom.getEditorGutterVirtualDom(gutterInfosArray)
+  const scrollBarDiagnosticsDom = GetDiagnosticsVirtualDom.getDiagnosticsVirtualDom(scrollBarDiagnosticsArray)
+
+  return [
+    {
+      childCount: 2,
+      className: 'Viewlet Editor',
+      role: 'code',
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: gutterInfosArray.length,
+      className: 'Gutter',
+      type: VirtualDomElements.Div,
+    },
+    ...gutterDom,
+    {
+      childCount: 4,
+      className: 'EditorContent',
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: 4,
+      className: 'EditorLayers',
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: selectionsDom.length,
+      className: 'Selections',
+      type: VirtualDomElements.Div,
+    },
+    ...selectionsDom,
+    {
+      childCount: textInfos.length,
+      className: 'EditorRows',
+      type: VirtualDomElements.Div,
+    },
+    ...rowsDom,
+    {
+      childCount: cursorsDom.length,
+      className: 'LayerCursor',
+      type: VirtualDomElements.Div,
+    },
+    ...cursorsDom,
+    {
+      childCount: diagnosticsDom.length,
+      className: 'LayerDiagnostics',
+      type: VirtualDomElements.Div,
+    },
+    ...diagnosticsDom,
+    {
+      childCount: scrollBarDiagnosticsDom.length,
+      className: 'EditorScrollBarDiagnostics',
+      type: VirtualDomElements.Div,
+    },
+    ...scrollBarDiagnosticsDom,
+    {
+      childCount: 1,
+      className: 'ScrollBar ScrollBarVertical',
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: 0,
+      className: 'ScrollBarThumb ScrollBarThumbVertical',
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: 1,
+      className: 'ScrollBar ScrollBarHorizontal',
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: 0,
+      className: 'ScrollBarThumb ScrollBarThumbHorizontal',
+      type: VirtualDomElements.Div,
+    },
+  ]
+}
