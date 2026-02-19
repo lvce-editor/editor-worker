0acd93e9c01ac4045afbee6fd9ccbf3fdcb602a0
Le Vivilet <levivilet@pm.me>
2026-02-19 14:49:41 +0100

    feature: update e2e:headless script to run tests in headless mode (#1065)

--- packages/e2e/src/editor.source-actions-empty.ts
+++ packages/e2e/src/editor.source-actions-empty.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'editor.source-actions-empty'
 
+export const skip = 1
+
 export const test: Test = async ({ Editor, expect, FileSystem, Locator, Main }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
