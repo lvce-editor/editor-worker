56584828b4521bcb34213aeef7a371f5f0519a9b
Le Vivilet <levivilet@pm.me>
2026-02-19 01:23:30 +0100

    feature: implement loadContent function for editor state management

---
+++ packages/editor-worker/src/parts/LoadContent/LoadContent.ts
@@ -0,0 +1,68 @@
+import { WhenExpression } from '@lvce-editor/constants'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+import type { EditorState } from '../State/State.ts'
+import * as Editor from '../Editor/Editor.ts'
+import * as EditorText from '../EditorText/EditorText.ts'
+import * as ExtensionHostCommandType from '../ExtensionHostCommandType/ExtensionHostCommandType.ts'
+import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
+import { getLanguageId } from '../GetLanguageId/GetLanguageId.ts'
+import { getLanguages } from '../GetLanguages/GetLanguages.ts'
+import * as LinkDetection from '../LinkDetection/LinkDetection.ts'
+import * as MeasureCharacterWidth from '../MeasureCharacterWidth/MeasureCharacterWidth.ts'
+import * as Preferences from '../Preferences/Preferences.ts'
+import * as SyncIncremental from '../SyncIncremental/SyncIncremental.ts'
+import * as UpdateDiagnostics from '../UpdateDiagnostics/UpdateDiagnostics.ts'
+
+export const loadContent = async (state: EditorState, savedState: unknown) => {
+  const { assetDir, diagnosticsEnabled, fontFamily, fontSize, fontWeight, height, letterSpacing, platform, uri, width, x, y } = state
+  // TODO support overwriting language id by setting it explicitly or via settings
+  const charWidth = await MeasureCharacterWidth.measureCharacterWidth(fontWeight, fontSize, fontFamily, letterSpacing)
+  const languages = await getLanguages(platform, assetDir)
+  const computedlanguageId = getLanguageId(uri, languages)
+  const newEditor0: EditorState = {
+    ...state,
+    charWidth,
+    languageId: computedlanguageId,
+  }
+  const content = await RendererWorker.readFile(uri)
+
+  // TODO avoid creating intermediate editors here
+  const newEditor1 = Editor.setBounds(newEditor0, x, y, width, height, 9)
+  const newEditor2 = Editor.setText(newEditor1, content)
+  let newEditor3 = newEditor2
+
+  // Detect links and initialize decorations
+  const linkDecorations = LinkDetection.detectAllLinksAsDecorations(newEditor3)
+  const newEditor3WithLinks = {
+    ...newEditor3,
+    decorations: linkDecorations,
+  }
+
+  const syncIncremental = SyncIncremental.getEnabled()
+  const { differences, textInfos } = await EditorText.getVisible(newEditor3WithLinks, syncIncremental)
+  const newEditor4 = {
+    ...newEditor3WithLinks,
+    differences,
+    focus: WhenExpression.FocusEditorText,
+    focused: true,
+    textInfos,
+  }
+
+  // TODO only sync when needed
+  // e.g. it might not always be necessary to send text to extension host worker
+  // @ts-ignore
+  await ExtensionHostWorker.invoke(ExtensionHostCommandType.TextDocumentSyncFull, uri, id, languageId, content)
+
+  // TODO await promise
+  if (diagnosticsEnabled) {
+    await UpdateDiagnostics.updateDiagnostics(newEditor4)
+  }
+
+  const completionsOnTypeRaw = await Preferences.get('editor.completionsOnType')
+  const completionsOnType = Boolean(completionsOnTypeRaw)
+  const newEditor5: EditorState = {
+    ...newEditor4,
+    completionsOnType,
+  }
+  return newEditor5
+}
