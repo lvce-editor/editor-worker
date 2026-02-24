ca91b16b92ef0c620f74724608d481fe72ef19cb
Le Vivilet <levivilet@pm.me>
2026-02-24 15:11:09 +0100

    feature: update getLanguages method to use correct invocation (#1088)

--- packages/editor-worker/src/parts/GetLanguages/GetLanguages.ts
+++ packages/editor-worker/src/parts/GetLanguages/GetLanguages.ts
@@ -4,6 +4,6 @@ import { ExtensionManagementWorker } from '@lvce-editor/rpc-registry'
 export const getLanguages = async (platform: number, assetDir: string): Promise<readonly any[]> => {
   Assert.number(platform)
   Assert.string(assetDir)
-  const languages = await ExtensionManagementWorker.invoke('Extensions.getLanguages', platform, assetDir)
+  const languages = await ExtensionManagementWorker.getLanguages(platform, assetDir)
   return languages
 }
