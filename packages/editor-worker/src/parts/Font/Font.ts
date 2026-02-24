b454319adb93f3d26297aca49eabd58e1484470b
Le Vivilet <levivilet@pm.me>
2026-02-24 15:16:42 +0100

    refactor: migrate TextMeasurementWorker imports to use @lvce-editor/rpc-registry (#1091)

--- packages/editor-worker/src/parts/Font/Font.ts
+++ packages/editor-worker/src/parts/Font/Font.ts
@@ -1,4 +1,4 @@
-import * as TextMeasurementWorker from '../TextMeasurementWorker/TextMeasurementWorker.ts'
+import { TextMeasurementWorker } from '@lvce-editor/rpc-registry'
 
 export const ensure = async (fontName: string, fontUrl: string) => {
   await TextMeasurementWorker.invoke('TextMeasurement.ensureFont', fontName, fontUrl)
