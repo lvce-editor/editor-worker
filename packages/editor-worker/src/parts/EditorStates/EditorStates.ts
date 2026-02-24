c96867df741cef816cda08809804f27a21ff11a8
Le Vivilet <levivilet@pm.me>
2026-02-24 16:29:50 +0100

    fix

--- packages/editor-worker/src/parts/EditorStates/EditorStates.ts
+++ packages/editor-worker/src/parts/EditorStates/EditorStates.ts
@@ -5,14 +5,14 @@ const editorStates = ViewletRegistry.create<any>()
 export const { diff, getCommandIds, registerCommands, wrapCommand, wrapGetter } = editorStates
 
 export const get = (id: number): any => {
-	return editorStates.get(id)
+  return editorStates.get(id)
 }
 
 export const getKeys = (): readonly string[] => {
-	const keys = editorStates.getKeys()
-	return keys.map(String)
+  const keys = editorStates.getKeys()
+  return keys.map(String)
 }
 
 export const set = (id: number, oldEditor: any, newEditor: any): void => {
-	editorStates.set(id, oldEditor, newEditor)
+  editorStates.set(id, oldEditor, newEditor)
 }
