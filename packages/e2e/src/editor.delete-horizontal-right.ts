7a661e3f2944fb48553139761d4c931930a233da
Le Vivilet <levivilet@pm.me>
2026-02-24 22:09:31 +0100

    skip

--- packages/e2e/src/editor.delete-horizontal-right.ts
+++ packages/e2e/src/editor.delete-horizontal-right.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.editor-delete-horizontal-right'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, Editor, FileSystem, Main, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/file1.txt`, `abc`)
