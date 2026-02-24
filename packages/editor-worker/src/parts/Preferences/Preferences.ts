6b7ac6e7532bb74ce05629ab1b3a6dbf3a233612
Le Vivilet <levivilet@pm.me>
2026-02-24 15:18:22 +0100

    refactor: replace invokeAndTransfer with direct RendererWorker methods for improved clarity (#1092)

--- packages/editor-worker/src/parts/Preferences/Preferences.ts
+++ packages/editor-worker/src/parts/Preferences/Preferences.ts
@@ -1,6 +1,6 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const get = async (key: string): Promise<any> => {
-  const value = await RendererWorker.invoke('Preferences.get', key)
+  const value = await RendererWorker.getPreference(key)
   return value
 }
--- packages/editor-worker/src/parts/SendMessagePortToRendererProcess/SendMessagePortToRendererProcess.ts
+++ packages/editor-worker/src/parts/SendMessagePortToRendererProcess/SendMessagePortToRendererProcess.ts
@@ -1,10 +1,5 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const sendMessagePortToRendererProcess = async (port: MessagePort) => {
-  await RendererWorker.invokeAndTransfer(
-    // @ts-ignore
-    'SendMessagePortToRendererProcess.sendMessagePortToRendererProcess',
-    port,
-    'HandleMessagePort.handleMessagePort',
-  )
+  await RendererWorker.sendMessagePortToRendererProcess(port)
 }
