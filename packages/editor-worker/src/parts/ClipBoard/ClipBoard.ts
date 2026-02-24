9daa7dde1221684a82c367698b329d41204c09d4
Le Vivilet <levivilet@pm.me>
2026-02-24 14:52:36 +0100

    feature: refactor clipboard methods to use RendererWorker directly

--- packages/editor-worker/src/parts/ClipBoard/ClipBoard.ts
+++ packages/editor-worker/src/parts/ClipBoard/ClipBoard.ts
@@ -4,19 +4,16 @@ import { VError } from '../VError/VError.ts'
 
 export const readText = async () => {
   try {
-    // @ts-ignore
-    return await RendererWorker.invoke('ClipBoard.readText')
+    return await RendererWorker.readClipBoardText()
   } catch (error) {
     // @ts-ignore
     if (error.message === 'Read permission denied.') {
-      // @ts-ignore
       throw new VError('Failed to read text from clipboard: The Browser disallowed reading from clipboard')
     }
     if (
       // @ts-ignore
       error.message === 'navigator.clipboard.readText is not a function'
     ) {
-      // @ts-ignore
       throw new VError('Failed to read text from clipboard: The Clipboard Api is not available in Firefox')
     }
     throw new VError(error, 'Failed to read text from clipboard')
@@ -26,7 +23,7 @@ export const readText = async () => {
 export const writeText = async (text: string) => {
   try {
     Assert.string(text)
-    await RendererWorker.invoke('ClipBoard.writeText', /* text */ text)
+    await RendererWorker.writeClipBoardText(text)
   } catch (error) {
     throw new VError(error, 'Failed to write text to clipboard')
   }
