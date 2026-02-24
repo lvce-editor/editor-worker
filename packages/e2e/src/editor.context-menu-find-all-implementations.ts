fe50cacb87734bfb1ce732a4d985472b90fd46bd
Le Vivilet <levivilet@pm.me>
2026-02-24 23:29:32 +0100

    feature: add context menu tests for copy, cut, paste, and other actions (#1107)

---
+++ packages/e2e/src/editor.context-menu-find-all-implementations.ts
@@ -0,0 +1,21 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'editor.context-menu-find-all-implementations'
+
+export const skip = 1
+
+export const test: Test = async ({ ContextMenu, Editor, expect, FileSystem, Locator, Main, Workspace }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, 'abc')
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+
+  // act
+  await Editor.openEditorContextMenu()
+  await ContextMenu.selectItem('Find All Implementations')
+
+  // assert
+  const sideBar = Locator('#SideBar')
+  await expect(sideBar).toBeVisible()
+}
