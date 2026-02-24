6c0c605b971d750a7a4aebaab2b7b6138269b162
Le Vivilet <levivilet@pm.me>
2026-02-24 23:35:00 +0100

    fix: adjust block size in large HTML document generation for accurate line count

--- packages/e2e/src/editor.large-html-file.ts
+++ packages/e2e/src/editor.large-html-file.ts
@@ -5,12 +5,12 @@ export const name = 'editor.large-html-file'
 const createLargeHtmlDocument = (): string => {
   const totalLines = 2_000_000
   const header = `<!doctype html>\n<meta charset="utf8">\n<body>\n`
-  const block = `  <p>These lines are repeated many times to save memory on\n  string data.</p>\n  <hr>\n  <img src="../../style/logo.svg">\n`
+  const block = `  <p>These lines are repeated many times to save memory on\n  string data.</p>\n  <hr>\n  <img src="../../style/logo.svg">\n\n`
   const footer = '</body>'
 
   const headerLines = 3
   const footerLines = 1
-  const blockLines = 4
+  const blockLines = 5
   const repeatCount = (totalLines - headerLines - footerLines) / blockLines
 
   return `${header}${block.repeat(repeatCount)}${footer}`
