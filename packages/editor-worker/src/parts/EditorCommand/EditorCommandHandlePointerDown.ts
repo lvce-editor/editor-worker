b6d4ba47f619912169dd98116cd52127360f139a
Le Vivilet <levivilet@pm.me>
2026-05-29 14:29:29 +0200

    feat: add handlePointerDown function for pointer down event handling (#1158)

---
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandHandlePointerDown.ts
@@ -0,0 +1,18 @@
+import * as ClickDetailType from '../ClickDetailType/ClickDetailType.ts'
+import * as EditorHandleDoubleClick from './EditorCommandHandleDoubleClick.ts'
+import * as EditorHandleSingleClick from './EditorCommandHandleSingleClick.ts'
+import * as EditorHandleTripleClick from './EditorCommandHandleTripleClick.ts'
+
+export const handlePointerDown = (state: any, button: number, altKey: number, ctrlKey: number, x: number, y: number, detail: any) => {
+  const modifier = 0
+  switch (detail) {
+    case ClickDetailType.Double:
+      return EditorHandleDoubleClick.handleDoubleClick(state, modifier, x, y)
+    case ClickDetailType.Single:
+      return EditorHandleSingleClick.handleSingleClick(state, modifier, x, y)
+    case ClickDetailType.Triple:
+      return EditorHandleTripleClick.handleTripleClick(state, modifier, x, y)
+    default:
+      return state
+  }
+}
