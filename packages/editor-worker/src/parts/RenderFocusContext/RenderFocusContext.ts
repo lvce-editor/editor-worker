d9dbdbfa2e06cecf2e1057de65770613e4ced26c
Le Vivilet <levivilet@pm.me>
2026-05-29 15:34:32 +0200

    fix: update focus context to use FocusEditorText instead of FocusExplorer

--- packages/editor-worker/src/parts/RenderFocusContext/RenderFocusContext.ts
+++ packages/editor-worker/src/parts/RenderFocusContext/RenderFocusContext.ts
@@ -2,5 +2,5 @@ import { ViewletCommand, WhenExpression } from '@lvce-editor/constants'
 import type { EditorState } from '../State/State.ts'
 
 export const renderFocusContext = (oldState: EditorState, newState: EditorState): readonly any[] => {
-  return [ViewletCommand.SetFocusContext, newState.uid, WhenExpression.FocusExplorer]
+  return [ViewletCommand.SetFocusContext, newState.uid, WhenExpression.FocusEditorText]
 }
