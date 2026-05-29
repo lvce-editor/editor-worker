d67b91c1a8d0f55cab5fcf9d0681e8e30a5143df
Le Vivilet <levivilet@pm.me>
2026-05-29 23:29:20 +0200

    fix

--- packages/editor-worker/test/EditorCommandHandleWheel.test.ts
+++ packages/editor-worker/test/EditorCommandHandleWheel.test.ts
@@ -3,15 +3,22 @@ import * as EditorCommandHandleWheel from '../src/parts/EditorCommand/EditorComm
 
 test('handleWheel delegates to shared setDelta logic', async () => {
   const editor = {
+    charWidth: 8,
     deltaX: 0,
     deltaY: 0,
+    decorations: [],
     finalDeltaY: 100,
     height: 40,
     itemHeight: 20,
+    invalidStartIndex: 0,
+    languageId: '',
+    lineCache: [],
     lines: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
     minimumSliderSize: 20,
     numberOfVisibleLines: 2,
     scrollBarHeight: 16,
+    tokenizerId: 0,
+    width: 100,
   }
 
   await expect(EditorCommandHandleWheel.handleWheel(editor, 0, 0, 40)).resolves.toMatchObject({
