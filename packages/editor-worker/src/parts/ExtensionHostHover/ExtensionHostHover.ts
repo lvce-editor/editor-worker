77c3de31d0fc0a6bacc0b1351dee1a233d1f548e
Le Vivilet <levivilet@pm.me>
2026-06-02 13:32:34 +0200

    feat: implement isolated hover provider execution for enhanced hover functionality

--- packages/editor-worker/src/parts/ExtensionHostHover/ExtensionHostHover.ts
+++ packages/editor-worker/src/parts/ExtensionHostHover/ExtensionHostHover.ts
@@ -1,11 +1,31 @@
+import { ExtensionManagementWorker } from '@lvce-editor/rpc-registry'
 import * as Assert from '../Assert/Assert.ts'
 import * as ExtensionHostActivationEvent from '../ExtensionHostActivationEvent/ExtensionHostActivationEvent.ts'
 import * as ExtensionHostCommandType from '../ExtensionHostCommandType/ExtensionHostCommandType.ts'
 import * as ExtensionHostEditor from '../ExtensionHostEditor/ExtensionHostEditor.ts'
+import * as TextDocument from '../TextDocument/TextDocument.ts'
+
+const getTextDocument = (editor: any) => {
+  return {
+    documentId: editor.id || editor.uid,
+    languageId: editor.languageId,
+    text: TextDocument.getText(editor),
+    uri: editor.uri,
+  }
+}
+
+const executeIsolatedHoverProvider = async (editor: any, offset: number) => {
+  const textDocument = getTextDocument(editor)
+  return ExtensionManagementWorker.invoke('Extensions.executeHoverProvider', textDocument, offset)
+}
 
 export const executeHoverProvider = async (editor: any, offset: number) => {
   Assert.object(editor)
   Assert.number(offset)
+  const isolatedHover = await executeIsolatedHoverProvider(editor, offset)
+  if (isolatedHover) {
+    return isolatedHover
+  }
   return ExtensionHostEditor.execute({
     args: [offset],
     editor,
