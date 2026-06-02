cec4f4f97dec074aa9bd7529a86644fc261a494a
Le Vivilet <levivilet@pm.me>
2026-06-02 12:19:21 +0200

    feat: refactor completion and formatting provider invocations to use text document structure

--- packages/editor-worker/src/parts/ExtensionHostCompletion/ExtensionHostCompletion.ts
+++ packages/editor-worker/src/parts/ExtensionHostCompletion/ExtensionHostCompletion.ts
@@ -1,12 +1,32 @@
+import { ExtensionManagementWorker } from '@lvce-editor/rpc-registry'
 import * as ExtensionHostActivationEvent from '../ExtensionHostActivationEvent/ExtensionHostActivationEvent.ts'
 import * as ExtensionHostCommandType from '../ExtensionHostCommandType/ExtensionHostCommandType.ts'
 import * as ExtensionHostEditor from '../ExtensionHostEditor/ExtensionHostEditor.ts'
+import * as TextDocument from '../TextDocument/TextDocument.ts'
 
 const combineResults = (results: any) => {
   return results[0] ?? []
 }
 
+const getTextDocument = (editor: any) => {
+  return {
+    documentId: editor.id || editor.uid,
+    languageId: editor.languageId,
+    text: TextDocument.getText(editor),
+    uri: editor.uri,
+  }
+}
+
+const executeIsolatedCompletionProvider = async (editor: any, offset: number) => {
+  const textDocument = getTextDocument(editor)
+  return ExtensionManagementWorker.invoke('Extensions.executeCompletionProvider', textDocument, offset)
+}
+
 export const executeCompletionProvider = async (editor: any, offset: number) => {
+  const isolatedCompletions = await executeIsolatedCompletionProvider(editor, offset)
+  if (isolatedCompletions.length > 0) {
+    return isolatedCompletions
+  }
   return ExtensionHostEditor.execute({
     args: [offset],
     combineResults,
@@ -22,7 +42,16 @@ const combineResultsResolve = (items: any) => {
   return items[0] ?? undefined
 }
 
+const executeIsolatedResolveCompletionItemProvider = async (editor: any, offset: number, name: string, completionItem: any) => {
+  const textDocument = getTextDocument(editor)
+  return ExtensionManagementWorker.invoke('Extensions.executeResolveCompletionItemProvider', textDocument, offset, name, completionItem)
+}
+
 export const executeResolveCompletionItem = async (editor: any, offset: any, name: any, completionItem: any) => {
+  const isolatedCompletionItem = await executeIsolatedResolveCompletionItemProvider(editor, offset, name, completionItem)
+  if (isolatedCompletionItem) {
+    return isolatedCompletionItem
+  }
   return ExtensionHostEditor.execute({
     args: [offset, name, completionItem],
     combineResults: combineResultsResolve,
