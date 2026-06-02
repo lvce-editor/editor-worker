2618a02126aa2040b657c4c1b901c78b5a536b1d
Le Vivilet <levivilet@pm.me>
2026-06-02 12:22:42 +0200

    feat: refactor completion and formatting provider invocations to use text document structure (#1177)

--- packages/editor-worker/src/parts/GetFormattingEdits/GetFormattingEdits.ts
+++ packages/editor-worker/src/parts/GetFormattingEdits/GetFormattingEdits.ts
@@ -1,5 +1,12 @@
 import { ExtensionManagementWorker } from '@lvce-editor/rpc-registry'
+import * as TextDocument from '../TextDocument/TextDocument.ts'
 
 export const getFormattingEdits = async (editor: any): Promise<readonly any[]> => {
-  return ExtensionManagementWorker.invoke('Extensions.executeFormattingProvider', editor)
+  const textDocument = {
+    documentId: editor.id || editor.uid,
+    languageId: editor.languageId,
+    text: TextDocument.getText(editor),
+    uri: editor.uri,
+  }
+  return ExtensionManagementWorker.invoke('Extensions.executeFormattingProvider', textDocument)
 }
