7ad9dc02e4728d3485c98851158c3d6f8b72288f
Le Vivilet <levivilet@pm.me>
2026-06-16 16:10:32 +0200

    fix

--- packages/e2e/src/editor.select-word-right-over-space.ts
+++ packages/e2e/src/editor.select-word-right-over-space.ts
@@ -2,14 +2,14 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.editor-select-word-right-over-space'
 
-export const test: Test = async ({ Editor, FileSystem, Main, Workspace }) => {
+export const test: Test = async ({ Command, Editor, FileSystem, Main, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/file1.txt`, `foo bar`)
   await Workspace.setPath(tmpDir)
   await Main.openUri(`${tmpDir}/file1.txt`)
   await Editor.setCursor(0, 3)
 
-  await Editor.selectWordRight()
+  await Command.execute('Editor.selectWordRight')
 
   await Editor.shouldHaveSelections(new Uint32Array([0, 3, 0, 7]))
 }
