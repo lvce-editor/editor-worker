36ec5fbaf5753552df367aab0baf219c2647bc86
Le Vivilet <levivilet@pm.me>
2026-06-16 14:54:10 +0200

    lazy

--- packages/editor-worker/src/parts/CreateExtensionHostRpc/CreateExtensionHostRpc.ts
+++ packages/editor-worker/src/parts/CreateExtensionHostRpc/CreateExtensionHostRpc.ts
@@ -1,19 +1,14 @@
-import { type Rpc, TransferMessagePortRpcParent } from '@lvce-editor/rpc'
+import { type Rpc, LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import { RpcId } from '@lvce-editor/rpc-registry'
-import { VError } from '@lvce-editor/verror'
 import { sendMessagePortToExtensionHostWorker2 } from '../SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts'
 
 export const createExtensionHostRpc = async (): Promise<Rpc> => {
-  try {
-    const initialCommand = 'HandleMessagePort.handleMessagePort2'
-    const rpc = await TransferMessagePortRpcParent.create({
-      commandMap: {},
-      async send(port) {
-        await sendMessagePortToExtensionHostWorker2(port, initialCommand, RpcId.EditorWorker)
-      },
-    })
-    return rpc
-  } catch (error) {
-    throw new VError(error, `Failed to create extension host rpc`)
-  }
+  const initialCommand = 'HandleMessagePort.handleMessagePort2'
+  const rpc = await LazyTransferMessagePortRpcParent.create({
+    commandMap: {},
+    async send(port) {
+      await sendMessagePortToExtensionHostWorker2(port, initialCommand, RpcId.EditorWorker)
+    },
+  })
+  return rpc
 }
