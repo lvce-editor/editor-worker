b72c3c3e22b751022b61614664342c45f3e8c170
Le Vivilet <levivilet@pm.me>
2026-06-16 14:11:15 +0200

    feat: reintroduce initialization of extension workers in listen function

--- packages/editor-worker/src/parts/Initialize/Initialize.ts
+++ packages/editor-worker/src/parts/Initialize/Initialize.ts
@@ -1,18 +1,8 @@
 import { setFactory } from '../CompletionWorker/CompletionWorker.ts'
-import { initializeExtensionHost } from '../InitializeExtensionHost/InitializeExtensionHost.ts'
-import { initializeExtensionManagementWorker } from '../InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts'
-import { initializeOpenerWorker } from '../InitializeOpenerWorker/InitializeOpenerWorker.ts'
 import { initializeSyntaxHighlighting } from '../InitializeSyntaxHighlighting/InitializeSyntaxHighlighting.ts'
-import { initializeTextMeasurementWorker } from '../InitializeTextMeasurementWorker/InitializeTextMeasurementWorker.ts'
 import { launchCompletionWorker } from '../LaunchCompletionWorker/LaunchCompletionWorker.ts'
 
 export const intialize = async (syntaxHighlightingEnabled: boolean, syncIncremental: boolean) => {
   setFactory(launchCompletionWorker)
-  await Promise.all([
-    initializeSyntaxHighlighting(syntaxHighlightingEnabled, syncIncremental),
-    initializeExtensionHost(),
-    initializeExtensionManagementWorker(),
-    initializeTextMeasurementWorker(),
-    initializeOpenerWorker(),
-  ])
+  await initializeSyntaxHighlighting(syntaxHighlightingEnabled, syncIncremental)
 }
--- packages/editor-worker/src/parts/Listen/Listen.ts
+++ packages/editor-worker/src/parts/Listen/Listen.ts
@@ -2,6 +2,10 @@ import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 import { registerCommands } from '../EditorStates/EditorStates.ts'
+import { initializeExtensionHost } from '../InitializeExtensionHost/InitializeExtensionHost.ts'
+import { initializeExtensionManagementWorker } from '../InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts'
+import { initializeOpenerWorker } from '../InitializeOpenerWorker/InitializeOpenerWorker.ts'
+import { initializeTextMeasurementWorker } from '../InitializeTextMeasurementWorker/InitializeTextMeasurementWorker.ts'
 
 export const listen = async () => {
   registerCommands(CommandMap.commandMap)
@@ -9,4 +13,5 @@ export const listen = async () => {
     commandMap: CommandMap.commandMap,
   })
   RendererWorker.set(rpc)
+  await Promise.all([initializeExtensionHost(), initializeExtensionManagementWorker(), initializeTextMeasurementWorker(), initializeOpenerWorker()])
 }
