66779ce8ae3d39b0bd12ff9b3c23f927cb94782c
Le Vivilet <levivilet@pm.me>
2026-06-17 12:23:05 +0200

    e2e

--- packages/e2e/src/editor.undo-redo-indent-selection.ts
+++ packages/e2e/src/editor.undo-redo-indent-selection.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.editor-undo-redo-indent-selection'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, Editor, FileSystem, Main, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/file1.txt`, 'one\ntwo\nthree')
--- packages/e2e/src/editor.undo-redo-toggle-line-comment-js.ts
+++ packages/e2e/src/editor.undo-redo-toggle-line-comment-js.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.editor-undo-redo-toggle-line-comment-js'
 
+export const skip = 1
+
 export const test: Test = async ({ Editor, FileSystem, Main, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/file1.js`, 'const one = 1\nconst two = 2')
