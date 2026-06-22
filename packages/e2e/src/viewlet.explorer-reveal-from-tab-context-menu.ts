0045a85a79b83ec4eae54cb6a3ef442e23fe8897
Le Vivilet <levivilet@pm.me>
2026-06-23 00:11:04 +0200

    e2e

--- packages/e2e/src/viewlet.explorer-reveal-from-tab-context-menu.ts
+++ packages/e2e/src/viewlet.explorer-reveal-from-tab-context-menu.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-reveal-from-tab-context-menu'
 
-// export const skip = 1
+export const skip = 1
 
 export const test: Test = async ({ Command, ContextMenu, expect, Explorer, FileSystem, Locator, Main, Workspace }) => {
   // arrange
