8e818846fd4b67f3c8f47f61989c060f5525b74e
Le Vivilet <levivilet@pm.me>
2026-06-23 10:33:16 +0200

    fix: focus issue (#1213)

--- packages/editor-worker/src/parts/DiffFocus/DiffFocus.ts
+++ packages/editor-worker/src/parts/DiffFocus/DiffFocus.ts
@@ -1,5 +1,8 @@
 import type { EditorState } from '../State/State.ts'
 
 export const isEqual = (oldState: EditorState, newState: EditorState): boolean => {
+  if (!newState.focused) {
+    return true
+  }
   return oldState.focused === newState.focused && oldState.focus === newState.focus
 }
