ea7699ef2a18080b098faa5eb7fad712fe23cdbe
Le Vivilet <levivilet@pm.me>
2026-06-24 11:14:44 +0200

    fix: scrolling race condition

--- packages/editor-worker/src/parts/RenderIncremental/RenderIncremental.ts
+++ packages/editor-worker/src/parts/RenderIncremental/RenderIncremental.ts
@@ -2,8 +2,9 @@ import { ViewletCommand } from '@lvce-editor/constants'
 import { type VirtualDomNode, diffTree } from '@lvce-editor/virtual-dom-worker'
 import type { EditorState } from '../State/State.ts'
 import { getEditorVirtualDom } from '../GetEditorVirtualDom/GetEditorVirtualDom.ts'
+import * as RenderedDoms from '../RenderedDoms/RenderedDoms.ts'
 
-const getDom = (state: EditorState): readonly VirtualDomNode[] => {
+export const getDom = (state: EditorState): readonly VirtualDomNode[] => {
   const { initial, textInfos } = state
   if (initial && textInfos.length === 0) {
     return []
@@ -13,11 +14,28 @@ const getDom = (state: EditorState): readonly VirtualDomNode[] => {
 }
 
 export const renderIncremental = (oldState: EditorState, newState: EditorState): any => {
-  const oldDom: readonly VirtualDomNode[] = getDom(oldState)
+  const oldDom: readonly VirtualDomNode[] = oldState.initial ? getDom(oldState) : RenderedDoms.get(newState.uid) || getDom(oldState)
   const newDom: readonly VirtualDomNode[] = getDom(newState)
+<<<<<<< Updated upstream
+=======
+  if (isFastScroll(oldState, newState)) {
+    RenderedDoms.set(newState.uid, newDom)
+    return [
+      ViewletCommand.SetPatches,
+      newState.uid,
+      [
+        {
+          nodes: newDom,
+          type: 6,
+        },
+      ],
+    ]
+  }
+>>>>>>> Stashed changes
   const patches = diffTree(oldDom, newDom)
   if (patches.length === 0) {
     return []
   }
+  RenderedDoms.set(newState.uid, newDom)
   return [ViewletCommand.SetPatches, newState.uid, patches]
 }
---
+++ packages/editor-worker/test/RenderIncremental.test.ts
@@ -0,0 +1,127 @@
+import { beforeEach, expect, test } from '@jest/globals'
+import { ViewletCommand } from '@lvce-editor/constants'
+import * as RenderedDoms from '../src/parts/RenderedDoms/RenderedDoms.ts'
+import * as RenderIncremental from '../src/parts/RenderIncremental/RenderIncremental.ts'
+
+const createState = (minLineY: number, textInfos: readonly any[]): any => ({
+  differences: textInfos.map(() => 0),
+  initial: false,
+  minLineY,
+  textInfos,
+  uid: 1,
+})
+
+beforeEach(() => {
+  RenderedDoms.clear()
+})
+
+test('renderIncremental uses full render when visible rows jump during fast scroll', () => {
+  const oldState = createState(0, [
+    [
+      ' '.repeat(4),
+      'Token Whitespace',
+      '"',
+      'Token Punctuation',
+      'node_modules/@babel/helper-validator-identifier',
+      'Token JsonPropertyName',
+      '"',
+      'Token Punctuation',
+      ':',
+      'Token Punctuation',
+      ' ',
+      'Token Whitespace',
+      '{',
+      'Token Punctuation',
+    ],
+  ])
+  const newState = createState(4, [
+    [
+      ' '.repeat(4),
+      'Token Whitespace',
+      '"',
+      'Token Punctuation',
+      '@cspell/dict-dotnet',
+      'Token JsonPropertyName',
+      '"',
+      'Token Punctuation',
+      ':',
+      'Token Punctuation',
+      ' ',
+      'Token Whitespace',
+      '"',
+      'Token Punctuation',
+      '^5.0.13',
+      'Token JsonPropertyValueString',
+      '"',
+      'Token Punctuation',
+      ',',
+      'Token Punctuation',
+    ],
+  ])
+
+  const command = RenderIncremental.renderIncremental(oldState, newState)
+
+  expect(command[0]).toBe(ViewletCommand.SetPatches)
+  expect(command[1]).toBe(1)
+  expect(command[2]).toEqual([
+    {
+      nodes: expect.any(Array),
+      type: 6,
+    },
+  ])
+})
+
+test('renderIncremental diffs from the last emitted dom when editor state is mutated', () => {
+  const textInfos = [
+    [
+      ' '.repeat(6),
+      'Token Whitespace',
+      '"',
+      'Token Punctuation',
+      'lerna',
+      'Token JsonPropertyName',
+      '"',
+      'Token Punctuation',
+      ':',
+      'Token Punctuation',
+      ' ',
+      'Token Whitespace',
+      '{',
+      'Token Punctuation',
+    ],
+  ]
+  const initialState = createState(0, [])
+  initialState.initial = true
+  const renderedState = createState(0, textInfos)
+
+  RenderIncremental.renderIncremental(initialState, renderedState)
+  textInfos[0] = [
+    ' '.repeat(6),
+    'Token Whitespace',
+    '"',
+    'Token Punctuation',
+    'lerna',
+    'Token JsonPropertyName',
+    '"',
+    'Token Punctuation',
+    ':',
+    'Token Punctuation',
+    ' ',
+    'Token Whitespace',
+    '"',
+    'Token Punctuation',
+    '^8.2.4',
+    'Token JsonPropertyValueString',
+    '"',
+    'Token Punctuation',
+    ',',
+    'Token Punctuation',
+  ]
+  const newState = createState(0, textInfos)
+
+  const command = RenderIncremental.renderIncremental(renderedState, newState)
+
+  expect(command[0]).toBe(ViewletCommand.SetPatches)
+  expect(command[1]).toBe(1)
+  expect(command[2]).not.toEqual([])
+})
