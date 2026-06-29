6301b1f6fc396120e7602cc610ec8484317463c4
Le Vivilet <levivilet@pm.me>
2026-06-29 21:18:54 +0200

    feature: add writable editor e2e test

---
+++ packages/e2e/src/viewlet.explorer-new-file-opens-writable-editor.ts
@@ -0,0 +1,31 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.explorer-new-file-opens-writable-editor'
+
+export const skip = 1
+
+export const test: Test = async ({ Editor, Explorer, expect, FileSystem, KeyBoard, Locator, Main, Workspace }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  const filePath = `${tmpDir}/created.txt`
+  await Workspace.setPath(tmpDir)
+
+  // act
+  await Explorer.newFile()
+  await Explorer.updateEditingValue('created.txt')
+  await Explorer.acceptEdit()
+
+  // assert
+  const tab = Locator('.MainTab[title$="created.txt"]')
+  const editorInput = Locator('.EditorInput textarea')
+  await expect(tab).toBeVisible()
+  await expect(editorInput).toBeFocused()
+
+  for (const key of 'hello') {
+    await KeyBoard.press(key)
+  }
+  await Editor.shouldHaveText('hello')
+
+  await Main.save()
+  await FileSystem.shouldHaveFile(filePath, 'hello')
+}
