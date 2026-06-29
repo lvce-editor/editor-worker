2245fba5039b4ac7166e589c88e6ccd83e52e827
Le Vivilet <levivilet@pm.me>
2026-06-29 21:43:08 +0200

    feature: improve explorer e2e test

--- packages/e2e/src/viewlet.explorer-new-file-opens-writable-editor.ts
+++ packages/e2e/src/viewlet.explorer-new-file-opens-writable-editor.ts
@@ -4,8 +4,10 @@ export const name = 'viewlet.explorer-new-file-opens-writable-editor'
 
 export const skip = 1
 
-export const test: Test = async ({ Editor, expect, Explorer, FileSystem, KeyBoard, Locator, Main, Workspace }) => {
+export const test: Test = async ({ SideBar, Editor, expect, Explorer, FileSystem, KeyBoard, Locator, Main, Workspace }) => {
   // arrange
+  await SideBar.hide()
+  await SideBar.open('Explorer')
   const tmpDir = await FileSystem.getTmpDir()
   const filePath = `${tmpDir}/created.txt`
   await Workspace.setPath(tmpDir)
@@ -19,6 +21,7 @@ export const test: Test = async ({ Editor, expect, Explorer, FileSystem, KeyBoar
   const tab = Locator('.MainTab[title$="created.txt"]')
   const editorInput = Locator('.EditorInput textarea')
   await expect(tab).toBeVisible()
+  // TODO
   await expect(editorInput).toBeFocused()
 
   for (const key of 'hello') {
