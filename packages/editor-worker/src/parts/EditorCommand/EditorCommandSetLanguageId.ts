18dba578caa7a759e5543bfc99974301d958bc13
Le Vivilet <levivilet@pm.me>
2026-07-16 11:52:17 +0200

    feature: add change language mode support (#1335)

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandSetLanguageId.ts
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandSetLanguageId.ts
@@ -1,48 +1,16 @@
-import * as EditorText from '../EditorText/EditorText.ts'
-import * as GetEditor from '../GetEditor/GetEditor.ts'
-import * as SyncIncremental from '../SyncIncremental/SyncIncremental.ts'
 import * as Tokenizer from '../Tokenizer/Tokenizer.ts'
 import * as TokenizerMap from '../TokenizerMap/TokenizerMap.ts'
-import * as TokenizerState from '../TokenizerState/TokenizerState.ts'
 
-// TODO add command to set language id
-// without needing to specify tokenizePath
 export const setLanguageId = async (editor: any, languageId: string, tokenizePath: string) => {
   const { tokenizerId } = editor
-  TokenizerState.setTokenizePath(languageId, tokenizePath)
-  // TODO move tokenizer to syntax highlighting worker
-  // TODO only load tokenizer if not already loaded
-  // if already loaded just set tokenizer and rerender text
-  // TODO race condition
   await Tokenizer.loadTokenizer(languageId, tokenizePath)
   const tokenizer = Tokenizer.getTokenizer(languageId)
   const newTokenizerId = tokenizerId + 1
   TokenizerMap.set(newTokenizerId, tokenizer)
 
-  const latest = GetEditor.getEditor(editor.uid)
-  if (!latest) {
-    return editor
-  }
-
-  const syncIncremental = SyncIncremental.getEnabled()
-  const { differences, textInfos } = await EditorText.getVisible(editor, syncIncremental)
-  const latest2 = GetEditor.getEditor(editor.uid)
-  if (!latest2) {
-    return editor
-  }
-  const newEditor4 = {
-    ...latest2,
-    differences,
-    focused: true,
-    textInfos,
-  }
-
-  // TODO don't update editor if tokenizer was already loaded
-  // TODO update syntax highlighting
-  // TODO get edits
-
   return {
-    ...newEditor4,
+    ...editor,
+    focused: true,
     invalidStartIndex: 0,
     languageId,
     tokenizerId: newTokenizerId,
---
+++ packages/editor-worker/test/EditorCommandSetLanguageId.test.ts
@@ -0,0 +1,49 @@
+import { beforeEach, expect, jest, test } from '@jest/globals'
+
+const getTokenizerMock = jest.fn()
+const loadTokenizerMock = jest.fn()
+const setTokenizerMock = jest.fn()
+
+jest.unstable_mockModule('../src/parts/Tokenizer/Tokenizer.ts', () => ({
+  getTokenizer: getTokenizerMock,
+  loadTokenizer: loadTokenizerMock,
+}))
+
+jest.unstable_mockModule('../src/parts/TokenizerMap/TokenizerMap.ts', () => ({
+  set: setTokenizerMock,
+}))
+
+const { setLanguageId } = await import('../src/parts/EditorCommand/EditorCommandSetLanguageId.ts')
+
+beforeEach(() => {
+  getTokenizerMock.mockReset()
+  loadTokenizerMock.mockReset()
+  setTokenizerMock.mockReset()
+})
+
+test('setLanguageId loads the tokenizer and invalidates syntax highlighting', async () => {
+  const editor = {
+    focused: false,
+    id: 1,
+    invalidStartIndex: 4,
+    languageId: 'plaintext',
+    tokenizerId: 2,
+    uid: 1,
+  }
+  const tokenizer = {
+    tokenizeLine() {},
+  }
+  getTokenizerMock.mockReturnValue(tokenizer)
+
+  const result = await setLanguageId(editor, 'xyz', '/extensions/test/tokenizeXyz.js')
+
+  expect(loadTokenizerMock).toHaveBeenCalledWith('xyz', '/extensions/test/tokenizeXyz.js')
+  expect(setTokenizerMock).toHaveBeenCalledWith(3, tokenizer)
+  expect(result).toEqual({
+    ...editor,
+    focused: true,
+    invalidStartIndex: 0,
+    languageId: 'xyz',
+    tokenizerId: 3,
+  })
+})
