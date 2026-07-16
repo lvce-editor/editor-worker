a5cb11dbcf1166ceda5fe354e5c3a63f4b131466
Le Vivilet <levivilet@pm.me>
2026-07-16 08:31:31 +0200

    feature: reuse page for e2e tests (#1326)

--- packages/e2e/src/find-widget-handle-replace-input.ts
+++ packages/e2e/src/find-widget-handle-replace-input.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'find-widget-handle-replace-input'
 
+export const skip = 1
+
 export const test: Test = async ({ Editor, expect, FileSystem, FindWidget, Locator, Main, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
