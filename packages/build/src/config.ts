e56f9998ca5834338416852b10e32afdee1d1316
Le Vivilet <levivilet@pm.me>
2026-07-25 03:57:57 +0000

    feature: migrate to npm workspaces

--- packages/build/src/config.ts
+++ packages/build/src/config.ts
@@ -7,4 +7,4 @@ export const workerPath = join(root, '.tmp/dist/dist/editorWorkerMain.js')
 
 export const minifiedWorkerPath = join(root, '.tmp/dist/dist/editorWorkerMain.min.js')
 
-export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).href
+export const playwrightPath = new URL('../../../node_modules/playwright/index.mjs', import.meta.url).href
