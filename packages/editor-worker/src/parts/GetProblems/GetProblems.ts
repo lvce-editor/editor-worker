5edfedec93ab141e826a4a12ca5c41760561f6bc
Le Vivilet <levivilet@pm.me>
2026-07-26 09:50:16 +0000

    fix: use cached diagnostics in problems view

--- packages/editor-worker/src/parts/GetProblems/GetProblems.ts
+++ packages/editor-worker/src/parts/GetProblems/GetProblems.ts
@@ -1,22 +1,12 @@
 import type { Problem } from '../Problem/Problem.ts'
 import * as Editors from '../EditorStates/EditorStates.ts'
-import * as ExtensionHostDiagnostic from '../ExtensionHostDiagnostic/ExtensionHostDiagnostic.ts'
-
-const getDiagnostics = (editor: any): Promise<any> => {
-  return ExtensionHostDiagnostic.executeDiagnosticProvider(editor)
-}
 
 export const getProblems = async (): Promise<readonly Problem[]> => {
-  // TODO maybe combine querying diagnostics for problems view with diagnostics for editor
-  // or query the diagnostics for the problems view directtly from the extension host worker
   const keys = Editors.getKeys()
-  const editors = keys.map((key) => {
+  const diagnostics = keys.flatMap((key) => {
     const numericKey = Number(key)
     const editor = Editors.get(numericKey)
-    return editor
+    return editor.newState.diagnostics
   })
-  const newEditors = editors.map((editor) => editor.newState)
-  const diagnostics = await Promise.all(newEditors.map(getDiagnostics))
-  const flatDiagnostics = diagnostics.flat()
-  return flatDiagnostics
+  return diagnostics
 }
