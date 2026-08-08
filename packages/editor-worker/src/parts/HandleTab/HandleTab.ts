a2990b153014981c4c7dd8d8fc197ed32c2579b7
Le Vivilet <levivilet@pm.me>
2026-08-08 05:05:11 +0000

    fix: indent selected text with tab

--- packages/editor-worker/src/parts/HandleTab/HandleTab.ts
+++ packages/editor-worker/src/parts/HandleTab/HandleTab.ts
@@ -1,7 +1,12 @@
 import * as ApplyTabCompletion from '../ApplyTabCompletion/ApplyTabCompletion.ts'
+import * as EditorIndentMore from '../EditorCommand/EditorCommandIndentMore.ts'
+import * as EditorSelection from '../EditorSelection/EditorSelection.ts'
 import * as TabCompletion from '../TabCompletion/TabCompletion.ts'
 
 export const handleTab = async (editor: any) => {
+  if (!EditorSelection.isEverySelectionEmpty(editor.selections)) {
+    return EditorIndentMore.indentMore(editor)
+  }
   const result = await TabCompletion.getTabCompletion(editor)
   if (!result) {
     // TODO enter tab or two spaces
