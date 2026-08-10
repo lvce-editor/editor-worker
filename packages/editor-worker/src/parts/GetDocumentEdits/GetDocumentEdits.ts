f7480cfed578ed8f1468362b6d912b28a4b92eb1
Le Vivilet <levivilet@pm.me>
2026-08-10 16:59:08 -0700

    Fix applying multiple offset-based document edits (#1431)

--- packages/editor-worker/src/parts/GetDocumentEdits/GetDocumentEdits.ts
+++ packages/editor-worker/src/parts/GetDocumentEdits/GetDocumentEdits.ts
@@ -6,24 +6,45 @@ import * as TextDocument from '../TextDocument/TextDocument.ts'
 
 export const getDocumentEdits = (editor: any, edits: readonly OffsetBasedEdit[]): readonly DocumentEdit[] => {
   const documentEdits: DocumentEdit[] = []
-  for (const edit of edits) {
-    const start = TextDocument.positionAt(editor, edit.startOffset)
-    const end = TextDocument.positionAt(editor, edit.endOffset)
-    const deleted = TextDocument.getSelectionText(editor, {
-      end,
-      start,
+  let currentDocument = editor
+  let linesDelta = 0
+  let offsetDelta = 0
+  const sortedEdits = edits.toSorted((a, b) => a.startOffset - b.startOffset || a.endOffset - b.endOffset)
+  for (const edit of sortedEdits) {
+    const currentStart = TextDocument.positionAt(currentDocument, edit.startOffset + offsetDelta)
+    const currentEnd = TextDocument.positionAt(currentDocument, edit.endOffset + offsetDelta)
+    const deleted = TextDocument.getSelectionText(currentDocument, {
+      end: currentEnd,
+      start: currentStart,
     })
     const documentEdit = {
       deleted,
-      end,
+      end: {
+        columnIndex: currentEnd.columnIndex,
+        rowIndex: currentEnd.rowIndex - linesDelta,
+      },
       inserted: SplitLines.splitLines(edit.inserted),
       origin: EditOrigin.Format,
-      start,
+      start: {
+        columnIndex: currentStart.columnIndex,
+        rowIndex: currentStart.rowIndex - linesDelta,
+      },
     }
     if (documentEdit.inserted.length === 0) {
       documentEdit.inserted = ['']
     }
     documentEdits.push(documentEdit)
+    const currentEdit = {
+      ...documentEdit,
+      end: currentEnd,
+      start: currentStart,
+    }
+    currentDocument = {
+      ...currentDocument,
+      lines: TextDocument.applyEdits(currentDocument, [currentEdit]),
+    }
+    linesDelta += documentEdit.inserted.length - documentEdit.deleted.length
+    offsetDelta += edit.inserted.length - (edit.endOffset - edit.startOffset)
   }
   return documentEdits
 }
