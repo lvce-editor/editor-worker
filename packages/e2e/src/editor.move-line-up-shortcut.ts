20b7607ed77ee64c434e150ebd300949e04707d9
Le Vivilet <levivilet@pm.me>
2026-08-21 13:26:16 +0000

    fix: await move line keybinding commands in e2e

--- packages/e2e/src/editor.move-line-up-shortcut.ts
+++ packages/e2e/src/editor.move-line-up-shortcut.ts
@@ -2,14 +2,16 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'editor.move-line-up-shortcut'
 
-export const test: Test = async ({ Editor, FileSystem, KeyBoard, Main, Workspace }) => {
+export const test: Test = async ({ Command, Editor, FileSystem, Main, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/file1.txt`, 'one\ntwo\nthree')
   await Workspace.setPath(tmpDir)
   await Main.openUri(`${tmpDir}/file1.txt`)
   await Editor.setSelections(new Uint32Array([1, 1, 2, 3]))
 
-  await KeyBoard.press('Alt+ArrowUp')
+  const alt = 1 << 9
+  const upArrow = 14
+  await Command.execute('KeyBindings.handleKeyBinding', alt | upArrow)
 
   await Editor.shouldHaveText('two\nthree\none')
   await Editor.shouldHaveSelections(new Uint32Array([0, 1, 1, 3]))
