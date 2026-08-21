6c09e8d3443ebe71451fc34083ede39f18df3f60
Le Vivilet <levivilet@pm.me>
2026-08-21 08:58:16 +0000

    fix: account for diagnostic marker memory

--- packages/editor-worker/src/parts/GetEditorScrollBarDiagnosticsVirtualDom/GetEditorScrollBarDiagnosticsVirtualDom.ts
+++ packages/editor-worker/src/parts/GetEditorScrollBarDiagnosticsVirtualDom/GetEditorScrollBarDiagnosticsVirtualDom.ts
@@ -1,16 +1,6 @@
 import type { ScrollBarDiagnostic } from '../ScrollbarDiagnostic/ScrollBarDiagnostic.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as DiagnosticType from '../DiagnosticType/DiagnosticType.ts'
-import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-
-const getClassName = (type: string): string => {
-  if (type === DiagnosticType.Warning) {
-    return MergeClassNames.mergeClassNames(ClassNames.ScrollBarDiagnostic, ClassNames.ScrollBarDiagnosticWarning)
-  }
-  return MergeClassNames.mergeClassNames(ClassNames.ScrollBarDiagnostic, ClassNames.ScrollBarDiagnosticError)
-}
 
 export const getEditorScrollBarDiagnosticsVirtualDom = (scrollBarDiagnostics: readonly ScrollBarDiagnostic[]): readonly VirtualDomNode[] => {
   return [
@@ -21,7 +11,7 @@ export const getEditorScrollBarDiagnosticsVirtualDom = (scrollBarDiagnostics: re
     },
     ...scrollBarDiagnostics.map(({ height, top, type }) => ({
       childCount: 0,
-      className: getClassName(type),
+      className: type === 'warning' ? 'ScrollBarDiagnostic ScrollBarDiagnosticWarning' : 'ScrollBarDiagnostic ScrollBarDiagnosticError',
       height,
       top,
       type: VirtualDomElements.Div,
