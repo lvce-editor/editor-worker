532252086f23e6f137604b168c41a48596843a38
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2026-09-13 14:40:42 +0000

    fix: show selected character count

--- packages/editor-worker/src/parts/GetEditorStatus/GetEditorStatus.ts
+++ packages/editor-worker/src/parts/GetEditorStatus/GetEditorStatus.ts
@@ -1,6 +1,25 @@
 import type { EditorStatus } from '../EditorStatus/EditorStatus.ts'
 import type { EditorState } from '../State/State.ts'
 
+const getOffset = (lines: readonly string[], rowIndex: number, columnIndex: number): number => {
+  let offset = columnIndex
+  for (let i = 0; i < rowIndex; i++) {
+    offset += lines[i].length + 1
+  }
+  return offset
+}
+
+const getSelectedChars = (editor: EditorState): number => {
+  const { lines, selections } = editor
+  let selectedChars = 0
+  for (let i = 0; i < selections.length; i += 4) {
+    const startOffset = getOffset(lines, selections[i], selections[i + 1])
+    const endOffset = getOffset(lines, selections[i + 2], selections[i + 3])
+    selectedChars += Math.abs(endOffset - startOffset)
+  }
+  return selectedChars
+}
+
 export const getEditorStatus = (editor: EditorState): EditorStatus => {
   const primarySelectionIndex = editor.primarySelectionIndex || 0
   const rowIndex = editor.selections[primarySelectionIndex + 2] || 0
@@ -12,6 +31,7 @@ export const getEditorStatus = (editor: EditorState): EditorStatus => {
     insertSpaces: editor.insertSpaces,
     languageId: editor.languageId,
     line: rowIndex + 1,
+    selectedChars: getSelectedChars(editor),
     tabSize: editor.tabSize,
   }
 }
