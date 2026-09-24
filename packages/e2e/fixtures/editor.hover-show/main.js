7fe314fc1805a2efcd17ed52573e0b9f55a0786e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2026-09-24 05:15:17 +0000

    test: isolate empty hover provider case

--- packages/e2e/fixtures/editor.hover-show/main.js
+++ packages/e2e/fixtures/editor.hover-show/main.js
@@ -1,16 +1,10 @@
 import { activate as activateExtensionApi, registerHoverProvider } from '@lvce-editor/api'
 
-let initialHoverReturned = false
-
 const provider = {
   id: 'xyz-hover',
   languageId: 'xyz',
   provideHover(textDocument, offset) {
     if (textDocument.uri.endsWith('/empty.xyz')) {
-      return {}
-    }
-    if (offset < 5 && !initialHoverReturned) {
-      initialHoverReturned = true
       return {}
     }
     return {
